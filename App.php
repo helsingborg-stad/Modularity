c5a1ec7dad7913dc676b36398aef6d29b9b3843b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-11-25 16:09:42 +0100

    Init

---
+++ App.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modular;
+
+class App
+{
+    public function __construct()
+    {
+        add_action('init', array($this, 'includeAcf'), 11);
+
+        new Options\General();
+    }
+
+    /**
+     * Includes Advanced Custom Fields if missing, notifies user to activate ACF PRO to get full expirience
+     * @return void
+     */
+    public function includeAcf()
+    {
+        include_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+        if (!is_plugin_active('advanced-custom-fields-pro/acf.php')
+            && !is_plugin_active('advanced-custom-fields/acf.php')
+        ) {
+            require_once MODULAR_PATH . 'source/acf/acf.php';
+
+            add_action('admin_notices', function () {
+                echo '<div class="notice error"><p>' .
+                        __('To get the full expirience of the <strong>Modular</strong> plugin, please activate the <a href="http://www.advancedcustomfields.com/pro/" target="_blank">Advanced Custom Fields Pro</a> plugin.', 'modular') .
+                     '</p></div>';
+            });
+        }
+    }
+}
