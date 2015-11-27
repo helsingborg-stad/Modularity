85b886759d4f0a3605fb6df77a75b21f6a1b5820
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-11-27 15:56:43 +0100

    Adds an empty page for module editor

--- source/php/Editor.php
+++ source/php/Editor.php
@@ -7,7 +7,28 @@ class Editor
     public function __construct()
     {
         $tabs = new \Modularity\Editor\Tabs();
-        $tabs->add(__('Content editor'), 'admin.php');
-        $tabs->add(__('Module editor'), 'layout.php');
+        $tabs->add(__('Content', 'modularity'), 'admin.php');
+        $tabs->add(__('Modules', 'modularity'), 'layout.php');
+
+        $this->modulesEditorPage();
+    }
+
+    public function modulesEditorPage()
+    {
+        add_action('admin_menu', function () {
+            add_submenu_page(
+                'options.php',
+                __('Modules', 'modularity'),
+                __('Modules'),
+                'edit_posts',
+                'modularity-editor',
+                array($this, 'modulesEditor')
+            );
+        });
+    }
+
+    public function modulesEditor()
+    {
+        echo "EDITORRRR!";
     }
 }
