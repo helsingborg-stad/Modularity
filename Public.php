53b0e5babe9ccdbb9f978fa9cd1af048f058abaa
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-12-01 10:49:36 +0100

    Decoding of base64 data uris

--- Public.php
+++ Public.php
@@ -1,1 +1,15 @@
 <?php
+
+function modularity_decode_icon($data) {
+    if (empty($data)) {
+        return;
+    }
+
+    $data = explode(',', $data);
+
+    if (strpos($data[0], 'svg') !== false) {
+        return base64_decode($data[1]);
+    }
+
+    return '<img src="' . base64_decode($data[1]) . '">';
+}
