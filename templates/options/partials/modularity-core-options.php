fea8c20b4a163519cadd9602e1b658bfbc3eeb04
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-12-01 10:49:58 +0100

    Changed variable name

--- templates/options/partials/modularity-core-options.php
+++ templates/options/partials/modularity-core-options.php
@@ -1,7 +1,7 @@
-<?php global $options; ?>
+<?php global $modularityOptions; ?>
 <div class="modularity-form-group">
     <label>
-        <input type="checkbox" name="<?php echo $this->getFieldName('show-modules-in-menu'); ?>" value="on" <?php checked(isset($options['show-modules-in-menu']) ? $options['show-modules-in-menu'] : null, 'on', true); ?>>
+        <input type="checkbox" name="<?php echo $this->getFieldName('show-modules-in-menu'); ?>" value="on" <?php checked(isset($modularityOptions['show-modules-in-menu']) ? $modularityOptions['show-modules-in-menu'] : null, 'on', true); ?>>
         <?php _e('Show module\'s post type in admin menu', 'modularity'); ?>
     </label>
 </div>
