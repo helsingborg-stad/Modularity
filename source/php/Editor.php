0be7fc8d14c57f9b302f45ca8dddf8bb68c7dc18
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-12-01 10:51:02 +0100

    Refactor

--- source/php/Editor.php
+++ source/php/Editor.php
@@ -2,15 +2,18 @@
 
 namespace Modularity;
 
-class Editor
+class Editor extends \Modularity\Options
 {
     public function __construct()
     {
         add_action('admin_head', array($this, 'registerTabs'));
-
         $this->registerEditorPage();
     }
 
+    /**
+     * Registers navigation tabs
+     * @return void
+     */
     public function registerTabs()
     {
         global $post;
@@ -19,27 +22,67 @@ class Editor
             $post = get_post($_GET['id']);
         }
 
-        $tabs = new \Modularity\Editor\Tabs();
-        $tabs->add(__('Content', 'modularity'), admin_url('post.php?post=' . $post->ID . '&action=edit'));
-        $tabs->add(__('Modules', 'modularity'), admin_url('options.php?page=modularity-editor&id=' . $post->ID));
+        if ($post) {
+            $tabs = new \Modularity\Editor\Tabs();
+            $tabs->add(__('Content', 'modularity'), admin_url('post.php?post=' . $post->ID . '&action=edit'));
+            $tabs->add(__('Modules', 'modularity'), admin_url('options.php?page=modularity-editor&id=' . $post->ID));
+        }
     }
 
+    /**
+     * Register an option page for the editor
+     * @return void
+     */
     public function registerEditorPage()
     {
-        add_action('admin_menu', function () {
-            add_submenu_page(
-                'options.php',
-                __('Modules', 'modularity'),
-                __('Modules'),
-                'edit_posts',
-                'modularity-editor',
-                array($this, 'modulesEditor')
-            );
-        });
+        $this->register(
+            $pageTitle = __('Modularity editor'),
+            $menuTitle = __('Editor'),
+            $capability = 'edit_posts',
+            $menuSlug = 'modularity-editor',
+            $iconUrl = null,
+            $position = 1,
+            $parent = 'options.php'
+        );
     }
 
-    public function modulesEditor()
+    /**
+     * Adds meta boxes to the options page
+     * @return void
+     */
+    public function addMetaBoxes()
     {
-        echo "EDITORRRR!";
+        // Publish
+        add_meta_box(
+            'modularity-mb-publish',
+            __('Save options', 'modularity'),
+            function () {
+                $templatePath = \Modularity\Helper\Wp::getTemplate('publish', 'options/partials');
+                include $templatePath;
+            },
+            $this->screenHook,
+            'side'
+        );
+
+        // Modules
+        add_meta_box(
+            'modularity-mb-modules',
+            __('Enabled modules', 'modularity'),
+            function () {
+                $modularityOptions = get_option('modularity-options');
+                $enabled = isset($modularityOptions['enabled-modules']) && is_array($modularityOptions['enabled-modules']) ? $modularityOptions['enabled-modules'] : array();
+                $available = \Modularity\Module::$available;
+
+                $modules = array();
+                foreach ($enabled as $module) {
+                    $modules[$module] = $available[$module];
+                }
+
+                $templatePath = \Modularity\Helper\Wp::getTemplate('enabled-modules', 'editor');
+                include $templatePath;
+            },
+            $this->screenHook,
+            'side'
+        );
     }
 }
