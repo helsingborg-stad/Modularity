8f085dedf201677481eb6f9854ff8adafa88bac2
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-12-03 09:12:26 +0100

    Added function_exist check for public function

--- Public.php
+++ Public.php
@@ -1,16 +1,18 @@
 <?php
 
-function modularity_decode_icon($data)
-{
-    if (empty($data)) {
-        return;
+if (!function_exists('modularity_decode_icon')) {
+    function modularity_decode_icon($data)
+    {
+        if (empty($data)) {
+            return;
+        }
+
+        $data = explode(',', $data);
+
+        if (strpos($data[0], 'svg') !== false) {
+            return base64_decode($data[1]);
+        }
+
+        return '<img src="' . base64_decode($data[1]) . '">';
     }
-
-    $data = explode(',', $data);
-
-    if (strpos($data[0], 'svg') !== false) {
-        return base64_decode($data[1]);
-    }
-
-    return '<img src="' . base64_decode($data[1]) . '">';
 }
