a67ec2fe59a0276ce7f9936c73e9d05595ce4ff7
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-19 14:21:55 +0100

    Inlaylist date support

--- templates/module/modularity-mod-inlaylist.php
+++ templates/module/modularity-mod-inlaylist.php
@@ -23,9 +23,17 @@
             <ul>
                 <?php foreach ($items as $item) : ?>
                     <?php if ($item['type'] == 'external') : ?>
-                    <li><a class="link-item link-item-outbound" href="<?php echo $item['link_external']; ?>" target="_blank"><?php echo $item['title'] ?></a></li>
+                    <li>
+                        <a class="link-item link-item-outbound" href="<?php echo $item['link_external']; ?>" target="_blank"><?php echo $item['title'] ?></a>
+                    </li>
                     <?php elseif ($item['type'] == 'internal') : ?>
-                    <li><a class="link-item" href="<?php echo get_permalink($item['link_internal']->ID); ?>" target="_blank"><?php echo (!empty($item['title'])) ? $item['title'] : $item['link_internal']->post_title; ?></a></li>
+                    <li>
+                        <a class="link-item" href="<?php echo get_permalink($item['link_internal']->ID); ?>"><?php echo (!empty($item['title'])) ? $item['title'] : $item['link_internal']->post_title; ?>
+                            <?php if ($item['date'] === true) : ?>
+                            <span class="date pull-right text-sm text-dark-gray"><?php echo date('Y-m-d', strtotime($item['link_internal']->post_date)); ?></span>
+                            <?php endif; ?>
+                        </a>
+                    </li>
                     <?php endif; ?>
                 <?php endforeach; ?>
             </ul>
