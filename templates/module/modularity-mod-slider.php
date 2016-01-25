8021b8c047b04e8b38dbca814c42ea4dc79d5a7d
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-25 16:23:45 +0100

    Slider template updates and fileds export

--- templates/module/modularity-mod-slider.php
+++ templates/module/modularity-mod-slider.php
@@ -1,24 +1,26 @@
 <?php
     $slides = get_field('slides', $module->ID);
 ?>
-<ul class="slider">
-<?php foreach ($slides as $slide) : ?>
-    <li>
-    <?php if ($slide['acf_fc_layout'] == 'image') : ?>
+<div class="slider">
+    <ul>
+    <?php foreach ($slides as $slide) : ?>
+        <li>
+        <?php if ($slide['acf_fc_layout'] == 'image') : ?>
 
-        <img src="<?php echo $slide['image']['url']; ?>">
+            <img src="<?php echo $slide['image']['url']; ?>">
 
-    <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'embed') : ?>
+        <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'embed') : ?>
 
-        <div class="ratio-16-9">
-            <?php echo \Modularity\Module\Slider\Slider::getEmbed($slide['embed_link'], ['content']); ?>
-        </div>
+            <div class="ratio-16-9">
+                <?php echo \Modularity\Module\Slider\Slider::getEmbed($slide['embed_link'], ['content']); ?>
+            </div>
 
-    <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'upload') : ?>
+        <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'upload') : ?>
 
-        Markup for uploaded video.
+            Markup for uploaded video.
 
-    <?php endif; ?>
-    </li>
-<?php endforeach; ?>
-</ul>
+        <?php endif; ?>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+</div>
