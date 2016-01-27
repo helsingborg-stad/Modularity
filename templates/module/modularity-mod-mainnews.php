947e0b5231944eef44ee33dc3c37c94b84663da3
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-27 15:51:17 +0100

    Main news template

---
+++ templates/module/modularity-mod-mainnews.php
@@ -0,0 +1,21 @@
+<?php
+    $news = get_field('main_news', $module->ID);
+    if (count($news) > 0) :
+?>
+<div class="grid">
+    <?php foreach ($news as $item) : $item = $item['news_item']; ?>
+    <div class="grid-lg-12">
+        <a href="#" class="box box-news box-news-horizontal">
+            <div class="box-image-container">
+                <img src="<?php echo get_thumbnail_source($item->ID); ?>">
+            </div>
+            <div class="box-content">
+                <h3 class="text-highlight"><?php echo apply_filters('the_title', $item->post_title); ?></h3>
+                <p><?php echo isset(get_extended($item->post_content)['main']) ? get_extended($item->post_content)['main'] : ''; ?></p>
+                <p><span class="link-item">Read more</span></p>
+            </div>
+        </a>
+    </div>
+    <?php endforeach; ?>
+</div>
+<?php endif; ?>
