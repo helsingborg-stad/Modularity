1703ab7f3276ffbb4d70620acbec11f33fb9b6f3
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-28 16:21:20 +0100

    Fixed inherit post template

--- templates/module/modularity-mod-inheritpost.php
+++ templates/module/modularity-mod-inheritpost.php
@@ -1,4 +1,5 @@
+<?php $inherit = get_field('page', $module->ID); ?>
 <article>
-    <h2><?php echo apply_filters('the_title', $module->post_title); ?></h2>
-    <?php echo apply_filters('the_content', $module->post_content); ?>
+    <h2><?php echo apply_filters('the_title', $inherit->post_title); ?></h2>
+    <?php echo apply_filters('the_content', $inherit->post_content); ?>
 </article>
