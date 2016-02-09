b7407c12a37e37cb4c0bf2ef8d0abb5547f6912b
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-09 14:44:35 +0100

    Now with embedded video functionality.

--- templates/module/modularity-mod-slider.php
+++ templates/module/modularity-mod-slider.php
@@ -23,7 +23,43 @@
 
         <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'upload') : ?>
 
-            Markup for uploaded video.
+            <?php
+
+                if (isset($slide['placeholder_image']) && !empty($slide['placeholder_image'])) {
+                    $image = wp_get_attachment_image_src(
+                        $slide['placeholder_image']['id'],
+                        apply_filters('modularity/image/hero/video',
+                            array(1140,641)
+                        )
+                    );
+                } else {
+                    $image = false;
+                }
+
+            ?>
+
+            <div class="slider-video" style="background-image:url('<?php echo ($image !== false ) ? $image[0] : ''; ?>');">
+
+                <video poster="<?php echo ($image !== false ) ? $image[0] : ''; ?>" loop="" preload="auto" autoplay="1">
+
+                    <!-- Mp4 -->
+                    <?php if(isset($slide['video_mp4']) && !empty($slide['video_mp4'])) { ?>
+                        <source src="<?php echo $slide['video_mp4']['url']; ?>" type="video/mp4">
+                    <?php } ?>
+
+                    <!-- Webm -->
+                    <?php if(isset($slide['video_webm']) && !empty($slide['video_webm'])) { ?>
+                        <source src="<?php echo $slide['video_webm']['url']; ?>" type="video/webm">
+                    <?php } ?>
+
+                    <!-- Ogg -->
+                    <?php if(isset($slide['video_ogg']) && !empty($slide['video_ogg'])) { ?>
+                        <source src="<?php echo $slide['video_ogg']['url']; ?>" type="video/ogg">
+                    <?php } ?>
+
+                </video>
+
+            </div>
 
         <?php endif; ?>
         </li>
