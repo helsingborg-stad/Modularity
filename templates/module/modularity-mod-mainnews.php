5bc445b0988beb402961011569a3c709d9612bb6
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-02 16:05:04 +0100

    Show image placeholder if any image is missing

--- templates/module/modularity-mod-mainnews.php
+++ templates/module/modularity-mod-mainnews.php
@@ -1,17 +1,30 @@
 <?php
     $news = get_field('main_news', $module->ID);
     if (count($news) > 0) :
+
+    $hasImages = false;
+
+    foreach ($news as $item) {
+        $item = $item['news_item'];
+        if (get_thumbnail_source($item->ID) !== false) {
+            $hasImages = true;
+        }
+    }
 ?>
 <div class="grid">
     <?php foreach ($news as $item) : $item = $item['news_item']; ?>
         <?php $thumbnail_image = get_thumbnail_source($item->ID); ?>
         <div class="grid-lg-12">
-            <a href="#" class="box box-news box-news-horizontal">
-                <?php if ( !empty ( $thumbnail_image ) ) { ?>
+            <a href="<?php echo get_permalink($item->ID); ?>" class="box box-news box-news-horizontal">
+                <?php if ($hasImages) : ?>
                     <div class="box-image-container">
+                        <?php if (get_thumbnail_source($item->ID)) : ?>
                         <img src="<?php echo get_thumbnail_source($item->ID); ?>" />
+                        <?php else : ?>
+                        <figure class="image-placeholder"></figure>
+                        <?php endif; ?>
                     </div>
-                <?php } ?>
+                <?php endif; ?>
                 <div class="box-content">
                     <h3 class="text-highlight"><?php echo apply_filters('the_title', $item->post_title); ?></h3>
                     <p><?php echo isset(get_extended($item->post_content)['main']) ? get_extended($item->post_content)['main'] : ''; ?></p>
