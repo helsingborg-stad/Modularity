218f838f5046e0c8d08f44d956138cd47d34bbe7
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-17 09:32:13 +0100

    Gallery markup

--- templates/module/modularity-mod-gallery.php
+++ templates/module/modularity-mod-gallery.php
@@ -2,10 +2,12 @@
 $fields = get_fields($module->ID);
 ?>
 
-<ul class="gallery">
+<ul class="image-gallery grid grid-gallery">
     <?php if (isset($fields['mod_gallery_images'])) : foreach ($fields['mod_gallery_images'] as $image) : ?>
-    <li class="gallery-item">
-        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-    </li>
+    <li class="grid-md-3">
+        <a class="box box-index lightbox-trigger" href="<?php echo $image['url']; ?>" <?php if (isset($image['caption']) && !empty($image['caption'])) : ?>data-caption="<?php echo $image['caption']; ?>"<?php endif; ?>>
+            <img alt="<?php echo $image['alt']; ?>" src="<?php echo $image['url']; ?>"/>
+        </a>
+     </li>
     <?php endforeach; endif; ?>
 </ul>
