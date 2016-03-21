d49bf573439956ade32a111b1fab565ec8be8533
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-03-21 14:14:32 +0100

    Updated text module with new markup.
    Not printing tile when not added.

--- templates/module/modularity-mod-text.php
+++ templates/module/modularity-mod-text.php
@@ -1,4 +1,8 @@
-<article>
-    <h2><?php echo apply_filters('the_title', $module->post_title); ?></h2>
-    <?php echo apply_filters('the_title', $module->post_content); ?>
-</article>
+<div class="box box-panel">
+    <?php if (!empty($module->post_title)) { ?>
+        <h4 class="box-title"><?php echo apply_filters('the_title', $module->post_title); ?></h4>
+    <?php } ?>
+    <div class="box-content gutter">
+        <?php echo apply_filters('the_content', $module->post_content); ?>
+    </div>
+</div>
