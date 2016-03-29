baabeb145d52c9505ffb5cf72f6f7e26a05eeeb7
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-29 15:44:53 +0200

    Slider image can now link to another internal or external page

--- templates/module/modularity-mod-slider.php
+++ templates/module/modularity-mod-slider.php
@@ -18,37 +18,41 @@
         }
         ?>
         <li class="type-<?php echo $slide['acf_fc_layout']; ?> <?php echo (isset($slide['activate_textblock']) && $slide['activate_textblock'] === true) ? 'has-text-block' : ''; ?>">
-        <?php if ($slide['acf_fc_layout'] == 'image') : ?>
+            <?php if (isset($slide['link_type']) && !empty($slide['link_type']) && $slide['link_type'] != 'false') : ?>
+            <a href="<?php echo isset($slide['link_url']) && !empty($slide['link_url']) ? $slide['link_url'] : '#' ?>" <?php if (isset($slide['link_target']) && $slide['link_target'] === true) : ?>target="_blank"<?php endif; ?>>
+            <?php endif; ?>
+            <?php // SLIDES ?>
+            <?php if ($slide['acf_fc_layout'] == 'image') : ?>
 
-            <div class="slider-image" style="background-image:url('<?php echo ($image !== false ) ? $image[0] : ''; ?>');"></div>
+                <div class="slider-image" style="background-image:url('<?php echo ($image !== false ) ? $image[0] : ''; ?>');"></div>
 
-        <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'embed') : ?>
-            <?php echo \Modularity\Module\Slider\Slider::getEmbed($slide['embed_link'], ['player'], $image); ?>
-        <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'upload') : ?>
+            <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'embed') : ?>
+                <?php echo \Modularity\Module\Slider\Slider::getEmbed($slide['embed_link'], ['player'], $image); ?>
+            <?php elseif ($slide['acf_fc_layout'] == 'video' && $slide['type'] == 'upload') : ?>
 
-            <div class="slider-video" style="background-image:url('<?php echo ($image !== false ) ? $image[0] : ''; ?>');">
+                <div class="slider-video" style="background-image:url('<?php echo ($image !== false ) ? $image[0] : ''; ?>');">
 
-                <video poster="<?php echo ($image !== false ) ? $image[0] : ''; ?>" preload="auto" autoplay loop muted>
+                    <video poster="<?php echo ($image !== false ) ? $image[0] : ''; ?>" preload="auto" autoplay loop muted>
 
-                    <!-- Mp4 -->
-                    <?php if (isset($slide['video_mp4']) && !empty($slide['video_mp4'])) : ?>
-                        <source src="<?php echo $slide['video_mp4']['url']; ?>" type="video/mp4">
-                    <?php endif; ?>
+                        <!-- Mp4 -->
+                        <?php if (isset($slide['video_mp4']) && !empty($slide['video_mp4'])) : ?>
+                            <source src="<?php echo $slide['video_mp4']['url']; ?>" type="video/mp4">
+                        <?php endif; ?>
 
-                    <!-- Webm -->
-                    <?php if (isset($slide['video_webm']) && !empty($slide['video_webm'])) : ?>
-                        <source src="<?php echo $slide['video_webm']['url']; ?>" type="video/webm">
-                    <?php endif; ?>
+                        <!-- Webm -->
+                        <?php if (isset($slide['video_webm']) && !empty($slide['video_webm'])) : ?>
+                            <source src="<?php echo $slide['video_webm']['url']; ?>" type="video/webm">
+                        <?php endif; ?>
 
-                    <!-- Ogg -->
-                    <?php if (isset($slide['video_ogg']) && !empty($slide['video_ogg'])) : ?>
-                        <source src="<?php echo $slide['video_ogg']['url']; ?>" type="video/ogg">
-                    <?php endif; ?>
+                        <!-- Ogg -->
+                        <?php if (isset($slide['video_ogg']) && !empty($slide['video_ogg'])) : ?>
+                            <source src="<?php echo $slide['video_ogg']['url']; ?>" type="video/ogg">
+                        <?php endif; ?>
 
-                </video>
-            </div>
+                    </video>
+                </div>
 
-        <?php endif; ?>
+            <?php endif; // END SLIDERS ?>
 
             <!-- Text -->
             <?php
@@ -64,6 +68,10 @@
                 echo '<span class="text-block' . $classes . '"><span>' . do_shortcode($slide['textblock_content']) . '</span></span>';
             }
             ?>
+
+            <?php if ($slide['link_type'] != 'false') : ?>
+            </a>
+            <?php endif; ?>
         </li>
     <?php endforeach; ?>
     </ul>
