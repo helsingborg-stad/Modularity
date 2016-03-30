843bc76615e00a5e84dc3de6744ef9ab35ac60e4
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-30 15:33:26 +0200

    Text module box fix

--- templates/module/modularity-mod-text.php
+++ templates/module/modularity-mod-text.php
@@ -2,7 +2,7 @@
     <?php if (!empty($module->post_title)) { ?>
         <h4 class="box-title"><?php echo apply_filters('the_title', $module->post_title); ?></h4>
     <?php } ?>
-    <div class="box-content gutter">
+    <div class="box-content">
         <?php echo apply_filters('the_content', $module->post_content); ?>
     </div>
 </div>
