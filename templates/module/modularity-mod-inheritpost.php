7972bf15fb4026657d52763883de2c3be3d3e2d2
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-08 08:56:51 +0200

    Do not display unpublished post in inheritpost module

--- templates/module/modularity-mod-inheritpost.php
+++ templates/module/modularity-mod-inheritpost.php
@@ -1,5 +1,9 @@
-<?php $inherit = get_field('page', $module->ID); ?>
+<?php
+    $inherit = get_field('page', $module->ID);
+    if ($inherit->post_status == 'publish') :
+?>
 <article>
     <h2><?php echo apply_filters('the_title', $inherit->post_title); ?></h2>
     <?php echo apply_filters('the_content', $inherit->post_content); ?>
 </article>
+<?php endif; ?>
