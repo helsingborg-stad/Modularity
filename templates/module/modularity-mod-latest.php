8c52566a9f91f4535f574486c0f0f3e5249e90a8
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-13 10:54:19 +0200

    Sort latest module by meta key

--- templates/module/modularity-mod-latest.php
+++ templates/module/modularity-mod-latest.php
@@ -1,37 +1,58 @@
 <?php
-    $fields = json_decode(json_encode(get_fields($module->ID)));
 
-    $sortBy = $fields->sorted_by ? $fields->sorted_by : 'date';
-    $order = $fields->order ? $fields->order : 'desc';
+$fields = json_decode(json_encode(get_fields($module->ID)));
 
-    $getPostsArgs = array(
-        'post_type' => $fields->post_type,
-        'posts_per_page' => $fields->number_of_posts,
-        'orderby' => $sortBy,
-        'order' => $order
+$metaQuery = false;
+$sortBy = $fields->sorted_by ? $fields->sorted_by : 'date';
+$order = $fields->order ? $fields->order : 'desc';
+
+if (strpos($sortBy, '_metakey_') > -1) {
+    $orderby = str_replace('_metakey_', '', $sortBy);
+    $metaQuery = array(
+        'relation' => 'OR',
+        array(
+            'key' => $orderby,
+            'compare' => 'EXISTS'
+        ),
+        array(
+            'key' => $orderby,
+            'compare' => 'NOT EXISTS'
+        )
     );
 
-    if ($fields->taxonomy_filter === true) {
-        $taxType = $fields->filter_posts_taxonomy_type;
-        $taxValues = (array) $fields->filter_posts_by_tag;
+    $sortBy = 'meta_key';
+}
 
-        foreach ($taxValues as $term) {
-            $getPostsArgs['tax_query'][] = array(
-                'taxonomy' => $taxType,
-                'field'    => 'name',
-                'terms'    => $term
-            );
-        }
-    }
+$getPostsArgs = array(
+    'post_type' => $fields->post_type,
+    'posts_per_page' => $fields->number_of_posts,
+    'orderby' => $sortBy,
+    'order' => $order
+);
 
-    $posts = get_posts($getPostsArgs);
+if ($fields->taxonomy_filter === true) {
+    $taxType = $fields->filter_posts_taxonomy_type;
+    $taxValues = (array) $fields->filter_posts_by_tag;
 
-    if (isset($fields->view_as) && $fields->view_as == 'list') {
-        include 'modularity-mod-latest-list.php';
+    foreach ($taxValues as $term) {
+        $getPostsArgs['tax_query'][] = array(
+            'taxonomy' => $taxType,
+            'field'    => 'name',
+            'terms'    => $term
+        );
     }
-    elseif (isset($fields->view_as) && $fields->view_as == 'news') {
-        include 'modularity-mod-latest-news.php';
-    } else {
-        include 'modularity-mod-latest-item.php';
-    }
-?>
+}
+
+if ($metaQuery) {
+    $getPostsArgs['meta_query'] = $metaQuery;
+}
+
+$posts = get_posts($getPostsArgs);
+
+if (isset($fields->view_as) && $fields->view_as == 'list') {
+    include 'modularity-mod-latest-list.php';
+} elseif (isset($fields->view_as) && $fields->view_as == 'news') {
+    include 'modularity-mod-latest-news.php';
+} else {
+    include 'modularity-mod-latest-item.php';
+}
