d5256344e303e0126a583460e6bff4153659ced0
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-13 10:25:24 +0200

    Method to get metakeys for a post type

--- source/php/Helper/Post.php
+++ source/php/Helper/Post.php
@@ -5,6 +5,30 @@ namespace Modularity\Helper;
 class Post
 {
     /**
+     * Lists all meta-keys existing for the given posttype
+     *
+     * Attention: Since this method is using the database to get the
+     * metadata keys there need to be posts in the posttype to get results
+     *
+     * @param  string $posttype The posttype
+     * @return array            Meta keys as array
+     */
+    public static function getPosttypeMetaKeys($posttype)
+    {
+        global $wpdb;
+        $metaKeys = $wpdb->get_results("
+            SELECT DISTINCT {$wpdb->postmeta}.meta_key
+            FROM {$wpdb->postmeta}
+            LEFT JOIN wp_posts ON {$wpdb->postmeta}.post_id = {$wpdb->posts}.ID
+            WHERE
+                {$wpdb->posts}.post_type = '$posttype'
+                AND NOT LEFT({$wpdb->postmeta}.meta_key, 1) = '_'
+        ");
+
+        return $metaKeys;
+    }
+
+    /**
      * Gets the post template of the current editor page
      * @return string Template slug
      */
