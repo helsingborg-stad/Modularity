fb603dc23e396769345cb38deb8a557e7a5255b9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-26 10:46:55 +0200

    Posts module templates

--- templates/module/modularity-mod-posts.php
+++ templates/module/modularity-mod-posts.php
@@ -1,4 +1,21 @@
 <?php
-    $posts = \Modularity\Module\Posts\Posts::getPosts($module);
-    var_dump(count($posts));
-?>
+$fields = json_decode(json_encode(get_fields($module->ID)));
+$posts = \Modularity\Module\Posts\Posts::getPosts($module);
+
+switch ($fields->posts_display_as) {
+    case 'list':
+        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-list', 'module/modularity-mod-posts', false);
+        break;
+
+    case 'news':
+        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-news', 'module/modularity-mod-posts', false);
+        break;
+
+    case 'items':
+        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-items', 'module/modularity-mod-posts', false);
+        break;
+
+    case 'index':
+        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-index', 'module/modularity-mod-posts', false);
+        break;
+}
---
+++ templates/module/modularity-mod-posts/modularity-mod-posts-news.php
@@ -0,0 +1,47 @@
+<div class="grid">
+<?php
+$hasImages = false;
+foreach ($posts as $post) {
+    if (get_thumbnail_source($post->ID) !== false) {
+        $hasImages = true;
+    }
+}
+?>
+
+<?php foreach ($posts as $post) :  ?>
+    <?php
+        $image = wp_get_attachment_image_src(
+            get_post_thumbnail_id($post->ID),
+            apply_filters('modularity/image/latest/box',
+                array(400, 300),
+                $args
+            )
+        );
+    ?>
+    <div class="grid-lg-12">
+        <a href="<?php echo get_permalink($post->ID); ?>" class="<?php echo implode(' ', apply_filters('Modularity/Module/Classes', array('box', 'box-news', 'box-news-horizontal'), $module->post_type, $args)); ?>">
+            <?php if ($hasImages) : ?>
+                <div class="box-image-container">
+                    <?php if ($image && in_array('image', $fields->posts_fields)) : ?>
+                    <img src="<?php echo $image[0]; ?>" alt="<?php echo $post->post_title; ?>">
+                    <?php else : ?>
+                    <figure class="image-placeholder"></figure>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+            <div class="box-content">
+                <?php if (in_array('title', $fields->posts_fields)) : ?>
+                <h3 class="text-highlight"><?php echo apply_filters('the_title', $post->post_title); ?></h3>
+                <?php endif; ?>
+
+
+                <?php if (in_array('excerpt', $fields->posts_fields)) : ?>
+                <?php echo isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', wp_trim_words(wp_strip_all_tags(get_extended($post->post_content)['main']), 30, null)) : ''; ?>
+                <?php endif; ?>
+
+                <p><span class="link-item"><?php _e('Read more', 'modularity'); ?></span></p>
+            </div>
+        </a>
+    </div>
+<?php endforeach; ?>
+</div>
