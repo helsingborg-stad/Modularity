4c100cf0741f0bcbc624df1c414a7fcddfe9c08d
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-05-10 14:51:48 +0200

    New wp-widget module (not done).

--- templates/module/modularity-mod-wpwidget.php
+++ templates/module/modularity-mod-wpwidget.php
@@ -1,10 +1,12 @@
 <?php
 
     //Get settings for widget
-    $settings_array =   Modularity\Module\WpWidget\WpWidget::createSettingsArray(get_field('mod_standard_widget_type', $module->ID), $module->ID);
+    $settings = Modularity\Module\WpWidget\WpWidget::createSettingsArray(get_field('mod_standard_widget_type', $module->ID), $module->ID);
+
+    var_dump($settings);
 
     //Show widget with these settings
     Modularity\Module\WpWidget\WpWidget::displayWidget(
         get_field('mod_standard_widget_type', $module->ID),
-        $settings_array
+        $settings
     );
