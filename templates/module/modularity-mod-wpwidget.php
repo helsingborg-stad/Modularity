c0ca77f61af972ca30a13cfa70bfb6b018861408
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-10 16:17:20 +0200

    Remove unwanted var_dump

--- templates/module/modularity-mod-wpwidget.php
+++ templates/module/modularity-mod-wpwidget.php
@@ -3,8 +3,6 @@
     //Get settings for widget
     $settings = Modularity\Module\WpWidget\WpWidget::createSettingsArray(get_field('mod_standard_widget_type', $module->ID), $module->ID);
 
-    var_dump($settings);
-
     //Show widget with these settings
     Modularity\Module\WpWidget\WpWidget::displayWidget(
         get_field('mod_standard_widget_type', $module->ID),
