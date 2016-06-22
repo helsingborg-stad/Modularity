627c5e11d33e2e729613f539d4901538bb01b77b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-22 15:29:14 +0200

    Show title and descirption of files in the file module

--- templates/module/modularity-mod-files.php
+++ templates/module/modularity-mod-files.php
@@ -7,10 +7,16 @@
 
     <ul class="files">
         <?php foreach ($files as $file) : ?>
-            <li><a target="_blank" class="link-item" href="<?php echo $file['url']; ?>" title="<?php echo $file['title']; ?>">
-                <?php echo $file['filename']; ?>
-                (<?php echo size_format(filesize(get_attached_file($file['ID'])), 2); ?>)
-            </a></li>
+            <li>
+                <a target="_blank" class="link-item" href="<?php echo $file['url']; ?>" title="<?php echo $file['title']; ?>">
+                    <?php echo $file['title']; ?>
+                    (<?php echo pathinfo($file['url'], PATHINFO_EXTENSION); ?>, <?php echo size_format(filesize(get_attached_file($file['ID'])), 2); ?>)
+                </a>
+
+                <?php if (isset($file['description']) && !empty($file['description'])) : ?>
+                    <?php echo wpautop($file['description']); ?>
+                <?php endif; ?>
+            </li>
         <?php endforeach; ?>
     </ul>
 </div>
