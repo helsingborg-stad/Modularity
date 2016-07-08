8e83e57c0e40223dd1b0b9ce505a5078eb2ea688
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-07-08 09:07:33 +0200

    Add box wrappers to script and iframe module templates

--- templates/module/modularity-mod-iframe.php
+++ templates/module/modularity-mod-iframe.php
@@ -1,1 +1,3 @@
-<iframe src="<?php echo get_field('iframe_url', $module->ID); ?>" frameborder="0" style="width: 100%; height: <?php echo get_field('iframe_height', $module->ID); ?>px;"></iframe>
+<div class="box no-padding">
+    <iframe src="<?php echo get_field('iframe_url', $module->ID); ?>" frameborder="0" style="width: 100%; height: <?php echo get_field('iframe_height', $module->ID); ?>px;"></iframe>
+</div>
--- templates/module/modularity-mod-script.php
+++ templates/module/modularity-mod-script.php
@@ -1,1 +1,3 @@
-<?php echo get_post_meta($module->ID, 'embed_code', true);
+<div class="box no-padding">
+    <?php echo get_post_meta($module->ID, 'embed_code', true); ?>
+</div>
