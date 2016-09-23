9d476f46f491b22fb92521144d4e5ab18c905b92
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-23 14:03:05 +0200

    Notice title

--- templates/module/modularity-mod-notice.php
+++ templates/module/modularity-mod-notice.php
@@ -1,12 +1,14 @@
 <?php $fields = get_fields($module->ID); ?>
 <div class="notice <?php echo $fields['notice_type']; ?> <?php echo $fields['notice_size']; ?>">
-
     <div class="grid grid-table-md grid-va-middle no-margin no-padding">
         <?php if (!empty($fields['notice_icon'])) : ?>
         <div class="grid-fit-content"><i class="fa <?php echo $fields['notice_icon']; ?>"></i></div>
         <?php endif; ?>
 
         <div class="grid-auto">
+            <?php if (!$module->hideTitle) : ?>
+                <strong><?php echo $module->post_title; ?></strong>
+            <?php endif; ?>
             <?php echo $fields['notice_text']; ?>
         </div>
     </div>
