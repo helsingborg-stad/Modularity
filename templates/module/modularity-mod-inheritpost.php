cdba5f6d336af86eed188476eef30fd3a0575eaf
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-23 13:59:51 +0200

    Inheritpost titel

--- templates/module/modularity-mod-inheritpost.php
+++ templates/module/modularity-mod-inheritpost.php
@@ -3,6 +3,10 @@
     if ($inherit->post_status == 'publish') :
 ?>
 <article>
+    <?php if (!$module->hideTitle) : ?>
+        <h1><?php echo $module->post_title; ?></h1>
+    <?php endif; ?>
+
     <h2><?php echo apply_filters('the_title', $inherit->post_title); ?></h2>
     <?php echo apply_filters('the_content', $inherit->post_content); ?>
 </article>
