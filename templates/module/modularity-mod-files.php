6d5967538329f0c510b38a701d8a673ba4399320
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-23 13:55:32 +0200

    mod-fiels hide title option

--- templates/module/modularity-mod-files.php
+++ templates/module/modularity-mod-files.php
@@ -1,7 +1,7 @@
 <?php $files = get_field('files', $module->ID); ?>
 
 <div class="<?php echo implode(' ', apply_filters('Modularity/Module/Classes', array('box', 'box-panel'), $module->post_type, $args)); ?>">
-    <?php if (!empty($module->post_title)) { ?>
+    <?php if (!$module->hideTitle && !empty($module->post_title)) { ?>
         <h4 class="box-title"><?php echo apply_filters('the_title', $module->post_title); ?></h4>
     <?php } ?>
 
