2b320a58dfecff30ad50fdade1397cc5985bb4a0
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-10-19 10:27:46 +0200

    Notice with container

--- templates/module/modularity-mod-notice.php
+++ templates/module/modularity-mod-notice.php
@@ -1,15 +1,17 @@
 <?php $fields = get_fields($module->ID); ?>
 <div class="notice <?php echo $fields['notice_type']; ?> <?php echo $fields['notice_size']; ?>">
-    <div class="grid grid-table-md grid-va-middle no-margin no-padding">
-        <?php if (!empty($fields['notice_icon'])) : ?>
-        <div class="grid-fit-content"><i class="fa <?php echo $fields['notice_icon']; ?>"></i></div>
-        <?php endif; ?>
-
-        <div class="grid-auto">
-            <?php if (!$module->hideTitle) : ?>
-                <strong><?php echo $module->post_title; ?></strong>
+    <div class="container">
+        <div class="grid-table grid-va-middle">
+            <?php if (!empty($fields['notice_icon'])) : ?>
+            <div class="grid-fit-content" style="padding-right:10px;"><i class="fa <?php echo $fields['notice_icon']; ?>"></i></div>
             <?php endif; ?>
-            <?php echo $fields['notice_text']; ?>
+
+            <div class="grid-auto">
+                <?php if (!$module->hideTitle) : ?>
+                    <strong><?php echo $module->post_title; ?></strong>
+                <?php endif; ?>
+                <?php echo $fields['notice_text']; ?>
+            </div>
         </div>
     </div>
 </div>
