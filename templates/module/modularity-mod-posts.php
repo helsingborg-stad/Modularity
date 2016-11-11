d77d508a68851e66d735bc416d70cb9d429a87f6
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-11 08:50:23 +0100

    Posts module template filter

--- templates/module/modularity-mod-posts.php
+++ templates/module/modularity-mod-posts.php
@@ -28,24 +28,27 @@ if (isset($fields->posts_taxonomy_type) && $fields->posts_taxonomy_type) {
     $filters['term[]'] = $taxType . '|' . $taxValues;
 }
 
-switch (apply_filters('Modularity/Module/Posts/template', $fields->posts_display_as, $module, $fields)) {
+switch ($fields->posts_display_as) {
     case 'list':
-        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-list', 'module/modularity-mod-posts', false);
+        $template = \Modularity\Helper\Wp::getTemplate($module->post_type . '-list', 'module/modularity-mod-posts', false);
         break;
 
     case 'news':
-        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-news', 'module/modularity-mod-posts', false);
+        $template = \Modularity\Helper\Wp::getTemplate($module->post_type . '-news', 'module/modularity-mod-posts', false);
         break;
 
     case 'items':
-        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-items', 'module/modularity-mod-posts', false);
+        $template = \Modularity\Helper\Wp::getTemplate($module->post_type . '-items', 'module/modularity-mod-posts', false);
         break;
 
     case 'index':
-        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-index', 'module/modularity-mod-posts', false);
+        $template = \Modularity\Helper\Wp::getTemplate($module->post_type . '-index', 'module/modularity-mod-posts', false);
         break;
 
     case 'expandable-list':
-        include \Modularity\Helper\Wp::getTemplate($module->post_type . '-expandable-list', 'module/modularity-mod-posts', false);
+        $template = \Modularity\Helper\Wp::getTemplate($module->post_type . '-expandable-list', 'module/modularity-mod-posts', false);
         break;
 }
+
+$template = apply_filters('Modularity/Module/Posts/template', $template, $module, $fields);
+include $template;
