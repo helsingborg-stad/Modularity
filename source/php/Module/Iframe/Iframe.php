2f792fcc690e756e6444baff89fd80a666205187
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-23 13:30:13 +0100

    Added SSL notice to iframe.

--- source/php/Module/Iframe/Iframe.php
+++ source/php/Module/Iframe/Iframe.php
@@ -17,5 +17,21 @@ class Iframe extends \Modularity\Module
             3600*24*7,
             true
         );
+
+        add_filter('acf/load_field/name=iframe_url',array($this,'sslNotice'));
+    }
+
+    public function sslNotice($field) {
+        if (is_ssl() || $this->isUsingSSLProxy()) {
+            $field['instructions'] = '<span style="color: #f00;">'.__("Your iframe link must start with http<strong>s</strong>://. Links without this prefix will not display.", 'modularity').'</span>';
+        }
+        return $field;
+    }
+
+    private function isUsingSSLProxy() {
+        if ((defined('SSL_PROXY') && SSL_PROXY === true)) {
+            return true;
+        }
+        return false;
     }
 }
