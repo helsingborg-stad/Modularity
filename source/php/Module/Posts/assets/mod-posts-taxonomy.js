8936961acb10fa0a2a51db54e05e126fcbe61542
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-23 13:07:21 +0100

    Posts module taxonomy filtering fix

--- source/php/Module/Posts/assets/mod-posts-taxonomy.js
+++ source/php/Module/Posts/assets/mod-posts-taxonomy.js
@@ -4,14 +4,14 @@ jQuery(document).ready(function ($) {
      * Posttype Meta keys
      */
     getPostMeta({
-        'action': 'get_sortable_meta_keys',
+        'action': 'get_sortable_meta_keys_v2',
         'posttype': $('#modularity-latest-post-type select').val(),
         'post': modularity_current_post_id
     });
 
     $('#modularity-latest-post-type select').on('change', function () {
         getPostMeta({
-            'action': 'get_sortable_meta_keys',
+            'action': 'get_sortable_meta_keys_v2',
             'posttype': $(this).val(),
             'post': modularity_current_post_id
         });
@@ -49,14 +49,14 @@ jQuery(document).ready(function ($) {
      * Taxonomy type update
      */
     getTaxonomyTypes({
-        'action': 'get_taxonomy_types',
+        'action': 'get_taxonomy_types_v2',
         'posttype': $('#modularity-latest-post-type select').val(),
         'post': modularity_current_post_id
     });
 
     $('#modularity-latest-post-type select').on('change', function () {
         getTaxonomyTypes({
-            'action': 'get_taxonomy_types',
+            'action': 'get_taxonomy_types_v2',
             'posttype': $(this).val(),
             'post': modularity_current_post_id
         });
@@ -80,7 +80,7 @@ jQuery(document).ready(function ($) {
             $('#modularity-latest-taxonomy .acf-label label .spinner').remove();
 
             getTaxonomyValues({
-                'action': 'get_taxonomy_values',
+                'action': 'get_taxonomy_values_v2',
                 'tax': $('#modularity-latest-taxonomy select').val(),
                 'post': modularity_current_post_id
             });
@@ -92,7 +92,7 @@ jQuery(document).ready(function ($) {
      */
     $('#modularity-latest-taxonomy select').on('change', function () {
         getTaxonomyValues({
-            'action': 'get_taxonomy_values',
+            'action': 'get_taxonomy_values_v2',
             'tax': $(this).val(),
             'post': modularity_current_post_id
         });
@@ -104,7 +104,7 @@ jQuery(document).ready(function ($) {
 
         $.post(ajaxurl, data, function (response) {
             $.each(response.tax, function (index, item) {
-                var is_selected = ($.inArray(item.slug, response.curr) > -1) ? 'selected' : '';
+                var is_selected = (item.slug == response.curr) ? 'selected' : '';
                 $('#modularity-latest-taxonomy-value select').append('<option value="' + item.slug + '" ' + is_selected + '>' + item.name + '</option>');
             });
 
