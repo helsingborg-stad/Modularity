b35f0ae1e123b52c02477ee29b08de0cf2d6399a
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-19 11:15:09 +0100

    Fixes loading indicator for editor modal

--- dist/js/modularity.dev.js
+++ dist/js/modularity.dev.js
@@ -657,8 +657,8 @@ Modularity.Prompt.Modal = (function ($) {
             <div id="modularity-modal">\
                 <div class="modularity-modal-wrapper">\
                     <button class="modularity-modal-close" data-modularity-modal-action="close">&times; ' + modularityAdminLanguage.close + '</button>\
-                    <span id="modularity-iframe-loader" class="spinner modularity-spinner-center" style="visibility:visible;"></span>\
-                    <iframe class="modularity-modal-iframe" src="' + url + '" frameborder="0" onload="document.getElementById(\'modularity-iframe-loader\').style.visibility=\'hidden\';" allowtransparency></iframe>\
+                    <div class="modularity-modal-spinner-container" id="modularity-iframe-loader"><span class="modularity-modal-spinner"></span></div>\
+                    <iframe class="modularity-modal-iframe" src="' + url + '" frameborder="0" onload="document.getElementById(\'modularity-iframe-loader\').style.display=\'none\';" allowtransparency></iframe>\
                 </div>\
             </div>\
         ');
