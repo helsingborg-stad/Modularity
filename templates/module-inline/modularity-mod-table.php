ec84482ae04404caff7889c3b1b2671d0b8b16c6
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-21 12:05:11 +0100

    Inline table module disable sorting fix

--- templates/module-inline/modularity-mod-table.php
+++ templates/module-inline/modularity-mod-table.php
@@ -9,11 +9,13 @@ if (isset($fields->mod_table_classes) && is_array($fields->mod_table_classes)) {
 echo str_replace(
     '<table class="',
     sprintf(
-        '<table data-paging="%2$s" data-page-length="%3$s" data-searching="%4$s" class="datatable %1$s ',
+        '<table data-paging="%2$s" data-page-length="%3$s" data-searching="%4$s" data-ordering="%6$s" data-info="%5$s" class="datatable %1$s ',
         $classes,
         $fields->mod_table_pagination,
         $fields->mod_table_pagination_count,
-        $fields->mod_table_search
+        $fields->mod_table_search,
+        $fields->mod_table_pagination || $fields->mod_table_search,
+        isset($fields->mod_table_ordering) ? $fields->mod_table_ordering : true
     ),
     $fields->mod_table
 );
