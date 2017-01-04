40402994f42252f60b4bd04ca9850cca78256246
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-04 13:32:23 +0100

    Get posttype from wp_query instead

--- source/php/Helper/Wp.php
+++ source/php/Helper/Wp.php
@@ -197,7 +197,7 @@ class Wp
             if (is_home()) {
                 return 'archive-post';
             } elseif (is_post_type_archive() && is_search()) {
-                return 'archive-' . get_post_type_object(get_post_type())->rewrite['slug'];
+                return 'archive-' . get_post_type_object($wp_query->query_vars['post_type'])->rewrite['slug'];
             } elseif (is_search()) {
                 return 'search';
             } elseif (is_404()) {
@@ -206,7 +206,7 @@ class Wp
                 return 'author';
             }
 
-            return 'archive-' . get_post_type_object(get_post_type())->rewrite['slug'];
+            return 'archive-' . get_post_type_object($wp_query->query_vars['post_type'])->rewrite['slug'];
         }
 
         return false;
