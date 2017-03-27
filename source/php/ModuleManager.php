7a0472c7a245e16c545659afbc83b706645b1909
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-27 11:01:44 +0200

    Add metaboxes to module post page

--- source/php/ModuleManager.php
+++ source/php/ModuleManager.php
@@ -51,6 +51,20 @@ class ModuleManager
         self::$enabled = self::getEnabled();
         self::$registered = $this->getRegistered(false);
         $this->init();
+
+        // Hide title option
+        add_action('edit_form_before_permalink', array($this, 'hideTitleCheckbox'));
+        add_action('save_post', array($this, 'saveHideTitleCheckbox'), 10, 2);
+
+        // Shortcode metabox
+        add_action('add_meta_boxes', array($this, 'shortcodeMetabox'));
+
+        // Add usage metabox
+        add_action('add_meta_boxes', array($this, 'whereUsedMetaBox'));
+
+        // Description meta box
+        add_action('add_meta_boxes', array($this, 'descriptionMetabox'), 5);
+        add_action('save_post', array($this, 'descriptionMetaboxSave'));
     }
 
     /**
@@ -273,5 +287,208 @@ class ModuleManager
         }
 
         return false;
+    }
+
+    /**
+     * Adds checkbox to post edit page to hide title
+     * @return void
+     */
+    public function hideTitleCheckbox()
+    {
+        global $post;
+
+        if (substr($post->post_type, 0, 4) != 'mod-') {
+            return;
+        }
+
+        $current = self::$moduleSettings[$post->post_type]['hide_title'];
+
+        if (strlen(get_post_meta($post->ID, 'modularity-module-hide-title', true)) > 0) {
+            $current = boolval(get_post_meta($post->ID, 'modularity-module-hide-title', true));
+        }
+
+        $checked = checked(true, $current, false);
+
+        echo '<div>
+            <label style="cursor:pointer;">
+                <input type="checkbox" name="modularity-module-hide-title" value="1" ' . $checked . '>
+                ' . __('Hide title', 'modularity') . '
+            </label>
+        </div>';
+    }
+
+    /**
+     * Saves the hide title checkboc
+     * @param  int $postId
+     * @param  WP_Post $post
+     * @return void
+     */
+    public function saveHideTitleCheckbox($postId, $post)
+    {
+        if (substr($post->post_type, 0, 4) != 'mod-') {
+            return;
+        }
+
+        if (!isset($_POST['modularity-module-hide-title'])) {
+            update_post_meta($post->ID, 'modularity-module-hide-title', 0);
+            return;
+        }
+
+        update_post_meta($post->ID, 'modularity-module-hide-title', 1);
+        return;
+    }
+
+    /**
+     * Shortcode metabox
+     * @return void
+     */
+    public function shortcodeMetabox()
+    {
+        add_meta_box('modularity-shortcode', 'Modularity Shortcode', function () {
+            global $post;
+            echo '<p>';
+            echo __('Copy and paste this shortcode to display the module inline.', 'modularity');
+            echo '</p><p>';
+            echo '<label><input type="checkbox" class="modularity-inline-template" checked> Use inline template</label>';
+            echo '<textarea style="margin-top:10px; overflow: hidden;width: 100%;height:30px;background:#f9f9f9;border:1px solid #ddd;padding:5px;">[modularity id="' . $post->ID . '"]</textarea>';
+            echo '</p>';
+
+            echo "<script>
+                jQuery(document).ready(function ($) {
+                    $('.modularity-inline-template').prop('checked', true);
+
+                    $('.modularity-inline-template').on('change', function () {
+                        if ($(this).prop('checked') == false) {
+                            $(this).parents('.inside').find('pre span').text(' inline=\"false\"');
+                        } else {
+                            $(this).parents('.inside').find('pre span').text('');
+                        }
+                    });
+                });
+            </script>";
+        }, self::$enabled, 'side', 'default');
+    }
+
+    /**
+     * Metabox that shows where the module is used
+     * @return void
+     */
+    public function whereUsedMetaBox()
+    {
+        global $post;
+        $module = $this;
+
+        if (is_null($post)) {
+            return;
+        }
+
+        $usage = self::getModuleUsage($post->ID);
+
+        add_meta_box('modularity-usage', 'Module usage', function () use ($module, $usage) {
+            if (count($usage) == 0) {
+                echo '<p>' . __('This modules is not used yet.', 'modularity')  . '</p>';
+                return;
+            }
+
+            echo '<p>' . __('This module is used on the following places:', 'modularity') . '</p><p><ul class="modularity-usage-list">';
+
+            foreach ($usage as $page) {
+                echo '<li><a href="' . get_permalink($page->post_id) . '">' . $page->post_title . '</a></li>';
+            }
+
+            echo '</ul></p>';
+        }, self::$enabled, 'side', 'default');
+    }
+
+    /**
+     * Search database for where the module is used
+     * @param  integer $id Module id
+     * @return array       List of pages where the module is used
+     */
+    public static function getModuleUsage($id, $limit = false)
+    {
+        global $wpdb;
+
+        // Normal modules
+        $query = "
+            SELECT
+                {$wpdb->postmeta}.post_id,
+                {$wpdb->posts}.post_title,
+                {$wpdb->posts}.post_type
+            FROM {$wpdb->postmeta}
+            LEFT JOIN
+                {$wpdb->posts} ON ({$wpdb->postmeta}.post_id = {$wpdb->posts}.ID)
+            WHERE
+                {$wpdb->postmeta}.meta_key = 'modularity-modules'
+                AND ({$wpdb->postmeta}.meta_value REGEXP '.*\"postid\";s:[0-9]+:\"{$id}\".*')
+                AND {$wpdb->posts}.post_type != 'revision'
+            ORDER BY {$wpdb->posts}.post_title ASC
+        ";
+
+        $modules = $wpdb->get_results($query, OBJECT);
+
+        // Shortcode modules
+        $query = "
+            SELECT
+                {$wpdb->posts}.ID AS post_id,
+                {$wpdb->posts}.post_title,
+                {$wpdb->posts}.post_type
+            FROM {$wpdb->posts}
+            WHERE
+                ({$wpdb->posts}.post_content REGEXP '([\[]modularity.*id=\"{$id}\".*[\]])')
+                AND {$wpdb->posts}.post_type != 'revision'
+            ORDER BY {$wpdb->posts}.post_title ASC
+        ";
+
+        $shortcodes = $wpdb->get_results($query, OBJECT);
+
+        $result = array_merge($modules, $shortcodes);
+
+        if (is_numeric($limit)) {
+            if (count($result) > $limit) {
+                $sliced = array_slice($result, $limit);
+            } else {
+                $sliced = $result;
+            }
+
+            return (object) array(
+                'data' => $sliced,
+                'more' => (count($result) > 0 && count($sliced) > 0) ? count($result) - count($sliced) : 0
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Description metabox content
+     * @return void
+     */
+    public function descriptionMetabox()
+    {
+        add_meta_box(
+            'modularity-description',
+            'Module description',
+            function () {
+                $description = get_post_meta(get_the_id(), 'module-description', true);
+                include MODULARITY_TEMPLATE_PATH . 'editor/modularity-module-description.php';
+            },
+            self::$enabled,
+            'normal',
+            'high'
+        );
+    }
+
+    /**
+     * Saves the description
+     * @return void
+     */
+    public function descriptionMetaboxSave()
+    {
+        if (!isset($_POST['modularity-module-description'])) {
+            return;
+        }
+
+        update_post_meta(intval($_POST['post_ID']), 'module-description', trim($_POST['modularity-module-description']));
     }
 }
