1ef1cd408bf7aef0ac1fd3806beac6e24676d2ad
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-27 09:21:51 +0200

    ModuleManager to get available/registered modules

--- source/php/ModuleManager.php
+++ source/php/ModuleManager.php
@@ -30,7 +30,7 @@ class ModuleManager
 
     public function __construct()
     {
-        $this->getAvailable();
+        self::$available = $this->getAvailable(false);
         $this->initModules();
     }
 
@@ -38,15 +38,41 @@ class ModuleManager
      * Get available modules (WP filter)
      * @return array
      */
-    public function getAvailable()
+    public function getAvailable($getBundled = true)
     {
+        if ($getBundled) {
+            $bundeled = $this->getBundeled();
+            self::$available = array_merge(self::$available, $bundeled);
+        }
+
         return apply_filters('Modularity/Modules', self::$available);
     }
 
+    /**
+     * Gets bundeled modules
+     * @return array
+     */
+    public function getBundeled() : array
+    {
+        $directory = MODULARITY_PATH . 'source/php/Module/';
+        $bundeled = array();
+
+        foreach (@glob($directory . "*", GLOB_ONLYDIR) as $folder) {
+            $bundeled[$folder] = basename($folder);
+        }
+
+        return $bundeled;
+    }
+
+    /**
+     * Initializes modules
+     * @return void
+     */
     public function initModules()
     {
         foreach (self::$available as $path => $module) {
-
+            var_dump($path, $module);
+            exit;
         }
     }
 }
