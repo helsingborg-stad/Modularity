299436faa1851d00ca33b69ba20166176e2bf1db
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-27 09:21:29 +0200

    Module base class

--- source/php/Module.php
+++ source/php/Module.php
@@ -4,5 +4,13 @@ namespace Modularity;
 
 abstract class Module
 {
-
+    public static $slug = '';
+    public static $nameSingular = '';
+    public static $namePlural = '';
+    public static $description = '';
+    public static $icon = '';
+    public static $supports = array();
+    public static $plugin = array();
+    public static $cacheTtl = 0;
+    public static $hideTitle  = false;
 }
