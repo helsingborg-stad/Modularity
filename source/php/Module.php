bc4aeb3d0855832cbd27cbf8cc5f998fbf5eb661
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-27 10:28:58 +0200

    Added comments and examples to properties of the module base class

--- source/php/Module.php
+++ source/php/Module.php
@@ -4,13 +4,68 @@ namespace Modularity;
 
 abstract class Module
 {
+    /**
+     * The slug of the module
+     * Example: image
+     * @var string
+     */
     public static $slug = '';
+
+    /**
+     * Singular name of the modue
+     * Example: Image
+     * @var string
+     */
     public static $nameSingular = '';
+
+    /**
+     * Plural name of the module
+     * Example: Images
+     * @var string
+     */
     public static $namePlural = '';
+
+    /**
+     * Module description
+     * Shows a fixed with and height image
+     * @var string
+     */
     public static $description = '';
+
+    /**
+     * Module icon (Base64 endoced data uri)
+     * @var string
+     */
     public static $icon = '';
+
+    /**
+     * What the module post type should support (title and revision will be added automatically)
+     * Example: array('editor', 'attributes')
+     * @var array
+     */
     public static $supports = array();
+
+    /**
+     * Any module plugins (path to file to include)
+     * @var array
+     */
     public static $plugin = array();
+
+    /**
+     * Cache ttl
+     * @var integer
+     */
     public static $cacheTtl = 0;
+
+    /**
+     * The initial setting for "hide title" of the module
+     * @var boolean
+     */
     public static $hideTitle  = false;
+
+    /**
+     * Is the module deprecated?
+     * @var boolean
+     */
+    public static $isDeprecated = false;
 }
