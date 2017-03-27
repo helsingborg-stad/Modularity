852a42fb7a5871c14993044d76ef3d2acca41ffd
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-27 09:22:10 +0200

    Helper function to get namespace from php-file

---
+++ source/php/Helper/File.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Modularity\Helper;
+
+class File
+{
+    /**
+     * Get PHP namespace from file
+     * @param  string $source Path to file
+     * @return string         Namespace
+     */
+    public static function getNamespace(string $source) : string
+    {
+        $tokens = token_get_all($src);
+        $count = count($tokens);
+        $i = 0;
+        $namespace = '';
+        $namespaceFound = false;
+
+        while ($i < $count) {
+            $token = $tokens[$i];
+
+            if (is_array($token) && $token[0] === T_NAMESPACE) {
+                // Found namespace declaration
+                while (++$i < $count) {
+                    if ($tokens[$i] === ';') {
+                        $namespaceFound = true;
+                        $namespace = trim($namespace);
+                        break;
+                    }
+
+                    $namespace .= is_array($tokens[$i]) ? $tokens[$i][1] : $tokens[$i];
+                }
+
+                break;
+            }
+
+            $i++;
+        }
+
+        if (!$namespaceFound) {
+            return null;
+        }
+
+        return $namespace;
+    }
+}
