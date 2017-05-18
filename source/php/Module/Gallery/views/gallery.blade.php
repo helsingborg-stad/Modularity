3761d037d68be205b65ba946cf143dfa64af6875
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-18 10:00:31 +0200

    Gallery module v2

---
+++ source/php/Module/Gallery/views/gallery.blade.php
@@ -0,0 +1,21 @@
+<div>
+    @if (!$hideTitle && !empty($post_title))
+    <h2>{!! apply_filters('the_title', $post_title) !!}</h2>
+    @endif
+
+    <ul class="image-gallery grid grid-gallery">
+        @if (isset($mod_gallery_images))
+        @foreach ($mod_gallery_images as $image)
+        <li class="grid-md-3">
+            <a class="{{ $classes }} lightbox-trigger" href="{{ $image['sizes']['large'] }}" {{ (isset($image['caption']) && !empty($image['caption']) && !in_array(strtolower($image['caption']), array('caption text'))) ? 'data-caption="' . $image['caption'] . '"' : '' }}>
+                @if (isset($image['thumbnail']) && $image['thumbnail'])
+                <img src="{{ $image['thumbnail'][0] }}" alt="{{ $image['alt'] }}">
+                @else
+                <figure class="image-placeholder"></figure>
+                @endif
+            </a>
+         </li>
+         @endforeach
+         @endif
+    </ul>
+</div>
