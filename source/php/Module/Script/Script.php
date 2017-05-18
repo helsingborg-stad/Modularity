d78e7f15c622b540ab105ffe5c50551756791309
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-18 12:30:10 +0200

    Script module v2

--- source/php/Module/Script/Script.php
+++ source/php/Module/Script/Script.php
@@ -4,17 +4,30 @@ namespace Modularity\Module\Script;
 
 class Script extends \Modularity\Module
 {
-    public function __construct()
+    public $slug = 'script';
+    public $supports = array();
+
+    public function init()
     {
-        $this->register(
-            'script',
-            __("Script", 'modularity'),
-            __("Script", 'modularity'),
-            __("Outputs unsanitized code to widget area.", 'modularity'),
-            array(),
-            null,
-            null,
-            3600*24*7
-        );
+        $this->nameSingular = __("Script", 'modularity');
+        $this->namePlural = __("Script", 'modularity');
+        $this->description = __("Outputs unsanitized code to widget area.", 'modularity');
     }
+
+    public function data() : array
+    {
+        $data = array();
+        $data['embed'] = get_post_meta($this->ID, 'embed_code', true);
+        return $data;
+    }
+
+    /**
+     * Available "magic" methods for modules:
+     * init()            What to do on initialization
+     * data()            Use to send data to view (return array)
+     * style()           Enqueue style only when module is used on page
+     * script            Enqueue script only when module is used on page
+     * adminEnqueue()    Enqueue scripts for the module edit/add page in admin
+     * template()        Return the view template (blade) the module should use when displayed
+     */
 }
