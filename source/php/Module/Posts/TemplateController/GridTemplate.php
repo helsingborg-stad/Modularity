340893195dd1930e5754e395bb7786607bc98100
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-19 08:56:00 +0200

    Posts module grid template

---
+++ source/php/Module/Posts/TemplateController/GridTemplate.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Modularity\Module\Posts\TemplateController;
+
+class GridTemplate
+{
+    protected $module;
+    protected $args;
+
+    public $data = array();
+
+    public function __construct(\Modularity\Module\Posts\Posts $module, array $args, $data)
+    {
+        $this->module = $module;
+        $this->args = $args;
+        $this->data = $data;
+
+        $fields = json_decode(json_encode(get_fields($this->module->ID)));
+
+        $this->data['posts_columns'] = $fields->posts_columns;
+        $this->data['classes'] = implode(' ', apply_filters('Modularity/Module/Classes', array('box', 'box-news'), $this->module->post_type, $this->args));
+
+        $this->data['gridSize'] = (int)str_replace('-', '', filter_var($fields->posts_columns, FILTER_SANITIZE_NUMBER_INT));
+        $this->data['column_width'] = 'grid-md-' . $this->data['gridSize'];
+        $this->data['column_height'] = false;
+
+        $this->preparePosts($fields);
+    }
+
+    public function preparePosts($fields)
+    {
+        $postNum = 0;
+        $gridRand = $this->getGridPattern($this->data['gridSize']);
+        $gridRow = array();
+
+        /* Image size */
+        $imageDimensions = array(1200, 900);
+
+        if (!$fields->posts_alter_columns) {
+            switch ($fields->posts_columns) {
+                case "grid-md-12":    //1-col
+                    $imageDimensions = array(1200, 900);
+                    break;
+
+                case "grid-md-6":    //2-col
+                    $imageDimensions = array(800, 600);
+                    break;
+
+                default:
+                    $imageDimensions = array(400, 300);
+            }
+        }
+
+        foreach ($this->data['posts'] as $post) {
+            $postNum++;
+
+            // Get altering grid size
+            if ($fields->posts_alter_columns) {
+                if (empty($gridRow)) {
+                    $gridRow = $gridRand;
+                }
+
+                if (empty($gridColumns)) {
+                    $gridColumns = $gridRow[0];
+                    array_shift($gridRow);
+                }
+
+                $columnSize = 'grid-md-' . $gridColumns[0];
+                array_shift($gridColumns);
+                $columnHeight = null;
+
+                switch ($this->data['gridSize']) {
+                    case 3:
+                        $columnHeight = '280px';
+                        break;
+
+                    case 4:
+                        $columnHeight = '400px';
+                        break;
+
+                    case 6:
+                        $columnHeight = '500px';
+                        break;
+
+                    case 12:
+                        $columnHeight = '500px';
+                        break;
+
+                    default:
+                        $columnHeight = false;
+                        break;
+                }
+
+                $post->column_width = $columnSize;
+                $post->column_height = $columnHeight;
+            }
+
+            /* Image */
+            $image = null;
+            if ($fields->posts_data_source !== 'input') {
+                $image = wp_get_attachment_image_src(
+                    get_post_thumbnail_id($post->ID),
+                    apply_filters(
+                        'modularity/image/posts/items',
+                        municipio_to_aspect_ratio('16:9', $imageDimensions),
+                        $this->args
+                    )
+                );
+            } else {
+                if ($post->image) {
+                    $image = wp_get_attachment_image_src(
+                        $post->image->ID,
+                        apply_filters(
+                            'modularity/image/posts/items',
+                            municipio_to_aspect_ratio('16:9', $imageDimensions),
+                            $this->args
+                        )
+                    );
+                }
+            }
+
+            $post->thumbnail = $image;
+            $post->extended = get_extended(wp_strip_all_tags(strip_shortcodes($post->post_content)));
+        }
+    }
+
+    public function getGridPattern($gridSize)
+    {
+        $gridRand = array();
+
+        switch ($gridSize) {
+            case 12:
+                $gridRand = array(
+                    array(12)
+                );
+                break;
+
+            case 6:
+                $gridRand = array(
+                    array(12),
+                    array(6, 6),
+                    array(6, 6)
+                );
+                break;
+
+            case 4:
+                $gridRand = array(
+                    array(8, 4),
+                    array(4, 4, 4),
+                    array(4, 8)
+                );
+                break;
+
+            case 3:
+                $gridRand = array(
+                    array(6, 3, 3),
+                    array(3, 3, 3, 3),
+                    array(3, 3, 6),
+                    array(3, 3, 3, 3),
+                    array(3, 6, 3)
+                );
+                break;
+
+            default:
+                $gridRand = array(
+                    array(12)
+                );
+                break;
+        }
+
+        return $gridRand;
+    }
+}
---
+++ source/php/Module/Posts/views/grid.blade.php
@@ -0,0 +1,54 @@
+<div class="grid">
+    @if (!$hideTitle && !empty($post_title))
+    <div class="grid-xs-12">
+        <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+    </div>
+    @endif
+
+    @if (count($posts) > 0)
+    @foreach ($posts as $post)
+        <div class="{{ isset($post->column_width) ? $post->column_width : $column_width }}">
+            <a href="<?php echo get_permalink($post->ID); ?>" class="box box-post-brick" {!! isset($post->column_height) && !empty($post->column_height) ? 'style="padding-bottom:0;height:' . $post->column_height . '"' : '' !!}>
+                @if (isset($post->thumbnail) && is_array($post->thumbnail))
+                <div class="box-image" style="background-image:url({{ $post->thumbnail[0] }});">
+                    <img src="{{ $post->thumbnail[0] }}" alt="{{ $post->post_title }}">
+                </div>
+                @endif
+
+                <div class="box-content">
+                    @if (in_array('date', $posts_fields) && $posts_data_source !== 'input')
+                    <span class="box-post-brick-date">
+                        <time>
+                            {{ get_the_time(get_option('date_format'), $post->ID) }}
+                            {{ get_the_time(get_option('time_format'), $post->ID) }}
+                        </time>
+                    </span>
+                    @endif
+
+                    @if (in_array('title', $posts_fields))
+                    <h3 class="post-title">{{ apply_filters('the_title', $post->post_title) }}</h3>
+                    @endif
+                </div>
+
+                @if (in_array('excerpt', $posts_fields))
+                <div class="box-post-brick-lead">
+                    {!! isset($extended['main']) && !empty($extended['main']) ? $extended['main'] : wp_trim_words(wp_strip_all_tags(strip_shortcodes($post->post_content)), 100, '') !!}
+                </div>
+                @endif
+            </a>
+        </div>
+    @endforeach
+    @else
+
+    <div class="grid-md-12">
+        <?php _e('No posts to show…', 'modularity'); ?>
+    </div>
+
+    @endif
+
+    @if ($posts_data_source !== 'input' && isset($archive_link) && $archive_link)
+    <div class="grid-lg-12">
+        <a class="read-more" href="{{ get_post_type_archive_link($posts_data_post_type) }}?{{ http_build_query($filters) }}"><?php _e('Show more', 'modularity'); ?></a>
+    </div>
+    @endif
+</div>
