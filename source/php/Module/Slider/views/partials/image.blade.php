a478804ad83de45dfe73bfe4c2ed2c56a1f1f772
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-05 14:23:00 +0200

    Move text block inside slider div

--- source/php/Module/Slider/views/partials/image.blade.php
+++ source/php/Module/Slider/views/partials/image.blade.php
@@ -1,12 +1,17 @@
 @if ($slide['image_use'] !== false)
-<div class="slider-image slider-image-desktop {{ apply_filters('Modularity/slider/desktop_image_hidden', 'hidden-xs hidden-sm') }}" style="background-image:url({{ ($slide['image_use'] !== false) ? $slide['image_use'][0] : '' }})"></div>
+<div class="slider-image slider-image-desktop {{ apply_filters('Modularity/slider/desktop_image_hidden', 'hidden-xs hidden-sm') }}" style="background-image:url({{ ($slide['image_use'] !== false) ? $slide['image_use'][0] : '' }})">
+	<!-- Text block -->
+	@if (isset($slide['activate_textblock']) && $slide['activate_textblock'])
+	@include('partials.textblock')'
+	@endif
+</div>
 @endif
 
 @if ($slide['mobile_image_use'] !== false)
-<div class="slider-image slider-image-mobile {{ apply_filters('Modularity/slider/mobile_image_hidden', 'hidden-md hidden-lg') }}" style="background-image:url({{ ($slide['mobile_image_use'] !== false) ? $slide['mobile_image_use'][0] : '' }})"></div>
-@endif
-
-<!-- Text block -->
-@if (isset($slide['activate_textblock']) && $slide['activate_textblock'])
-@include('partials.textblock')'
+<div class="slider-image slider-image-mobile {{ apply_filters('Modularity/slider/mobile_image_hidden', 'hidden-md hidden-lg') }}" style="background-image:url({{ ($slide['mobile_image_use'] !== false) ? $slide['mobile_image_use'][0] : '' }})">
+	<!-- Text block -->
+	@if (isset($slide['activate_textblock']) && $slide['activate_textblock'])
+	@include('partials.textblock')'
+	@endif
+</div>
 @endif
