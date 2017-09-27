1e1c8b7a4e34c1a0b71501a29273db8597f33169
Andreas Ek <andreas@elseif.se>
2017-09-27 10:04:25 +0200

    Check if module array has property set before using it. Cleaning up messy row to multiple.

--- templates/editor/modularity-enabled-modules.php
+++ templates/editor/modularity-enabled-modules.php
@@ -1,6 +1,8 @@
 <div class="modularity-modules">
     <?php foreach ($modules as $moduleId => $module) : ?>
-    <div class="modularity-module modularity-js-draggable" data-module-id="<?php echo $moduleId; ?>" data-sidebar-incompability='<?php echo is_array($module['sidebar_incompability']) ? json_encode($module['sidebar_incompability']) : ''; ?>'>
+    <div class="modularity-module modularity-js-draggable"
+         data-module-id="<?php echo $moduleId; ?>"
+         data-sidebar-incompability='<?php echo (isset($module['sidebar_incompability']) && is_array($module['sidebar_incompability'])) ? json_encode($module['sidebar_incompability']) : ''; ?>'>
         <span class="modularity-module-icon">
             <?php echo modularity_decode_icon($module); ?>
         </span>
