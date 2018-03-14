3d61c7fd4be19ae7023803f537f365d6023aee28
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-03-14 15:28:47 +0100

    Slider, pause & Play icon

--- source/php/Module/Slider/views/partials/video.blade.php
+++ source/php/Module/Slider/views/partials/video.blade.php
@@ -2,6 +2,7 @@
     <?php echo \Modularity\Module\Slider\Slider::getEmbed($slide['embed_link'], ['player'], $slide['image_use']); ?>
 @else
     <div class="slider-video" style="background-image:url('{{ ($slide['image_use'] !== false) ? $slide['image_use'][0] : '' }}');">
+        <div class="embeded-mini-toolbar"><i class="pricon pricon-sm pricon-badge pricon-pause embed-control embeded-pause"></i><i class="pricon pricon-sm pricon-badge pricon-play embed-control embeded-play hidden"></i></div>
         <video poster="{{ ($slide['image_use'] !== false) ? $slide['image_use'][0] : '' }}" preload="auto" autoplay loop muted>
             <!-- Mp4 -->
             @if (isset($slide['video_mp4']) && !empty($slide['video_mp4']))
