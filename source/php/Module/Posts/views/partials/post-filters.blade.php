258310c8e9317e598d85ed28c417781ee84f3750
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-05-28 16:43:43 +0200

    post filter - Gutters

--- source/php/Module/Posts/views/partials/post-filters.blade.php
+++ source/php/Module/Posts/views/partials/post-filters.blade.php
@@ -1,14 +1,14 @@
 @if (!empty($enabledTaxonomyFilters))
 
-    <section class="creamy creamy-border-bottom gutter-lg gutter-vertical sidebar-content-area post-filters">
+    <section class="sidebar-content-area post-filters">
 
-        <form method="get" action="" class="container" id="post-filter">
+        <form method="get" action="" id="post-filter" class="gutter-lg gutter-vertical">
             @if ( !empty($enabledTaxonomyFilters->category))
                 @foreach ($enabledTaxonomyFilters->category as $taxKey => $taxonomy)
 
                     @if(count( $taxonomy->values ) > 1)
-                        <div class="pos-relative">
-                            <button type="button" class="btn"
+                        <div class="pos-relative gutter-bottom gutter">
+                            <button type="button" class="btn "
                                     data-dropdown=".dropdown-{{ $taxKey }}"><?php printf(__('Select') . ' %s…',
                                     $taxonomy->label); ?></button>
                             <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-arrow-right dropdown-{{ $taxKey }}">
@@ -29,15 +29,15 @@
                                 :</strong></label>
                         <div class="input-group">
                             <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                            <input type="text" name="search" id="filter-keyword" class="form-control"
+                            <input type="text" name="search" id="filter-keyword" class="form-control "
                                    value="{{ $searchQuery }}" placeholder="<?php _e('Search', 'municipio'); ?>">
                         </div>
                     </div>
                 @endif
 
                 @if ($frontEndFilters['front_end_tax_filtering_dates'])
-                        <i class="btn pricon pricon-lg show-date-filter pricon-calendar"> <?php _e('Date', 'municipio'); ?> </i>
-                        <div class="grid-sm-12 grid-md-auto hidden date-filter">
+                        <i id="show-date-filter" data-toogle=".date-filter" class="btn pricon pricon-lg toogle show-date-filter pricon-calendar"> <?php _e('Date', 'municipio'); ?> </i>
+                        <div id="date-filter" class="grid-sm-12 grid-md-auto hidden date-filter">
                         <label for="filter-date-from" class="text-sm sr-only"><strong><?php _e('Date published',
                                     'municipio'); ?>:</strong></label>
                         <div class="input-group">
