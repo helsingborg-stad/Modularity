42e60ff6a6db0c4205dc919cb620ed911059e93d
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-05-30 15:19:09 +0200

    postFilters

--- dist/js/modularity.dev.js
+++ dist/js/modularity.dev.js
@@ -592,48 +592,6 @@ Modularity.Editor.Validate = (function ($) {
 })(jQuery);
 
 Modularity = Modularity || {};
-Modularity.Prompt = Modularity.Prompt || {};
-
-Modularity.Prompt.Modal = (function ($) {
-
-    var isOpen = false;
-
-    function Modal() {
-        this.handleEvents();
-    }
-
-    Modal.prototype.open = function (url) {
-        $('body').addClass('modularity-modal-open').append('\
-            <div id="modularity-modal">\
-                <div class="modularity-modal-wrapper">\
-                    <button class="modularity-modal-close" data-modularity-modal-action="close">&times; ' + modularityAdminLanguage.close + '</button>\
-                    <div class="modularity-modal-spinner-container" id="modularity-iframe-loader"><span class="modularity-modal-spinner"></span></div>\
-                    <iframe class="modularity-modal-iframe" src="' + url + '" frameborder="0" onload="document.getElementById(\'modularity-iframe-loader\').style.display=\'none\';" allowtransparency></iframe>\
-                </div>\
-            </div>\
-        ');
-
-        isOpen = true;
-    };
-
-    Modal.prototype.close = function () {
-        $('body').removeClass('modularity-modal-open');
-        $('#modularity-modal').remove();
-        isOpen = false;
-    };
-
-    Modal.prototype.handleEvents = function () {
-        $(document).on('click', '[data-modularity-modal-action="close"]', function (e) {
-            e.preventDefault();
-            this.close();
-        }.bind(this));
-    };
-
-    return new Modal();
-
-})(jQuery);
-
-Modularity = Modularity || {};
 Modularity.Helpers = Modularity.Helpers || {};
 
 Modularity.Helpers = (function ($) {
@@ -650,6 +608,7 @@ Modularity.Helpers = (function ($) {
     return new Helpers();
 
 })(jQuery);
+
 
 jQuery.fn.serializeObject = function()
 {
@@ -726,5 +685,47 @@ Modularity.Helpers.Widget = (function ($) {
     };
 
     return new Widget();
+
+})(jQuery);
+
+Modularity = Modularity || {};
+Modularity.Prompt = Modularity.Prompt || {};
+
+Modularity.Prompt.Modal = (function ($) {
+
+    var isOpen = false;
+
+    function Modal() {
+        this.handleEvents();
+    }
+
+    Modal.prototype.open = function (url) {
+        $('body').addClass('modularity-modal-open').append('\
+            <div id="modularity-modal">\
+                <div class="modularity-modal-wrapper">\
+                    <button class="modularity-modal-close" data-modularity-modal-action="close">&times; ' + modularityAdminLanguage.close + '</button>\
+                    <div class="modularity-modal-spinner-container" id="modularity-iframe-loader"><span class="modularity-modal-spinner"></span></div>\
+                    <iframe class="modularity-modal-iframe" src="' + url + '" frameborder="0" onload="document.getElementById(\'modularity-iframe-loader\').style.display=\'none\';" allowtransparency></iframe>\
+                </div>\
+            </div>\
+        ');
+
+        isOpen = true;
+    };
+
+    Modal.prototype.close = function () {
+        $('body').removeClass('modularity-modal-open');
+        $('#modularity-modal').remove();
+        isOpen = false;
+    };
+
+    Modal.prototype.handleEvents = function () {
+        $(document).on('click', '[data-modularity-modal-action="close"]', function (e) {
+            e.preventDefault();
+            this.close();
+        }.bind(this));
+    };
+
+    return new Modal();
 
 })(jQuery);
