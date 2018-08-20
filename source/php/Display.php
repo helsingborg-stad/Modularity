a8170b6e44049571e6a4022d22f7cf0497573285
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-08-20 13:32:06 +0200

    Hide modules on password protected pages

--- source/php/Display.php
+++ source/php/Display.php
@@ -65,7 +65,7 @@ class Display
         global $post;
         global $wp_query;
 
-        if (is_admin() || is_feed() || is_tax()) {
+        if (is_admin() || is_feed() || is_tax() || post_password_required()) {
             return;
         }
 
