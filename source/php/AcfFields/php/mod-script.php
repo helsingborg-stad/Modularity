72ec01f0d1b9f453848a1e57b5b39d2f44d23ca1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-02-28 15:50:17 +0100

    Removes wpauto p on newlines.

--- source/php/AcfFields/php/mod-script.php
+++ source/php/AcfFields/php/mod-script.php
@@ -3,19 +3,14 @@
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group(array(
     'key' => 'group_56a8b9eddfced',
-    'title' => 'Embed',
+    'title' => __('Embed', 'modularity'),
     'fields' => array(
         0 => array(
-            'default_value' => '',
-            'new_lines' => 'wpautop',
-            'maxlength' => '',
-            'placeholder' => '',
-            'rows' => '',
             'key' => 'field_56a8b9f1902a6',
             'label' => __('Embed code', 'modularity'),
             'name' => 'embed_code',
             'type' => 'textarea',
-            'instructions' => __('Paste your script code here (with &lt;style&gt; &lt;script&gt; tags if needed). ', 'modularity'),
+            'instructions' => __('Paste your script code here (with &lt;style&gt; &lt;script&gt; tags if needed).', 'modularity'),
             'required' => 1,
             'conditional_logic' => 0,
             'wrapper' => array(
@@ -23,8 +18,11 @@
                 'class' => '',
                 'id' => '',
             ),
-            'readonly' => 0,
-            'disabled' => 0,
+            'default_value' => '',
+            'placeholder' => '',
+            'maxlength' => '',
+            'rows' => '',
+            'new_lines' => '',
         ),
     ),
     'location' => array(
@@ -44,7 +42,5 @@
     'hide_on_screen' => '',
     'active' => 1,
     'description' => '',
-    'modified' => 1459423764,
-    'local' => 'json',
 ));
 }
