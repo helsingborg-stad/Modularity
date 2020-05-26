811829cf4f53d45304acae295ab9895671ed51a7
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-05-26 09:59:48 +0200

    Implemented new module check

--- source/php/Module/InheritPost/views/inheritposts.blade.php
+++ source/php/Module/InheritPost/views/inheritposts.blade.php
@@ -5,6 +5,6 @@
     @endif
 
     <h2>{!! apply_filters('the_title', $inherit->post_title) !!}</h2>
-    {!! apply_filters('the_content', apply_filters('Modularity/Display/SanitizeContent', $this->post_content)) !!}
+    {!! apply_filters('the_content', apply_filters('Modularity/Display/SanitizeContent', $this->post_content, $this->ID)) !!}
 </article>
 @endif
