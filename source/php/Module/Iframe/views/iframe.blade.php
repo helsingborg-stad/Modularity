566a3160c097aef721597e2e487d88118bdf4a8a
Sabina Hall <Sabina.Hall@sigma.se>
2020-05-26 10:34:16 +0200

    SIEW-380 WCAG Fixed problem related to iFrame module.

--- source/php/Module/Iframe/views/iframe.blade.php
+++ source/php/Module/Iframe/views/iframe.blade.php
@@ -3,5 +3,5 @@
     <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
     @endif
 
-    <iframe src="{{ $url }}" frameborder="0" style="width: 100%; height: {{ $height }}px;"></iframe>
+    <iframe src="{{ $url }}" title="{{ $post_title }}" style="width: 100%; height: {{ $height }}px;"></iframe>
 </div>
