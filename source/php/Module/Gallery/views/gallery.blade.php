983a8ea803d2c7e4ff3812a84aafc0b6e9fb89dc
Silvergrund <johan.silvergrund@hiq.se>
2020-06-18 15:04:40 +0200

    Module - Image

--- source/php/Module/Gallery/views/gallery.blade.php
+++ source/php/Module/Gallery/views/gallery.blade.php
@@ -1,11 +1,10 @@
 <div>
     @if (!$hideTitle && !empty($post_title))
         @typography([
-        "variant" => "h2"
+            "variant" => "h2"
         ])
             {!! apply_filters('the_title', $post_title) !!}
         @endtypography
-
     @endif
 
     @if ($images)
