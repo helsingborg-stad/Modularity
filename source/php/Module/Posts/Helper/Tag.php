51250ba13fff2d1382177b4f7618ccdcc961b989
Silvergrund <johan.silvergrund@hiq.se>
2020-06-25 13:51:31 +0200

    Module-posts - Helper for tags, news view
    Added Helper for tags
    Changed news view and replaced markup with components.

---
+++ source/php/Module/Posts/Helper/Tag.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Modularity\Module\Posts\Helper;
+
+/**
+ * Class Tag
+ * @package Modularity\Module\Posts\Helper
+ */
+class Tag
+{
+
+    /**
+     * @param $postId
+     * @param $tax
+     * @return array|null
+     */
+    public function getTags($postId, $tax)
+    {
+        if (!$tax || !$postId) {
+            return null;
+        }
+
+        foreach ($tax as $taxonomy => $placement) {
+
+            $terms = wp_get_post_terms($postId, $taxonomy);
+
+            if (count($terms) > 0) {
+
+                foreach ($terms as $index => $term) {
+                    $tags[$index]['label'] = $term->name;
+                    $tags[$index]['color'] = 'secondary';
+                    $tags[$index]['href'] = get_term_link( $term->term_id);
+                }
+
+                if (!isset($tags)) {
+                    return null;
+                }
+
+                return $tags;
+            }
+        }
+    }
+}
+
--- source/php/Module/Posts/Posts.php
+++ source/php/Module/Posts/Posts.php
@@ -206,7 +206,6 @@ class Posts extends \Modularity\Module
         }
 
         $data['taxonomyDisplay'] = $this->getTaxonomyDisplay($fields);
-
         $data['posts_data_post_type'] = isset($fields->posts_data_post_type) ? $fields->posts_data_post_type : false;
         $data['posts_data_source'] = $fields->posts_data_source;
         $data['posts_fields'] = isset($fields->posts_fields) ? $fields->posts_fields : false;
