45abd870aec31c3f7b47026f8c290a248181d41d
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-06-25 10:21:40 +0200

    Moved translate string in to controller and cast slide to object

--- source/php/Module/Slider/views/slider.blade.php
+++ source/php/Module/Slider/views/slider.blade.php
@@ -3,21 +3,21 @@
 ])
     @foreach ($slides as $slide)
         @segment([
-            'title' => $slide['textblock_title'],
-            'text' => $slide['textblock_content'],
-            'background_image' => $slide['image_use'][0],
+            'title' => $slide->textblock_title,
+            'text' => $slide->textblock_content,
+            'background_image' => $slide->image_use[0],
             'overlay' => 'light',
             'overlay_opacity' => 'high'
         ])
 
-            @if ($slide['link_url']) 
+            @if ($slide->link_url) 
                 @slot('bottom')
                     @button([
-                        'text' => __('Read more', 'modularity'),
+                        'text' => $slide->link_text,
                         'color' => 'primary',
                         'type' => 'filled',
-                        'href' => $slide['link_url'],
-                        'target' => $slide['link_type'] === 'external' ? '_blank' : '_self' 
+                        'href' => $slide->link_url,
+                        'target' => $slide->link_type === 'external' ? '_blank' : '_self' 
                     ])
                     @endbutton
                 @endslot
