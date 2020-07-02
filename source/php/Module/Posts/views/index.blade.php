df5ad67a8c1cc008f7793862140de034158f8ecd
Johan Silvergrund <johan.silvergrund@hiq.se>
2020-07-02 13:57:29 +0200

    3.0/feature/module posts, Index and Horizontal view (#70)
    * Module-posts - Helper for tags, news view
    
    Added Helper for tags
    Changed news view and replaced markup with components.
    
    * cleanup
    
    * module - posts : prepareList method
    
    Prepare List format the data to fit listing component
    
    * Better error handling with stack trace
    
    * Moved method to helper
    
    * Module - Image
    
    * first draft - without cropping etc.
    
    * cleanup
    
    * list
    
    * Moved prepareList method to controller
    
    * added slider component to slider module (#67)
    
    * added slider component to slider module
    
    * Moved translate string in to controller and cast slide to object
    
    * Updated polyfills.
    
    * List module
    
    * cleanup
    
    * list component , before date
    
    * index
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * horizontal
    
    * cleanup
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>
    Co-authored-by: Alexander Boman Skoug <39676080+alexanderbomanskoug2@users.noreply.github.com>

--- source/php/Module/Posts/views/index.blade.php
+++ source/php/Module/Posts/views/index.blade.php
@@ -1,58 +1,105 @@
 @include('partials.post-filters')
 
 <div class="grid" data-equal-container>
+
     @if (!$hideTitle && !empty($post_title))
-    <div class="grid-xs-12">
-        <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
-    </div>
+
+        @typography([
+            'element' => "h4",
+            'classList' => ['box-title']
+        ])
+            {!! apply_filters('the_title', $post_title) !!}
+        @endtypography
+
     @endif
 
     @foreach ($posts as $post)
-    <div class="{{ $posts_columns }}">
-        <a href="{{ $posts_data_source === 'input' ? $post->permalink : get_permalink($post->ID) }}" class="{{ $classes }}" data-equal-item>
-
+        <div class="{{ $posts_columns }}">
             @if ($post->thumbnail && in_array('image', $posts_fields))
-                <div class="box-image-container">
-                    <?php if (isset($taxonomyDisplay['top'])) : foreach ($taxonomyDisplay['top'] as $taxonomy => $placement) : $terms = wp_get_post_terms($post->ID, $taxonomy); if (count($terms) > 0) : ?>
-                        <ul class="tags-<?php echo $taxonomy; ?> pos-absolute-<?php echo $placement; ?>">
-                            <?php foreach ($terms as $term) : ?>
-                                <li class="tag tag-<?php echo $term->taxonomy; ?> tag-<?php echo $term->slug; ?>"><?php echo $term->name; ?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php endif; endforeach; endif; ?>
 
-                    <img src="{{ $post->thumbnail[0] }}" alt="{{ $post->post_title }}" class="box-image">
+                <div class="box-image-container">
+
+                    @tags([
+                        'tags' => (new Modularity\Module\Posts\Helper\Tag)->getTags($post->ID, $taxonomyDisplay['top'])
+                    ])
+                    @endtags
+
+                    @link([
+                        'href' => $posts_data_source === 'input' ? $post->permalink : get_permalink ($post->ID),
+                        'classList' => $classes
+                    ])
+
+                        @if ($post->thumbnail && in_array('image', $posts_fields))
+
+                            @image([
+                                'src'=> $post->thumbnail[0],
+                                'alt' => $post->post_title,
+                                'classList' => ['box-image'],
+                            ])
+                            @endimage
+
+                        @else
+                            <figure class="image-placeholder"></figure>
+                        @endif
+                    @endlink
+
                 </div>
             @endif
 
             <div class="box-content">
                 @if (in_array('title', $posts_fields))
-                <h5 class="box-index-title link-item">{!! apply_filters('the_title', $post->post_title) !!}</h5>
+
+                    @link([
+                        'href' => $posts_data_source === 'input' ? $post->permalink : get_permalink ($post->ID),
+                        'classList' => $classes
+                    ])
+
+                        @typography([
+                            'element' => "h5",
+                        'classList' => ['box-index-title', 'link-item-light']
+                        ])
+                            {!! apply_filters('the_title', $post->post_title) !!}
+                        @endtypography
+
+                    @endlink
+
                 @endif
 
                 @if (in_array('excerpt', $posts_fields))
-                {!! isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', wp_trim_words(wp_strip_all_tags(strip_shortcodes(get_extended($post->post_content)['main'])), 30, null)) : '' !!}
+
+                     @link([
+                        'href' => $posts_data_source === 'input' ? $post->permalink : get_permalink ($post->ID),
+                        'classList' => $classes
+                    ])
+                        {!! isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', wp_trim_words(wp_strip_all_tags(strip_shortcodes(get_extended($post->post_content)['main'])), 30, null)) : '' !!}
+                    @endlink
+
                 @endif
 
                 @if (isset($taxonomyDisplay['below']))
                     <div class="gutter gutter-top">
-                    <?php foreach ($taxonomyDisplay['below'] as $taxonomy => $placement) : $terms = wp_get_post_terms($post->ID, $taxonomy); if (count($terms) > 0) : ?>
-                    <ul class="tags tags-<?php echo $taxonomy; ?>">
-                        <?php foreach ($terms as $term) : ?>
-                            <li class="tag tag-<?php echo $term->taxonomy; ?> tag-<?php echo $term->slug; ?>"><?php echo $term->name; ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <?php endif; endforeach; ?>
+                        @tags([
+                            'tags' => (new Modularity\Module\Posts\Helper\Tag)->getTags($post->ID, $taxonomyDisplay['below'])
+                        ])
+                        @endtags
                     </div>
                 @endif
+
             </div>
-        </a>
-    </div>
+        </div>
     @endforeach
 
     @if ($posts_data_source !== 'input' && isset($archive_link) && $archive_link && $archive_link_url)
-    <div class="grid-lg-12">
-        <a class="read-more" href="{{ $archive_link_url }}?{{ http_build_query($filters) }}"><?php _e('Show more', 'modularity'); ?></a>
-    </div>
+        <div>
+
+            @link([
+                'href' => $archive_link_url ."?".http_build_query($filters) ,
+                    'classList' => ['read-more']
+            ])
+                {{_e('Show more', 'modularity')}}
+            @endlink
+
+        </div>
     @endif
+
 </div>
