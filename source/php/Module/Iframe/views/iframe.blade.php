b4b1de8fa2455a916eb6a8178ed7b925f338932a
Silvergrund <johan.silvergrund@hiq.se>
2020-08-13 10:59:10 +0200

    iframe module h4

--- source/php/Module/Iframe/views/iframe.blade.php
+++ source/php/Module/Iframe/views/iframe.blade.php
@@ -1,6 +1,13 @@
 <div class="box box-iframe no-padding">
     @if (!$hideTitle && !empty($post_title))
-    <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+
+        @typography([
+            'element' => "h4",
+            'classList' => ['box-title']
+        ])
+            {!! apply_filters('the_title', $post_title) !!}
+        @endtypography
+
     @endif
 
     <iframe src="{{ $url }}" frameborder="0" style="width: 100%; height: {{ $height }}px;"></iframe>
