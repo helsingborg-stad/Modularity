35b1e4b56209f2d6b8c86ea9df82b663f984b736
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-08 16:59:06 +0200

    Update markup

--- source/php/Module/Index/views/index.blade.php
+++ source/php/Module/Index/views/index.blade.php
@@ -1,42 +1,26 @@
-<div class="grid" data-equal-container>
-    @if (!$hideTitle && !empty($post_title))
-        <div class="grid-xs-12">
-            @typography([
-                'element' => 'h2'
-            ])
-                {!! apply_filters('the_title', $post_title) !!}
-            @endtypography
-        </div>
-    @endif
 
-    @foreach ($items as $item)
-        <div class="{{ $columnClass }}">
-            @link([
-                'href' => $item['permalink'],
-                'classList' => [$classes],
-                'attributeList' => ['data-equal-item' => '']
-            ])
-                @if ($item['thumbnail'])
-                    @image([
-                        'src'=> $item['thumbnail'][0],
+@if (!$hideTitle && !empty($post_title))
+    @typography(['element' => 'h2'])
+        {!! apply_filters('the_title', $post_title) !!}
+    @endtypography
+@endif
+
+<div class="grid">
+        @foreach ($items as $item)
+            <div class="{{ $columnClass }}">
+                @card([
+                    'heading' => $item['title'],
+                    'content' => $item['lead'],
+                    'image' => [
+                        'src' => $item['thumbnail'][0],
                         'alt' => $item['title'],
-                        'classList' => ['box-image']
-                    ])
-                    @endimage
-                @endif
-
-                <div class="box-content">
-                    @typography([
-                        'element' => 'h5',
-                        'variant' => 'h5',
-                        'classList' => ['box-index-title link-item']
-                    ])
-                        {{ $item['title'] }}
-                    @endtypography
-
-                    {!! $item['lead'] !!}
-                </div>
-            @endlink
-        </div>
-    @endforeach
+                        'backgroundColor' => 'secondary',
+                        'padded' => false
+                    ],
+                    'link' => $item['permalink']
+                ])
+                @endcard
+            </div>
+        @endforeach
+    </div>
 </div>
