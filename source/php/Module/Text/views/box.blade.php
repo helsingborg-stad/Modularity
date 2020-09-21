67ddc1ad68899d7c46331d42ab87fcad71e9f619
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-09-21 14:46:48 +0200

    Added box for text module

--- source/php/Module/Text/views/box.blade.php
+++ source/php/Module/Text/views/box.blade.php
@@ -8,7 +8,7 @@
         @endtypography
     @endif
 
-    <div class="box-content">
+    @card([])
         {!! apply_filters('the_content', apply_filters('Modularity/Display/SanitizeContent', $post_content)) !!}
-    </div>
+    @endcard
 </div>
