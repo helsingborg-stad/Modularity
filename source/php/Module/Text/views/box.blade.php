b3de2e4c4627dbdbcfea2d4bd112a9c70e2ca710
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-21 16:39:18 +0200

    Use buildtin card heading in text module

--- source/php/Module/Text/views/box.blade.php
+++ source/php/Module/Text/views/box.blade.php
@@ -1,14 +1,4 @@
-<div class="{{ $classes }} {{ isset($font_size) ? $font_size : '' }}">
-    @if (!$hideTitle && !empty($post_title))
-        @typography([
-            'variant' => "h4",
-            'classList' => ['box-title']
-        ])
-            {!! apply_filters('the_title', $post_title) !!}
-        @endtypography
-    @endif
 
-    @card([])
-        {!! apply_filters('the_content', apply_filters('Modularity/Display/SanitizeContent', $post_content)) !!}
-    @endcard
-</div>
+@card(['heading' => !$hideTitle && $post_title != "" ? $post_title : ''])
+    {!! apply_filters('the_content', apply_filters('Modularity/Display/SanitizeContent', $post_content)) !!}
+@endcard
