87567149d6ddd54eb5114b21163bc09fbfaadc28
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-10-01 20:05:27 +0200

    - added parsing of layout to partials  - added new options to view

--- source/php/Module/Slider/views/partials/textblock.blade.php
+++ source/php/Module/Slider/views/partials/textblock.blade.php
@@ -2,15 +2,15 @@
     'title' => $slide->textblock_title,
     'text' => $slide->textblock_content,
     'background_image' => $slide->image_use[0],
-    'overlay' => 'light',
-    'overlay_opacity' => 'high'
+    'layout' => $layout,
+    'containerColor' => 'tertiary',
 ])
 
     @if ($slide->link_url) 
         @slot('bottom')
             @button([
                 'text' => $slide->link_text,
-                'color' => 'primary',
+                'color' => 'secondary',
                 'type' => 'filled',
                 'href' => $slide->link_url,
                 'target' => $slide->link_type === 'external' ? '_blank' : '_self' 
