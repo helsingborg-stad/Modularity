0a6b3683394c8e62a8e594617dd5b70a091747e2
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-11-01 00:22:13 +0100

    Add titles to text cards

--- source/php/Module/Text/views/box.blade.php
+++ source/php/Module/Text/views/box.blade.php
@@ -1,6 +1,17 @@
-
-@card(['heading' => !$hideTitle && $post_title != "" ? $post_title : ''])
+@card()
     <div class="c-card__body">
+        @if (!$hideTitle && !empty($post_title))
+            <div class="c-card__heading">
+                @typography([
+                    "element" => "h2",
+                    "variant" => "h2",
+                    "classList" => ['c-card__heading']
+                ])
+                    {{ $post_title }}
+                @endtypography
+            </div>
+        @endif
+
         {!! apply_filters('the_content', apply_filters('Modularity/Display/SanitizeContent', $post_content)) !!}
     </div>
 @endcard
