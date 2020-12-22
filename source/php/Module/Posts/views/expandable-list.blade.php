0b36ebd980f56af49c2fac349bdf8bb4a71f47be
Silvergrund <johan.silvergrund@hiq.se>
2020-12-22 15:16:17 +0100

    post accordion list - view and controller - header columns

--- source/php/Module/Posts/views/expandable-list.blade.php
+++ source/php/Module/Posts/views/expandable-list.blade.php
@@ -14,33 +14,6 @@
         </div>
     @endif
 
-    @if (isset($posts_list_column_titles) && $posts_list_column_titles)
-        <header class="accordion-table accordion-table-head">
-            @if ($posts_hide_title_column)
-
-                @typography([
-                    'element' => "span",
-                    'classList' => ['column-header']
-                ])
-                    {{ isset($title_column_label) && !empty($title_column_label) ? $title_column_label : __('Title', 'modularity') }}
-                @endtypography
-
-            @endif
-
-            @foreach ($posts_list_column_titles as $column)
-
-                    @typography([
-                        'element' => "span",
-                        'classList' => ['column-header']
-                    ])
-                        {{ $column->column_header }}
-                    @endtypography
-
-            @endforeach
-        </header>
-    @endif
-
-
     <div js-filter-container=''>
 
         @if (!isset($allow_freetext_filtering) || $allow_freetext_filtering)
@@ -59,14 +32,67 @@
             </div>
 
         @endif
+    
+            @if (isset($posts_list_column_titles) && $posts_list_column_titles)
 
+                <header class="accordion-table__head">
+                    @if ($posts_hide_title_column)
+                        @typography([
+                            'element' => "span",
+                            'classList' => ['accordion-table__head-column']
+                        ])
+                            {{ isset($title_column_label) && !empty($title_column_label) ? $title_column_label : __('Title', 'modularity') }}
+                        @endtypography
+                    @endif
+              
+                    @if ( is_array($posts_list_column_titles)  && !empty($posts_list_column_titles))
+                        @foreach ($posts_list_column_titles as $column)
+                            @typography([
+                                'element' => "span",
+                                'classList' => ['accordion-table__head-column']
+                            ])
+                                {{ $column->column_header }}
+                            @endtypography
+                        @endforeach
+                        <span class="accordion-table__head-column-icon"></span>
+                   @endif
+                </header>
+            @endif
+        
+        
         @if(count($prepareAccordion) > 0)
-
+           
             @accordion([])
                 @foreach ($prepareAccordion as $accordionItem)
-                    @accordion__item(['heading' => $accordionItem['heading'],'attributeList' => ['js-filter-item' => '', 'js-filter-data' => '']])
+
+                    @if( is_array($accordionItem['column_values'])  && !empty($accordionItem['column_values']))
+               
+                        @accordion__item([
+                            'heading' => ($posts_hide_title_column) ?
+                                array_merge( (array) $accordionItem['heading'], (array) $accordionItem['column_values'] ) :
+                                $accordionItem['column_values'],
+                            'attributeList' => [
+                                'js-filter-item' => '',
+                                'js-filter-data' => ''
+                            ],
+                            'classList' => ['c-accordion-table']
+                       ])
+                            {!! $accordionItem['content'] !!}
+                       @endaccordion__item
+                        
+                    @else
+                 
+                        @accordion__item([
+                            'heading' => $accordionItem['heading'],
+                            'attributeList' => [
+                                'js-filter-item' => '',
+                                'js-filter-data' => ''
+                            ]
+                        ])
                         {!! $accordionItem['content'] !!}
-                    @endaccordion__item
+                        @endaccordion__item
+                        
+                   @endif
                 @endforeach
             @endaccordion
 
