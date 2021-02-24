c921336cb307912355065bf18dd1f3c7a2bc1f91
Muckbuck <rosenborg.eric@gmail.com>
2021-02-24 14:55:24 +0100

    Add alt text to slider image (#152)
    * Add alt text to slider image
    
    * Change from background image to desktop image in slider item
    
    Co-authored-by: Eric Rosenborg <eric.rosenborg@hiq.se>

--- source/php/Module/Slider/views/partials/item.blade.php
+++ source/php/Module/Slider/views/partials/item.blade.php
@@ -1,12 +1,13 @@
 @slider__item([
     'title'             => $slide->textblock_title,
     'text'              => $slide->textblock_content,
-    'background_image'  => $slide->image_use[0],
+    'desktop_image'     => $slide->image_use[0],
     'layout'            => $slide->textblock_position,
     'mobile_image'      => $slide->mobile_image_use[0],
     'background_video'  => $slide->video_mp4['url'] ?? false,
     'containerColor'    => $slide->background_color ?? 'none',
     'textColor'         => $slide->text_color,
-    'link'              => $slide->link_url
+    'link'              => $slide->link_url,
+    'alt'               => $slide->image['alt']
 ])
 @endslider__item
