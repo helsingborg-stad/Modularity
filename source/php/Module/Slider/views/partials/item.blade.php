8752bc8040ab3938ca17298f37df6efc5465b62c
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-02-24 14:53:19 +0100

    Change from background image to desktop image in slider item

--- source/php/Module/Slider/views/partials/item.blade.php
+++ source/php/Module/Slider/views/partials/item.blade.php
@@ -1,7 +1,7 @@
 @slider__item([
     'title'             => $slide->textblock_title,
     'text'              => $slide->textblock_content,
-    'background_image'  => $slide->image_use[0],
+    'desktop_image'     => $slide->image_use[0],
     'layout'            => $slide->textblock_position,
     'mobile_image'      => $slide->mobile_image_use[0],
     'background_video'  => $slide->video_mp4['url'] ?? false,
