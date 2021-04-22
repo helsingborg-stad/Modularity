0e7a347aa843e9ea1dbac4fa5f90a03861ba214f
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2021-04-22 09:06:35 +0200

    Removes deprecated modules (#160)
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Module/Social/views/social.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="{{ $classes }}">
-    @if (!$hideTitle && !empty($post_title))
-        <h4 class="module-title"><i class="fa fa-{{ $feedArgs['network'] }}"></i> {!! apply_filters('the_title', $post_title) !!}</h4>
-    @endif
-
-    {!! $feed->render() !!}
-</div>
