d89cbb1bdf61a58091baa989e3ee351cb3ae943c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-05-12 12:26:28 +0200

    Adds v1 curator module

---
+++ source/php/Module/Curator/views/curator.blade.php
@@ -0,0 +1,83 @@
+<!-- Social media by curator --> 
+
+@if (!$hideTitle && !empty($post_title))
+    @typography([
+        'id' => 'mod-posts-' . $ID . '-label',
+        'element' => 'h4', 
+        'variant' => 'h2', 
+        'classList' => ['module-title']
+    ])
+        {!! apply_filters('the_title', $post_title) !!} 
+    @endtypography
+@endif
+
+@if($showFeed) 
+    <div class="o-grid modularity-socialmedia-container">
+        @foreach ($posts as $post)
+            <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+                @card([ 
+                    'link' => $post->url,
+                    'image' => [
+                        'src' => $post->image,
+                        'alt' => $post->text,
+                        'backgroundColor' => 'secondary'
+                    ],
+                    'ratio' => '4:3',
+                    'content' => $post->text,
+                    'classList' => ['u-height--100']
+                ])
+                    
+                    @avatar(
+                        [
+                            'name' => $post->user_readable_name,
+                            'size' => 'sm',
+                            'classList' => ['u-position--absolute', 'u-level-1', 'u-box-shadow--1', 'u-margin--2']
+                        ]
+                    )
+                    @endavatar
+
+                    @image([
+                        'src'=> $post->image,
+                        'alt' => $post->text,
+                        'classList' => ['c-card__image'],
+                        'rounded' => false,
+                    ])
+                    @endimage
+
+                    <div class="c-card__body">
+                        @typography(['element' => 'span', 'variant' => 'meta'])
+                            @date(['action' => 'formatDate', 'timestamp' => $post->source_created_at])
+                            @enddate
+                        @endtypography   
+
+                        @typography(['element' => 'div'])
+                            {{ $post->text }}
+                        @endtypography
+                    </div>
+                @endcard
+            </div>
+        @endforeach
+
+        <div class="o-grid-12">
+            @typography(['element' => 'div', 'variant' => 'meta', 'classList' => ['u-text-align--right']])
+                <a href="https://curator.io" target="_blank">Powered by Curator.io</a>
+            @endtypography
+        </div>
+    </div>
+@else
+
+    @notice([
+        'type' => 'info',
+        'message' => [
+            'text' => $errorMessage,
+            'size' => 'sm'
+        ],
+        'icon' => [
+            'name' => 'report',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
+
+@endif
