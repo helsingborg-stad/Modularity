0f0e1b16beb01d21c67b4c9393daee6583a2de78
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-06-29 11:32:23 +0200

    Added description to iframe module

--- source/php/AcfFields/php/mod-iframe.php
+++ source/php/AcfFields/php/mod-iframe.php
@@ -3,16 +3,14 @@
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group(array(
     'key' => 'group_56c47016ea9d5',
-    'title' => 'Iframe settings',
+    'title' => __('Iframe settings', 'modularity'),
     'fields' => array(
         0 => array(
-            'default_value' => '',
-            'placeholder' => __('Enter your embed url', 'modularity'),
             'key' => 'field_56c4701d32cb4',
             'label' => __('Iframe URL', 'modularity'),
             'name' => 'iframe_url',
             'type' => 'url',
-            'instructions' => '',
+            'instructions' => __('<span style="color: #f00;">Your iframe link must start with http<strong>s</strong>://. Links without this prefix will not display.</span>', 'modularity'),
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
@@ -20,15 +18,10 @@
                 'class' => '',
                 'id' => '',
             ),
+            'default_value' => '',
+            'placeholder' => __('Enter your embed url', 'modularity'),
         ),
         1 => array(
-            'default_value' => 350,
-            'min' => 100,
-            'max' => 10000,
-            'step' => 10,
-            'placeholder' => '',
-            'prepend' => '',
-            'append' => __('pixels', 'modularity'),
             'key' => 'field_56c4704f32cb5',
             'label' => __('Iframe height', 'modularity'),
             'name' => 'iframe_height',
@@ -41,8 +34,34 @@
                 'class' => '',
                 'id' => '',
             ),
+            'default_value' => 350,
+            'min' => 100,
+            'max' => 10000,
+            'step' => 10,
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => __('pixels', 'modularity'),
             'readonly' => 0,
             'disabled' => 0,
+        ),
+        2 => array(
+            'key' => 'field_60d9ccff3a64e',
+            'label' => __('Description', 'modularity'),
+            'name' => 'iframe_description',
+            'type' => 'text',
+            'instructions' => __('Describe the contents of this Iframe (not shown).', 'modularity'),
+            'required' => 1,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
         ),
     ),
     'location' => array(
@@ -53,6 +72,62 @@
                 'value' => 'mod-iframe',
             ),
         ),
+        1 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
+        2 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
+        3 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
+        4 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
+        5 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
+        6 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
+        7 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
+        8 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/iframe',
+            ),
+        ),
     ),
     'menu_order' => 0,
     'position' => 'normal',
@@ -60,9 +135,7 @@
     'label_placement' => 'top',
     'instruction_placement' => 'label',
     'hide_on_screen' => '',
-    'active' => 1,
+    'active' => true,
     'description' => '',
-    'modified' => 1455714456,
-    'local' => 'json',
 ));
 }
--- source/php/Module/Iframe/views/iframe.blade.php
+++ source/php/Module/Iframe/views/iframe.blade.php
@@ -1,11 +1,11 @@
 @if (!$hideTitle && !empty($post_title))
     @typography([
-        'element' => 'h4', 
-        'variant' => 'h2', 
+        'element' => 'h4',
+        'variant' => 'h2',
         'classList' => ['module-title']
     ])
         {!! apply_filters('the_title', $post_title) !!}
     @endtypography
 @endif
 
-<iframe src="{{ $url }}" title="{!! apply_filters('the_title', $post_title) !!}" frameborder="0" style="width: 100%; height: {{ $height }}px;"></iframe>
+<iframe src="{{ $url }}" title="{!! $description ?? apply_filters('the_title', $post_title) !!}" frameborder="0" style="width: 100%; height: {{ $height }}px;"></iframe>
