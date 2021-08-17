58474938999fccc60de251cb1d379f0f208b3639
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-08-17 13:04:00 +0200

    Add the missing dynamic table plugin and load it in the plugins class

--- source/php/Plugins.php
+++ source/php/Plugins.php
@@ -8,7 +8,8 @@ class Plugins
         "johannheyne/advanced-custom-fields-table-field/advanced-custom-fields-table-field/trunk/acf-table.php",
         "clark-nikdel-powell/post-type-select-for-acf/acf-posttype-select.php",
         "jeradin/acf-website-field/acf-website_field.php",
-        "ooksanen/acf-focuspoint/acf-focuspoint.php"
+        "ooksanen/acf-focuspoint/acf-focuspoint.php",
+        'acf-dynamic-table-field/acf-anagram_dynamic_table_field.php'
     );
 
     public function __construct()
@@ -17,9 +18,18 @@ class Plugins
 
         foreach ($this->plugins as $plugin) {
 
-            $pluginPath = MODULARITY_PATH . "vendor/" . $plugin; 
+            $pluginPaths = [
+                MODULARITY_PATH . "vendor/" . $plugin,
+                MODULARITY_PATH . "plugins/" . $plugin
+            ]; 
 
-            if (file_exists($pluginPath)) {
+            foreach($pluginPaths as $path) {
+                $fileExistsInPath = file_exists($path) ? true : false;
+                $pluginPath = $path;
+                continue;
+            }
+
+            if ($fileExistsInPath) {
                 require_once $pluginPath;
             } else {
                 error_log("A plugin not existing tried to enqueue to acf in modularity."); 
