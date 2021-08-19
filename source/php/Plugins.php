05aa74cdf6f88abca54d8a917b4eaaae30964fcd
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2021-08-19 13:59:01 +0200

    3.0/feature/composer load plugins (#199)
    * Remove dynamic table plugins & plugin dir.
    
    * Load table field with composer
    
    * Add table field in plugins loader. Remove support for plugins directory.
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Plugins.php
+++ source/php/Plugins.php
@@ -9,31 +9,26 @@ class Plugins
         "clark-nikdel-powell/post-type-select-for-acf/acf-posttype-select.php",
         "jeradin/acf-website-field/acf-website_field.php",
         "ooksanen/acf-focuspoint/acf-focuspoint.php",
-        'acf-dynamic-table-field/acf-anagram_dynamic_table_field.php'
+        "jeradin/acf-dynamic-table-field/acf-anagram_dynamic_table_field.php"
     );
 
     public function __construct()
     {
         $this->plugins = apply_filters('Modularity/Plugins', $this->plugins);
 
-        foreach ($this->plugins as $plugin) {
+        if(is_array($this->plugins) && !empty($this->plugins)) {
+            foreach ($this->plugins as $plugin) {
 
-            $pluginPaths = [
-                MODULARITY_PATH . "vendor/" . $plugin,
-                MODULARITY_PATH . "plugins/" . $plugin
-            ]; 
+                $pluginPath = MODULARITY_PATH . "vendor/" . $plugin; 
 
-            foreach($pluginPaths as $path) {
-                $fileExistsInPath = file_exists($path) ? true : false;
-                $pluginPath = $path;
-                continue;
+                if (file_exists($pluginPath)) {
+                    require_once $pluginPath;
+                } else {
+                    error_log("A plugin not existing tried to enqueue to acf in modularity. Have you run composer install?"); 
+                }
             }
-
-            if ($fileExistsInPath) {
-                require_once $pluginPath;
-            } else {
-                error_log("A plugin not existing tried to enqueue to acf in modularity."); 
-            }
+        } else {
+            error_log("No plugins acf plugins found in array for modulary, please review your filters."); 
         }
     }
 }
