755261a9b977464d03325b836a428557760a0fd8
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-08-19 13:14:02 +0200

    - removed mobile and adapter for new data

--- source/php/Module/Slider/views/partials/item.blade.php
+++ source/php/Module/Slider/views/partials/item.blade.php
@@ -1,17 +1,15 @@
 @slider__item([
     'title'             => $slide->textblock_title,
     'text'              => $slide->textblock_content,
-    'desktop_image'     => $slide->image_use[0],
+    'image'             => $slide->image_use[0],
     'layout'            => $slide->textblock_position,
-    'mobile_image'      => $slide->mobile_image_use[0],
     'background_video'  => $slide->video_mp4['url'] ?? false,
     'containerColor'    => $slide->background_color ?? 'none',
     'textColor'         => $slide->text_color,
     'link'              => $slide->link_url,
-    'alt'               => $slide->image['alt'],
-    'altMobile'         => $slide->mobile_image['alt'],
+    'alt'               => $slide->alt_text,
     'cta'               => $slide->call_to_action,
     'heroStyle'         => $slide->heroStyle,
     'focusPoint'        => $slide->focusPoint, 
 ])
-@endslider__item 
+@endslider__item
