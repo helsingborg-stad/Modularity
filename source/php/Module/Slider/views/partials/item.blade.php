56aa071f3210dc1810c01f6e8039832a0e573a06
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-08-20 09:39:14 +0200

    - add backwards compatability to slider item

--- source/php/Module/Slider/views/partials/item.blade.php
+++ source/php/Module/Slider/views/partials/item.blade.php
@@ -1,7 +1,7 @@
 @slider__item([
     'title'             => $slide->textblock_title,
     'text'              => $slide->textblock_content,
-    'image'             => $slide->image_use[0],
+    'desktop_image'     => $slide->image_use[0],
     'layout'            => $slide->textblock_position,
     'background_video'  => $slide->video_mp4['url'] ?? false,
     'containerColor'    => $slide->background_color ?? 'none',
