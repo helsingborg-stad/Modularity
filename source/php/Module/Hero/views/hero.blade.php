b439a3b888c56be977a4a6e652adf7d1b5790a78
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-09-23 09:03:38 +0200

    Adds image focus.

--- source/php/Module/Hero/views/hero.blade.php
+++ source/php/Module/Hero/views/hero.blade.php
@@ -1,5 +1,6 @@
 @hero([
     "image" => $modHeroImage->url,
+    "imageFocus" => $modHeroImage->focus,
     "size" => $modHeroSize,
     "overlay" => $modHeroOverlayType,
     "color" => $modHeroFontColor,
