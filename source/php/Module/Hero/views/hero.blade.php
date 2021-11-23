30ea980b56ac862bc923e972d484be9c151d8c29
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-11-23 08:52:29 +0100

    Remove font color selector

--- source/php/Module/Hero/views/hero.blade.php
+++ source/php/Module/Hero/views/hero.blade.php
@@ -2,7 +2,6 @@
     "image" => $modHeroImage->url,
     "imageFocus" => $modHeroImage->focus,
     "size" => $modHeroSize,
-    "color" => $modHeroFontColor,
     "title" => !$hideTitle ? $postTitle : false,
     "byline" => $modHeroByline,
     "paragraph" => $modHeroBody,
