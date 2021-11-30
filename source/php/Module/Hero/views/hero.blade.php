8f22045cc8620a0296390c2938b1201024d5f7fb
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2021-11-30 08:08:59 +0100

    3.0/feature/no overlay select (#220)
    * Remove overlay selector
    
    * Remove font color selector
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Module/Hero/views/hero.blade.php
+++ source/php/Module/Hero/views/hero.blade.php
@@ -2,8 +2,6 @@
     "image" => $modHeroImage->url,
     "imageFocus" => $modHeroImage->focus,
     "size" => $modHeroSize,
-    "overlay" => $modHeroOverlayType,
-    "color" => $modHeroFontColor,
     "title" => !$hideTitle ? $postTitle : false,
     "byline" => $modHeroByline,
     "paragraph" => $modHeroBody,
