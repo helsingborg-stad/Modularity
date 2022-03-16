b9d777f44678a9f1cefc6047b491561a073ccaa2
Freya <752642+faejr@users.noreply.github.com>
2022-03-16 12:46:15 +0100

    feat: add sidebar context

--- source/php/Module/Slider/views/partials/item.blade.php
+++ source/php/Module/Slider/views/partials/item.blade.php
@@ -4,13 +4,11 @@
     'desktop_image'     => $slide->image_use[0],
     'layout'            => $slide->textblock_position,
     'background_video'  => $slide->video_mp4['url'] ?? false,
-    'containerColor'    => $slide->container_colour ?? 'transparent',
-    'textAlignment'     => $slide->text_alignment ?? 'left',
-    'textColor'         => $slide->text_color,
     'link'              => $slide->link_url,
     'alt'               => $slide->alt_text,
     'cta'               => $slide->call_to_action,
     'heroStyle'         => $slide->heroStyle,
     'focusPoint'        => $slide->focusPoint,
+    'context'           => ['module.slider-item', $sidebarContext . '.module.slider-item']
 ])
 @endslider__item
