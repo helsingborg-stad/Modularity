c961efca9be7dc33b59183df68a2ae434e7100aa
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-30 13:38:32 +0200

    Utilize parameters on indexes

--- source/php/Module/Index/views/index.blade.php
+++ source/php/Module/Index/views/index.blade.php
@@ -12,31 +12,15 @@
     @foreach ($items as $item)
         <div class="{{ apply_filters('Municipio/Controller/Archive/GridColumnClass', $columnClass) }}">
             @card([
-                'link'      => $item['permalink'],
-                'classList' => ['u-height--100', 'u-height-100'],
-                'context'   => 'module.index',
-                'hasAction' => true,
+                'link'              => $item['permalink'],
+                'classList'         => ['u-height--100', 'u-height-100'],
+                'context'           => 'module.index',
+                'hasAction'         => !empty($item['permalink']),
+                'image'             => isset($item['thumbnail'][0]) ? $item['thumbnail'][0] : false,
+                'heading'           => isset($item['title']) && !empty($item['title']) ? $item['title'] : false,
+                'content'           => $item['lead'],
+                'containerAware'    => true
             ])
-
-                @if($item['thumbnail'][0])
-                    <div class="c-card__image c-card__image--secondary">
-                        <div class="c-card__image-background u-ratio-16-9" alt="{{ $item['title'] }}" style="height:initial; background-image:url('{{ $item['thumbnail'][0] }}');"></div>
-                    </div>
-                @endif
-
-                <div class="c-card__body">
-                    @if (!empty($item['title']))
-                        @typography([
-                            'element' => "h2",
-                            'classList' => ['c-card__heading'],
-                        ])
-                            {{ $item['title'] }}
-                        @endtypography
-                    @endif
-
-                    {!! $item['lead'] !!}
-                    
-                </div>
             @endcard
         </div>
     @endforeach
