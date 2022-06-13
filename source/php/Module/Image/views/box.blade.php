7c31c937f69145a4b23c2380e5282cdd0625720c
niclas.norin <niclas.norin@klingit.team>
2022-06-13 15:04:41 +0200

    Removes underline from text under image

--- source/php/Module/Image/views/box.blade.php
+++ source/php/Module/Image/views/box.blade.php
@@ -2,6 +2,8 @@
     @if (isset($mod_image_link_url) && strlen($mod_image_link_url) > 0)
             @link([
                 'href' => $mod_image_link_url
+                'classList' => ['u-no-decoration'],
+
             ])
                 @image([
                     'src'=> $img_src,
