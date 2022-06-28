a8975c2024d02a7ef6179357bdba6bb9f256d261
Erik Granström <erik.granstrom@helsingborg.se>
2022-06-28 15:25:36 +0200

    feat: make script module block compatible

--- source/php/Module/Script/Script.php
+++ source/php/Module/Script/Script.php
@@ -6,7 +6,7 @@ class Script extends \Modularity\Module
 {
     public $slug = 'script';
     public $supports = array();
-    public $isBlockCompatible = false;
+    public $isBlockCompatible = true;
 
     public function init()
     {
