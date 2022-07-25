717d8f01a06bc1cdceac5ba5db0d10c51cbf6bd0
niclas.norin <niclas.norin@klingit.team>
2022-07-25 15:21:30 +0200

    New way of making all tags show

--- source/php/Module/Posts/Helper/Tag.php
+++ source/php/Module/Posts/Helper/Tag.php
@@ -21,16 +21,16 @@ class Tag
         }
 
         $tags = [];
+  
         foreach ($tax as $key => $taxonomy) {
             $terms = wp_get_post_terms($postId, $taxonomy);
-        
 
             if (count($terms) > 0)  {
                 foreach ($terms as $index => $term) {
-                    $tags[$index] = [];
-                    $tags[$index]['label'] = $term->name;
-                    $tags[$index]['color'] = 'secondary';
-                    $tags[$index]['href'] = get_term_link($term->term_id);
+                    $tags[$term->name] = [];
+                    $tags[$term->name]['label'] = $term->name;
+                    $tags[$term->name]['color'] = 'secondary';
+                    $tags[$term->name]['href'] = get_term_link($term->term_id);
                 }
 
                 
@@ -38,10 +38,6 @@ class Tag
              
         }
         
-                echo "<pre>";
-                //var_dump(wp_get_post_terms( $postId, 'post_tag' ));
-                var_dump($tags);
-                echo "</pre>";
         return $tags; 
     }
 }
