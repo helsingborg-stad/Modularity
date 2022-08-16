fbe36aa920f81cb937f5e51099a14233f41a3af4
niclas.norin <niclas.norin@klingit.team>
2022-08-16 09:58:20 +0200

    Fixing code

--- source/php/Module/Iframe/assets/iframe-acceptance.js
+++ source/php/Module/Iframe/assets/iframe-acceptance.js
@@ -1,31 +1,28 @@
-const accepted = localStorage.getItem('iframeAccepted');
+const accepted = localStorage.getItem('iframeAccepted') === 'accepted';
 const iframeContainers = document.querySelectorAll('[iframe-container]');
 const buttons = document.querySelectorAll('[data-js-toggle-trigger="show-iframe"]');
-console.log(iframeContainers);
 
-for (const button of buttons) {
-    button.addEventListener('click', showIframe);
+if(!accepted){
+    for (const button of buttons) {
+        button.addEventListener('click', showIframe);
+    }
 }
 
-if (accepted === "accepted"){
+if (accepted){
     showIframe();
 }
 
 function showIframe () {
-    if(!accepted) {
-        localStorage.setItem('iframeAccepted', "accepted");
-    }
 
+    localStorage.setItem('iframeAccepted', "accepted");
+    
     for (let iframeContainer of iframeContainers) {
         let iframeAcceptanceWrapper = iframeContainer.querySelector('[iframe-acceptance-wrapper]');
         let iframe = iframeContainer.querySelector('iframe');
         iframeAcceptanceWrapper.style.display = 'none';
         iframe.style.filter = "none";
         iframe.style.pointerEvents = "auto";
-        console.log(accepted);
-   
-    }
-    
+    } 
 }
 
 
