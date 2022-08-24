634549b1e798e35b12d8bb0612c2938041ac3558
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2022-08-24 08:31:46 +0200

    Make query more specific to embed wrappers.

--- source/php/Module/Video/assets/video.js
+++ source/php/Module/Video/assets/video.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const embedPosters = document.querySelectorAll('.embed__poster');
+    const embedPosters = document.querySelectorAll('.embed .embed__poster');
     embedPosters.forEach((embedPoster) => {
         embedPoster.addEventListener('click', () => {
             embedPoster.parentElement.innerHTML = document.querySelector('#' + embedPoster.dataset.embedId).innerHTML;
