e92e527169964c42fc46d58bdfd8d3e856b0b98e
NiclasNorin <niclas.norin@frontwalker.se>
2022-08-30 11:24:34 +0200

    Removed files from pr

--- source/php/Module/Iframe/views/iframe.blade.php
+++ source/php/Module/Iframe/views/iframe.blade.php
@@ -8,4 +8,4 @@
     @endtypography
 @endif
 
-<iframe loading="lazy" src="{{ $url }}" height="{{$height}}" width="100%" title="{!! $description ?? apply_filters('the_title', $post_title) !!}" frameborder="0"></iframe>
+<iframe src="{{ $url }}" height="{{$height}}" width="100%" title="{!! $description ?? apply_filters('the_title', $post_title) !!}" frameborder="0"></iframe>
