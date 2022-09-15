f0bb350087da12f6b0f22fc0fcafdcd055ff9690
Anna <anna@thehappyhipster.se>
2022-09-15 15:14:16 +0200

    Use the new iframe component in the iframe module

--- source/php/Module/Iframe/views/iframe.blade.php
+++ source/php/Module/Iframe/views/iframe.blade.php
@@ -7,5 +7,9 @@
         {!! apply_filters('the_title', $post_title) !!}
     @endtypography
 @endif
-
-<iframe src="{{ $url }}" height="{{$height}}" width="100%" title="{!! $description ?? apply_filters('the_title', $post_title) !!}" frameborder="0"></iframe>
+@iframe([
+	'src' => $url,
+	'height' => $height,
+	'title' => $description ?? $post_title,
+])
+@endiframe
