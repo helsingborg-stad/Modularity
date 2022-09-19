195a1e25d762d095fc2eb76e9eff537ebdbf99c2
Cedrik von Heiroth <64852452+cedrikvonheiroth@users.noreply.github.com>
2022-09-19 16:29:21 +0200

    Update iframe.blade.php

--- source/php/Module/Iframe/views/iframe.blade.php
+++ source/php/Module/Iframe/views/iframe.blade.php
@@ -9,19 +9,11 @@
 @endif
 
 @iframe([
-<<<<<<< HEAD
 	'src' => $url,
 	'height' => $height,
 	'title' => $description ?? $post_title,
-=======
-	  'src' => $url,
-	  'height' => $height,
-    'width' => '100%',
-    'frameborder' => '0',
-	  'title' => $description ?? $post_title,
->>>>>>> iframe-info-text-varibales
-    'classList' => ['js-suppressed-iframe'],
-    'data-suppressed-iframe-options' => json_encode($suppressedIframeOptions),
+    	'classList' => ['js-suppressed-iframe'],
+    	'data-suppressed-iframe-options' => json_encode($suppressedIframeOptions),
 ])
 @endiframe
 
