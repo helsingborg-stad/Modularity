48e3021a82f517e7544307cd2a60886bfed6c3dc
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2022-10-07 13:35:36 +0200

    Fix PHP errors. Make case handling of video/image more clear. (#297)
    * Fix PHP errors. Make case handling of video/image more clear.
    
    Removes alls warnings on use of the component. Accompanied by PR on the component library.
    
    * Set default values of image focus.
    
    * Remove code-smells.
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Module/Hero/views/hero.blade.php
+++ source/php/Module/Hero/views/hero.blade.php
@@ -1,13 +1,1 @@
-@hero([
-    "classList" => $stretch ? [$class] : [],
-    "image" => $modHeroBackgroundType === 'image' ? $modHeroImage->url : false,
-    "imageFocus" => $modHeroImage->focus,
-    "video" => $modHeroBackgroundType === 'video' && isset($modHeroVideo->url) ? $modHeroVideo->url : false,
-    "size" => $modHeroSize,
-    "title" => !$hideTitle ? $postTitle : false,
-    "byline" => $modHeroByline,
-    "paragraph" => $modHeroBody,
-    "stretch" => $stretch,
-    "context" => ['hero', 'module.hero']
-])
-@endhero
+@include($type)
