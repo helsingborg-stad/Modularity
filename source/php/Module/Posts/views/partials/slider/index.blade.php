a308a1f822ac50c8cab3d76385fefd265cc65764
Cedrik <cedrikvonheiroth@gmail.com>
2022-10-25 13:31:01 +0200

    only show date if showDate is true

--- source/php/Module/Posts/views/partials/slider/index.blade.php
+++ source/php/Module/Posts/views/partials/slider/index.blade.php
@@ -1,14 +1,15 @@
 @card([
     'heading' => $post->post_title,
-    'subHeading' => 'index.blade.php',
-    'classList' => [$classes, 'u-color__text--info'],
-    'date' => $post->post_date,
+    // 'subHeading' => 'index.blade.php',
+    'classList' => ['u-color__text--info'],
+    'date' => ($post->showDate ? get_post_time( "Y-m-d H:i",  $post ) : false),
     'image' => ['src' => $post->thumbnail[0], 'alt' => $post->post_title],
     'imageFirst' => true,
-    // 'link' => $post->link,
+    'link' => $post->link,
     'containerAware' => true,
-    'hasAction' => true,
-    'tags' => $post->tags,
-    'context' => ['module.posts.slider'],
-    ])
+    // 'hasAction' => true,
+    // 'tags' => $post->tags,
+    // 'context' => ['module.posts.slider'],
+    'context' => ['module.posts.index'],
+])
 @endcard
