244b30eb0adda15c72415f8697c72c9801c8769b
Cedrik <cedrikvonheiroth@gmail.com>
2022-10-25 13:36:55 +0200

    only show excerpt if showExcerpt is true

--- source/php/Module/Posts/views/partials/slider/index.blade.php
+++ source/php/Module/Posts/views/partials/slider/index.blade.php
@@ -3,6 +3,7 @@
     'imageFirst' => true,
     'image' => ['src' => $post->thumbnail[0], 'alt' => $post->post_title],
     'heading' => ($post->showTitle? $post->post_title : false),
+    'content' => ($post->showExcerpt ? $post->post_content : false),
     // 'subHeading' => 'index.blade.php',
     'classList' => ['u-color__text--info'],
     'date' => ($post->showDate ? get_post_time( "Y-m-d H:i",  $post ) : false),
