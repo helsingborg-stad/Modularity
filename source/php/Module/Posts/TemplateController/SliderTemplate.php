307ef2d796f919ed53935e7ea1eef04770d90163
Cedrik <cedrikvonheiroth@gmail.com>
2022-10-25 10:30:23 +0200

    get the value from post_columns and use it in slides_per_page

--- source/php/Module/Posts/TemplateController/SliderTemplate.php
+++ source/php/Module/Posts/TemplateController/SliderTemplate.php
@@ -19,13 +19,15 @@ class SliderTemplate extends AbstractController
         $this->data = $data;
 
         $fields = json_decode(json_encode(get_fields($this->module->ID)));
+        
+        $postsColumnsInt = (int) str_replace('grid-md-'," ", $fields->posts_columns);
 
-        $this->data['slider']['slidesPerPage'] = isset($fields->slides_per_page) ? (int) $fields->slides_per_page: 4;
+        $this->data['slider']['slidesPerPage'] = 12 / $postsColumnsInt;
         $this->data['slider']['autoSlide']     = isset($fields->auto_slide) ? (bool) $fields->auto_slide    : false;
         $this->data['slider']['showStepper']   = isset($fields->show_stepper) ? (bool) $fields->show_stepper: false;
         $this->data['slider']['repeatSlide']   = isset($fields->repeat_slide) ? (bool) $fields->repeat_slide: true;
         $this->data['postsDisplayAs'] = $fields->posts_display_as;
-
+        
         $this->data['slider'] = apply_filters(
             'Modularity/Module/Posts/Slider/Arguments',
             (object) $this->data['slider']
