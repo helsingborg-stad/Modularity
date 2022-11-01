021c45c9a84f476403dae7964ecb486dca605fa3
Anna Johansson <anna@thehappyhipster.se>
2022-11-01 15:23:49 +0100

    removed commented code

--- source/php/Module/Script/Script.php
+++ source/php/Module/Script/Script.php
@@ -53,12 +53,6 @@ class Script extends \Modularity\Module
             }
         }
         
-
-        // $data['embed'] = is_admin() ? '<pre>' . htmlspecialchars($embed) . '</pre>' : $embed;
-        // (str_contains($embed, "<script") && !str_contains($embed, "defer")
-        // ? str_replace("<script", "<script defer", $embed)
-        // : $embed));
-                
         $data['scriptWrapWithClassName'] = get_field('script_wrap_with', $this->ID) ?? 'card';
 
         $placeholder = get_field('embedded_placeholder_image', $this->ID);
