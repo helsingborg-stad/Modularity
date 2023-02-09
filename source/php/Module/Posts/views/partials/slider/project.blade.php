b3a16f0314f171d3a1913020ab3f115aceaacaf8
NiclasNorin <niclas.norin@frontwalker.se>
2023-02-09 08:41:31 +0100

    if check

--- source/php/Module/Posts/views/partials/slider/project.blade.php
+++ source/php/Module/Posts/views/partials/slider/project.blade.php
@@ -12,6 +12,7 @@
     'classList' => ['u-height--100', 'project-card']
 ])
     @slot('afterContent')
+    @if($post->project->statusBar['value'])
         <div class="u-margin__top--auto u-width--100">
             {{-- TODO: fix tooltip overflow --}}
             @tooltip([
@@ -27,5 +28,6 @@
             ])
             @endprogressBar
         </div>
+        @endif
     @endslot
 @endcard
