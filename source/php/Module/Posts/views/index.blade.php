32adc34a1d475dbec10e3b2089b9a113ee3173f7
NiclasNorin <niclas.norin@frontwalker.se>
2023-02-27 17:32:05 +0100

    reset this line

--- source/php/Module/Posts/views/index.blade.php
+++ source/php/Module/Posts/views/index.blade.php
@@ -74,7 +74,7 @@
     @endforeach
 </div>
 
-@if ($posts_data_source !== 'input' && isset($archive_link) && $archive_link && $archive_link_url)
+@if ($posts_data_source !== 'input' && $archive_link_url)
     <div class="t-read-more-section u-display--flex u-align-content--center u-margin__y--4">
         @button([
             'text' => __('Show more', 'modularity'),
