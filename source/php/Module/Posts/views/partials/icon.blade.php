a2aac6e16388bf795078beda0bde539be2f7cdb8
Anna <anna@thehappyhipster.se>
2023-04-13 13:39:46 +0200

    Use first event date as post date if event (#403)

--- source/php/Module/Posts/views/partials/icon.blade.php
+++ source/php/Module/Posts/views/partials/icon.blade.php
@@ -1,8 +1,8 @@
 @icon([
-    'icon' => $post->floatingIcon['icon'],
+    'icon' => $post->floatingIcon['icon'] ?? '',
     'size' => $post->floatingIcon['size'] ?? 'md',
     'filled' => $post->floatingIcon['filled'] ?? false,
-    'attributeList' => $post->floatingIcon['attributeList'],
-    'classList' => $post->floatingIcon['classList'],
+    'attributeList' => $post->floatingIcon['attributeList'] ?? [],
+    'classList' => $post->floatingIcon['classList'] ?? []
 ])
 @endicon
