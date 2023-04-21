37866f21040d7e1bd596a91070846fbe883595e4
Anna Johansson <anna@thehappyhipster.se>
2023-04-21 10:35:32 +0200

    Made post-title and preamble into partials

--- source/php/Module/Posts/views/collection.blade.php
+++ source/php/Module/Posts/views/collection.blade.php
@@ -1,84 +1,71 @@
 @include('partials.post-filters')
 
-@if (!$hideTitle && !empty($postTitle))
-    @typography([
-        'id' => 'mod-posts-' . $ID . '-label',
-        'element' => 'h2', 
-        'variant' => 'h2', 
-        'classList' => ['module-title']
-    ])
-        {!! $postTitle !!}
-    @endtypography
-@endif
+@includeWhen(!$hideTitle && !empty($postTitle), 'partials.post-title')
+@includeWhen($preamble, 'partials.preamble')
 
-@if ($preamble)
-    @typography([
-        'classList' => ['module-preamble', 'u-margin__bottom--3'] 
-    ])
-        {!! $preamble !!}
-    @endtypography
-@endif
-
-    @collection([
-        'classList' => ['c-collection--posts', 'o-grid'],
-    ])
-        @foreach ($posts as $post)
-        <div class="{{$posts_columns}}">
+@collection([
+    'classList' => ['c-collection--posts', 'o-grid']
+])
+    @foreach ($posts as $post)
+        <div class="{{ $posts_columns }}">
             @collection__item([
                 'link' => $post->link,
                 'classList' => ['c-collection__item--post'],
                 'context' => ['module.posts.collection__item'],
-                'containerAware' => true,
-        ])
-            @slot('floating')
-                @include('partials.icon')
-            @endslot
-            @slot('before')
-            @if($post->showImage && isset($post->thumbnail[0]))
-                @image([
-                    'src' => $post->thumbnail[0] ,
-                    'alt' => $post->post_title,
-                    'classList' => ['u-width--100'],
-                ])
-                @endimage
-            @endif
-            @endslot
+                'containerAware' => true
+            ])
+                @slot('floating')
+                    @include('partials.icon')
+                @endslot
+                @slot('before')
+                    @if ($post->showImage && isset($post->thumbnail[0]))
+                        @image([
+                            'src' => $post->thumbnail[0],
+                            'alt' => $post->post_title,
+                            'classList' => ['u-width--100']
+                        ])
+                        @endimage
+                    @endif
+                @endslot
                 @group([
                     'direction' => 'vertical'
                 ])
-                @group([
-                    'justifyContent' => 'space-between'
-                ])
-                    @typography([
-                        'element' => 'h2',
-                        'variant' => 'h3',
+                    @group([
+                        'justifyContent' => 'space-between'
                     ])
-                        {{$post->showTitle ? $post->post_title : false}}
-                    @endtypography
-                    @if($post->termIcon)
-                    <div class="c-collection__item__icon">
-                        @inlineCssWrapper([
-                            'styles' => ['background-color' => $post->termIcon['backgroundColor'], 'display' => 'flex'],
-                            'classList' => [$post->termIcon['backgroundColor'] ? '' : 'u-color__bg--primary', 'u-rounded--full', 'u-detail-shadow-3']
+                        @typography([
+                            'element' => 'h2',
+                            'variant' => 'h3'
                         ])
-                            @icon($post->termIcon)
-                            @endicon
-                        @endinlineCssWrapper
-                    </div>
-                    @endif
-                @endgroup
-                @tags([
-                    'tags' => $post->tags,
-                    'classList' => ['u-padding__y--2'],
-                    'format' => false,
-                ])
-                @endtags
-                @typography([])
-                    {!! $post->showExcerpt ? $post->post_content : false !!}
-                @endtypography
-
+                            {{ $post->showTitle ? $post->post_title : false }}
+                        @endtypography
+                        @if ($post->termIcon)
+                            <div class="c-collection__item__icon">
+                                @inlineCssWrapper([
+                                    'styles' => ['background-color' => $post->termIcon['backgroundColor'], 'display' => 'flex'],
+                                    'classList' => [
+                                        $post->termIcon['backgroundColor'] ? '' : 'u-color__bg--primary',
+                                        'u-rounded--full',
+                                        'u-detail-shadow-3'
+                                    ]
+                                ])
+                                    @icon($post->termIcon)
+                                    @endicon
+                                @endinlineCssWrapper
+                            </div>
+                        @endif
+                    @endgroup
+                    @tags([
+                        'tags' => $post->tags,
+                        'classList' => ['u-padding__y--2'],
+                        'format' => false
+                    ])
+                    @endtags
+                    @typography([])
+                        {!! $post->showExcerpt ? $post->post_content : false !!}
+                    @endtypography
                 @endgroup
             @endcollection__item
         </div>
-        @endforeach
-    @endcollection
+    @endforeach
+@endcollection
--- source/php/Module/Posts/views/horizontal.blade.php
+++ source/php/Module/Posts/views/horizontal.blade.php
@@ -1,29 +1,12 @@
 @include('partials.post-filters')
 
-<div class="{{ $classes }} posts-{{$posts_display_as}}">
+<div class="{{ $classes }} posts-{{ $posts_display_as }}">
 
-    @if (!$hideTitle && !empty($postTitle))
-
-        @typography([
-            'element' => 'h2', 
-            'variant' => 'h2', 
-            'classList' => ['module-title']
-        ])
-            {!! $postTitle !!}
-        @endtypography
-
-    @endif
-
-    @if ($preamble)
-        @typography([
-            'classList' => ['module-preamble', 'u-margin__bottom--3'] 
-        ])
-            {!! $preamble !!}
-        @endtypography
-    @endif
+    @includeWhen(!$hideTitle && !empty($postTitle), 'partials.post-title')
+    @includeWhen($preamble, 'partials.preamble')
 
     @if (count($posts) > 0)
-        <div class="o-grid grid--columns js-mod-posts-{{$ID}} {{ $stretch ? 'o-grid--stretch' : '' }}">
+        <div class="o-grid grid--columns js-mod-posts-{{ $ID }} {{ $stretch ? 'o-grid--stretch' : '' }}">
             @foreach ($posts as $post)
                 @if ($loop->first && get_field('posts_highlight', $ID))
                     <div class="o-grid-12@xs">
@@ -31,68 +14,61 @@
                         @link([
                             'href' => apply_filters('Modularity/Module/Posts/Permalink', get_permalink($post), $post)
                         ])
-
                             <article class="full u-mb-2">
 
                                 @if (in_array('image', $posts_fields) && $post->image)
-
                                     @image([
-                                        'src'=> $post->image,
+                                        'src' => $post->image,
                                         'alt' => $post->post_title,
-                                        'clasList' => ['u-mb-3','u-w-100','u-pb-0']
+                                        'clasList' => ['u-mb-3', 'u-w-100', 'u-pb-0']
                                     ])
                                     @endimage
-
                                 @endif
 
                                 @if (in_array('title', $posts_fields))
-
                                     @typography([
-                                        'element' => "h2",
-                                        'variant' => "h2"
+                                        'element' => 'h2',
+                                        'variant' => 'h2'
                                     ])
-                                        {{$post->post_title}}
+                                        {{ $post->post_title }}
                                     @endtypography
-
                                 @endif
 
                                 @if (in_array('date', $posts_fields))
-
-                                    <time datetime="{{get_the_time(get_option('date_format'), $post->ID) . ' ' . get_the_time(get_option('time_format'), $post->ID)}}">
+                                    <time
+                                        datetime="{{ get_the_time(get_option('date_format'), $post->ID) . ' ' . get_the_time(get_option('time_format'), $post->ID) }}">
                                         @if ($post->humanReadableTime)
-                                            {{$post->humanReadableTime}}
+                                            {{ $post->humanReadableTime }}
                                         @else
                                             {{ apply_filters('Modularity/Module/Posts/Date', get_the_time(get_option('date_format'), $post->ID) . ' ' . get_the_time(get_option('time_format'), $post->ID), $post->ID, $post->post_type, $posts_display_as) }}
                                         @endif
                                     </time>
-
                                 @endif
 
                                 @if (is_array($post->terms) && !empty($post->terms))
-
                                     @foreach ($post->terms as $term)
-
                                         @typography([
-                                            'element' => "span"
+                                            'element' => 'span'
                                         ])
-                                            - {{$term->name}}
+                                            - {{ $term->name }}
                                         @endtypography
-
                                     @endforeach
-
                                 @endif
 
-                                {!! isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', wp_trim_words(wp_strip_all_tags(strip_shortcodes(get_extended($post->post_content)['main'])), 45, null)) : '' !!}
+                                {!! isset(get_extended($post->post_content)['main'])
+                                    ? apply_filters(
+                                        'the_excerpt',
+                                        wp_trim_words(wp_strip_all_tags(strip_shortcodes(get_extended($post->post_content)['main'])), 45, null),
+                                    )
+                                    : '' !!}
 
                             </article>
-
                         @endlink
 
                     </div>
                 @else
                     @include('partials.post.post-horizontal')
                 @endif
-
             @endforeach
         </div>
 
@@ -104,18 +80,18 @@
                         'text' => 'Secondary',
                         'color' => 'default',
                         'style' => 'filled',
-                        'attributeList' => ['data-mod-posts-load-more' =>
-                        base64_encode($loadMorePostsAttributes)],
-                        'classList' => ['js-mod-posts-load-more']
+                        'attributeList' => [
+                            'data-mod-posts-load-more' => base64_encode($loadMorePostsAttributes),
+                        ],
+                        'classList' => ['js-mod-posts-load-more'],
                     ])
-                        {{$loadMoreButtonText}}
+                    {{ $loadMoreButtonText }}
                     @endbutton
 
                 </div>
             </div>
         @endif
     @else
-
         <section>
             <?php _e('Nothing to display…', 'modularity'); ?>
         </section>
