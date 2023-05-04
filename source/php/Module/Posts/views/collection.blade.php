09559184af7113696b9398a5c81dd46d9a47b390
NiclasNorin <niclas.norin@frontwalker.se>
2023-05-04 16:48:59 +0200

    horizontal not vertical

--- source/php/Module/Posts/views/collection.blade.php
+++ source/php/Module/Posts/views/collection.blade.php
@@ -3,7 +3,7 @@
 @includeWhen(!$hideTitle && !empty($postTitle), 'partials.post-title')
 @includeWhen($preamble, 'partials.preamble')
 @collection([
-    'classList' => ['c-collection', 'o-grid', 'o-grid--vertical'],
+    'classList' => ['c-collection', 'o-grid', 'o-grid--horizontal'],
 ])
     @foreach ($posts as $post)
         @include('partials.post.collection-item')
