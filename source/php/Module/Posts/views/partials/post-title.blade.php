b28f50838083cd15062cc321fcb7fba6e2acc4d3
NiclasNorin <niclas.norin@frontwalker.se>
2023-05-12 11:36:16 +0200

    fixing the titles having a padding for normal posts

--- source/php/Module/Posts/views/partials/post-title.blade.php
+++ source/php/Module/Posts/views/partials/post-title.blade.php
@@ -1,10 +1,8 @@
-<div class="c-card__header">
-    @typography([
-        'id' => 'mod-posts-' . $ID . '-label',
-        'element' => $element ?? 'h2',
-        'variant' => $variant ?? 'h4',
-        'classList' => $classList ?? []
-    ])
-        {!! $postTitle !!}
-    @endtypography
-</div>
+@typography([
+    'id' => 'mod-posts-' . $ID . '-label',
+    'element' => $element ?? 'h2',
+    'variant' => $variant ?? 'h4',
+    'classList' => $classList ?? []
+])
+    {!! $postTitle !!}
+@endtypography
