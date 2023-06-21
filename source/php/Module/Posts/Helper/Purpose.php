47c16d8cc5b7da986c62485c9d3e42dd9cef3555
Anna Johansson <anna@thehappyhipster.se>
2023-06-21 09:21:35 +0200

    Removed unnecessary variable

--- source/php/Module/Posts/Helper/Purpose.php
+++ source/php/Module/Posts/Helper/Purpose.php
@@ -7,7 +7,6 @@ class Purpose
     public static function getPurpose(string $type = '')
     {
         if (class_exists('\Municipio\Helper\Purpose')) {
-            $purpose = \Municipio\Helper\Purpose::getPurpose($type);
             if ($purpose = \Municipio\Helper\Purpose::getPurpose($type)) {
                 return $purpose->getKey();
             }
