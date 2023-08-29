9102108bf678154bae70519a61a4b0572de4b211
Niclas Norin <niclas.norin@helsingborg.se>
2023-08-29 11:31:34 +0200

    Fix: Map error notices

--- source/php/Module/Map/Map.php
+++ source/php/Module/Map/Map.php
@@ -71,14 +71,14 @@ if(!empty($fields['osm_markers']) && is_array($fields['osm_markers'])) {
 
         //Create data array
         $data['map_url']            = $map_url;
-        $data['map_description']    = $fields['map_description'];
+        $data['map_description']    = !empty($fields['map_description']) ? $fields['map_description'] : '';
         
-        $data['show_button']        = $fields['show_button'];
-        $data['button_label']       = $fields['button_label'];
-        $data['button_url']         = $fields['button_url'];
-        $data['more_info_button']   = $fields['more_info_button'];
-        $data['more_info']          = $fields['more_info'];
-        $data['more_info_title']    = $fields['more_info_title'];
+        $data['show_button']        = !empty($fields['show_button']) ? $fields['show_button'] : false;
+        $data['button_label']       = !empty($fields['button_label']) ? $fields['button_label'] : false;
+        $data['button_url']         = !empty($fields['button_url']) ? $fields['button_url'] : false;
+        $data['more_info_button']   = !empty($fields['more_info_button']) ? $fields['more_info_button'] : false;
+        $data['more_info']          = !empty($fields['more_info']) ? $fields['more_info'] : false;
+        $data['more_info_title']    = !empty($fields['more_info_title']) ? $fields['more_info_title'] : false;
 
         $data['cardMapCss']         = ($data['more_info_button']) ? 'o-grid-12@xs o-grid-8@md' : 'o-grid-12@md';
         $data['cardMoreInfoCss']    = ($data['more_info_button']) ? 'o-grid-12@xs o-grid-4@md' : '';
