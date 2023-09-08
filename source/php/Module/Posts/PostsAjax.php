4b859f130fb4c131680fc1f71e5d746d28542a3f
Niclas Norin <niclas.norin@helsingborg.se>
2023-09-08 16:30:03 +0200

    Refactor: Remove ajax

--- source/php/Module/Posts/PostsAjax.php
+++ source/php/Module/Posts/PostsAjax.php
@@ -17,10 +17,6 @@ class PostsAjax {
         add_action('wp_ajax_get_taxonomy_types_v2', array($this, 'getTaxonomyTypes'));
         add_action('wp_ajax_get_taxonomy_values_v2', array($this, 'getTaxonomyValues'));
         add_action('wp_ajax_get_sortable_meta_keys_v2', array($this, 'getSortableMetaKeys'));
-
-        add_action('wp_ajax_mod_posts_load_more', array($this, 'loadMorePostsUsingAjax'));
-        add_action('wp_ajax_nopriv_mod_posts_load_more', array($this, 'loadMorePostsUsingAjax'));
-
         add_action('wp_ajax_mod_posts_get_date_source', array($this, 'loadDateFieldAjax'));
     }
 
@@ -33,83 +29,6 @@ class PostsAjax {
         }
 
         wp_send_json($this->posts->getDateSource($postType));
-    }
-
-        /**
-     * Load more data with Ajax
-     * @return json data
-     */
-    public function loadMorePostsUsingAjax()
-    {
-        if (!defined('DOING_AJAX') || !DOING_AJAX) {
-            return false;
-            die;
-        }
-
-        //Nonce
-        if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'mod-posts-load-more')) {
-            die('Busted!');
-        }
-
-        $statusCodes = [
-            'badRequest' => 400,
-            'noContent' => 204,
-            'success' => 200
-        ];
-
-        //Make sure required post data exists
-        $requiredPostDataKeys = ['postsPerPage', 'offset', 'module', 'bladeTemplate'];
-        foreach ($requiredPostDataKeys as $key) {
-            if (!isset($_POST[$key])) {
-                $error = 'Missing required $_POST[' . $key . '].';
-                error_log($error);
-                wp_send_json_error(['error' => $error], $statusCodes['badRequest']);
-                die;
-            }
-        }
-
-        //Append class propeties
-        foreach ($_POST['module'] as $key => $value) {
-            $this->$key = $value;
-        }
-
-        //Get posts
-        $args = Posts::getPostArgs($this->ID);
-        $args['posts_per_page'] = $_POST['postsPerPage'];
-        $args['offset'] = $_POST['offset'];
-        $this->data['posts'] = get_posts($args);
-
-        $this->posts->getTemplateData(
-            Posts::replaceDeprecatedTemplate(
-                $this->data['posts_display_as']
-            )
-        ); //Include template controller data
-
-        //No posts
-        if (empty($this->data['posts'])) {
-            wp_send_json(['Message' => 'Could not find more posts'], $statusCodes['noContent']);
-            die;
-        }
-
-        $moduleView = MODULARITY_PATH . 'source/php/Module/Posts/views';
-        $init = new CompLibInitator([$moduleView]);
-        $blade = $init->getEngine();
-        $posts = [];
-
-        foreach ($this->data['posts'] as $post) {
-
-            try {
-                $posts[] = $blade->make($_POST['bladeTemplate'], array_merge(['post' => $post], $this->data))->render();
-            } catch (Throwable $e) {
-                echo '<pre style="border: 3px solid #f00; padding: 10px;">';
-                echo '<strong>' . $e->getMessage() . '</strong>';
-                echo '<hr style="background: #000; outline: none; border:none; display: block; height: 1px;"/>';
-                echo $e->getTraceAsString();
-                echo '</pre>';
-            }
-        }
-
-        wp_send_json($posts);
     }
 
         /**
