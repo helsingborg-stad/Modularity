bd7747cb820d6095fae2b0c88fb8e3400d2400f1
Niclas Norin <niclas.norin@helsingborg.se>
2023-09-13 11:11:10 +0200

    Fix: Block view not working without out div

--- source/php/Module/ManualInput/views/box.blade.php
+++ source/php/Module/ManualInput/views/box.blade.php
@@ -1,14 +1,15 @@
 <div class="o-grid{{ !empty($stretch) ? ' o-grid--stretch' : '' }}">
     @foreach ($manualInputs as $input)
+    <div class="{{$columns}}">
             @box([
                 'heading'   => $input['title'],
                 'content'   => $input['content'],
                 'link'      => $input['link'],
                 'ratio'     => $blockBoxRatio,
                 'image'     => $input['image'],
-                'classList' => [$columns],
                 'context'   => $context
             ])
             @endbox
+    </div>
     @endforeach
 </div>
