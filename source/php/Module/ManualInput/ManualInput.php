350b51d6640cbf904baa817c979ab3b382b4510a
Niclas Norin <niclas.norin@helsingborg.se>
2023-09-13 16:49:22 +0200

    Fix: Change requests

--- source/php/Module/ManualInput/ManualInput.php
+++ source/php/Module/ManualInput/ManualInput.php
@@ -23,37 +23,64 @@ class ManualInput extends \Modularity\Module
     {
         $data   = [];
         $fields = $this->getFields();
-        $this->template = !empty($fields['display_as']) ? $fields['display_as'] : 'card';
+        $this->template = $this->getTemplateToUse($fields);
 
         $data['manualInputs'] = [];
         $data['columns'] = !empty($fields['columns']) ? $fields['columns'] . '@md' : 'o-grid-4@md';
         $data['context'] = ['module.manual-input.' . $this->template];
-
+        $data['blockBoxRatio'] = !empty($fields['ratio']) ? $fields['ratio'] : '4:3';
         $data['accordionColumnTitles'] = $this->createAccordionTitles(
             isset($fields['accordion_column_titles']) ? $fields['accordion_column_titles'] : [], 
             isset($fields['accordion_column_marking']) ? $fields['accordion_column_marking'] : ''
         );
 
-        $data['blockBoxRatio'] = !empty($fields['ratio']) ? $fields['ratio'] : '4:3';
+        $manualInputDefaultValues = $this->getManualInputDefaultValues();
 
-        if (!empty($fields['manual_inputs'])) {
+        if (!empty($fields['manual_inputs']) && is_array($fields['manual_inputs'])) {
             foreach ($fields['manual_inputs'] as $input) {
-                $data['manualInputs'][] = [
-                    'title'     => !empty($input['title']) ? $input['title'] : false,
-                    'content'   => !empty($input['content']) ? $input['content'] : false,
-                    'link'      => !empty($input['link']) ? $input['link'] : false,
-                    'linkText'  => !empty($input['link_text']) ? $input['link_text'] : __("Read more", 'modularity'),
-                    'image'     => !empty($input['image']['sizes']['medium_large']) ? [
-                        'src'   => $input['image']['sizes']['medium_large'],
-                        'alt'   => !empty($input['image']['alt']) ? $input['image']['alt'] : '',
-                        ] : [],
-                    'imageBeforeContent' => isset($input['image_before_content']) ? $input['image_before_content'] : true,
-                    'accordionColumnValues' => $this->createAccordionTitles($input['accordion_column_values'], $input['title']),
-                ];
+                $arr = array_merge($this->getManualInputDefaultValues(), $input);
+                $arr['image'] = $this->getImage($arr['image']);
+                $arr['accordion_column_values'] = $this->createAccordionTitles($arr['accordion_column_values'], $arr['title']);
+
+                if (class_exists('\Municipio\Helper\FormatObject')) {
+                    $arr = \Municipio\Helper\FormatObject::camelCase($arr);
+                }
+
+                $data['manualInputs'][] = $arr;
             }
         }
 
         return $data;
+    }
+
+    /**
+     * @return array
+     */
+    private function getManualInputDefaultValues() {
+        return [
+            'title'                     => false,
+            'content'                   => false,
+            'link'                      => false,
+            'link_text'                 => __("Read more", 'modularity'),
+            'image'                     =>  [],
+            'image_before_content'      => true,
+            'accordion_column_values'   => []
+        ];
+    }
+
+    /**
+     * @param array $imageData Data surrounding the image.
+     * @return array
+     */
+    private function getImage($imageData) {
+        if (!empty($imageData)) {
+            $image = [
+                'src' => wp_get_attachment_image_src($imageData['id'], 'full')[0],
+                'alt' => !empty($imageData['alt']) ? $imageData['alt'] : false
+            ];
+            return $image;
+        }
+        return [];  
     }
 
      /**
@@ -68,7 +95,7 @@ class ManualInput extends \Modularity\Module
                 $titles[] = is_string($accordionColumnMarker) ? $accordionColumnMarker : __('Title', 'Modularity');
             }
 
-            if (!empty($accordionColumnTitles)) {
+            if (!empty($accordionColumnTitles) && is_array($accordionColumnTitles)) {
                 foreach ($accordionColumnTitles as $accordionColumnTitle) {
                     $titles = array_merge($titles, array_values($accordionColumnTitle));
                 }
@@ -94,6 +121,15 @@ class ManualInput extends \Modularity\Module
         }
         return $viewData;
     }
+
+    /**
+     * @param string $template name of the view
+     * @return string
+     */
+    public function getTemplateToUse($fields) {
+        return apply_filters('Modularity/Module/ManualInput/Template', !empty($fields['display_as']) ? $fields['display_as'] : 'card');
+    }
+
 
     public function template() {
         $path = __DIR__ . "/views/" . $this->template . ".blade.php";
