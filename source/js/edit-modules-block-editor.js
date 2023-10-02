71318754f18e95b9a1e24b7c710f1283297f7827
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2023-10-02 14:43:39 +0200

    feat: Reintroduce the edit module button (#498)
    * Create script according to docs.
    
    * Revert "Create script according to docs."
    
    This reverts commit c29bdcf84768cb9ac411440ff01c53e94485cf0e.
    
    * fix: Make script load in footer. Make it cachebust.
    
    * Refine method of adding edit button
    
    * fix: Use const for conts.
    
    ---------
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/js/edit-modules-block-editor.js
+++ source/js/edit-modules-block-editor.js
@@ -1,70 +1,37 @@
-/* const { link } = require("fs");
- */
-( function( window, wp ){
-    
+(function(window, wp){
+    const editModulesLinkId       = 'editModulesPageLink';
+    const {editModulesLinkLabel, editModulesLinkHref} = modularityBlockEditor;
+
+    // prepare our custom link's html.
+    const editModulesLinkHTML = `
+        <a 
+            id="${editModulesLinkId}" 
+            class="components-button has-icon"
+            aria-label="${editModulesLinkLabel}" 
+            href="${editModulesLinkHref}"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" style="margin-right: .3em;" aria-hidden="true" focusable="false">
+                <path d="M240-440h360v-80H240v80Zm0-120h360v-80H240v80Zm-80 400q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h640v-480H160v480Zm0 0v-480 480Z"/>
+            </svg>
+            ${editModulesLinkLabel}
+        </a>
+    `;
+
     // check if gutenberg's editor root element is present.
-    const editorEl = document.getElementById( 'editor' );
-    if( !editorEl ){
-        return; // do nothing if there's no gutenberg root element on page.
+    var editorEl = document.getElementById('editor');
+    if(!editorEl){
+        return;
     }
 
-    //Set default Values
-    const buttonID = 'mod-edit-modules';
-
-    //create button
-    let button = document.createElement('a');
-    button.className += ' components-button c-button c-button__outlined c-button__outlined--default c-button--sm';
-    button.setAttribute('id', buttonID);
-    button.setAttribute('href', blockeditior.hrefeditmodules);
-
-    //create label
-    let label = document.createElement('span');
-    label.classList.add = 'c-button__label';
-
-    let labelText = document.createElement('span');
-    labelText.classList.add = 'c-button__label-text';
-
-    //create text
-    const labelTextString = document.createTextNode(blockeditior.langeditmodules);
-    labelText.appendChild(labelTextString);
-
-    label.appendChild(labelText);
-    button.appendChild(label);
-
-    const unsubscribe = wp.data.subscribe( function () {
+    //Append button if not existing
+    wp.data.subscribe( function () {
         setTimeout( function () {
-            if ( !document.getElementById( buttonID ) ) {
-                const toolbalEl = editorEl.querySelector( '.edit-post-header__toolbar' );
-                if( toolbalEl instanceof HTMLElement ){
-                    toolbalEl.appendChild( button );
+            if (!document.getElementById( editModulesLinkId )) {
+                var toolbalEl = editorEl.querySelector( '.edit-post-header__toolbar' );
+                if(toolbalEl instanceof HTMLElement){
+                    toolbalEl.insertAdjacentHTML('beforeend', editModulesLinkHTML);
                 }
             }
-        }, 1 )
+        }, 1)
     } );
-    // unsubscribe is a function - it's not used right now 
-    // but in case you'll need to stop this link from being reappeared at any point you can just call unsubscribe();
-
-    const callback = function(mutationsList, observer) {
-        for(const mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                //Items has changed, check if it was a block
-                const typewriter = editorEl.querySelector('.edit-post-visual-editor');
-                const links = typewriter.querySelectorAll('[href]');
-
-                //block href from new items
-                links.forEach(link => {
-                    link.onclick = function(event) {
-                        console.log("Prevented opening of link in browser."); 
-                        event.preventDefault();
-                    }
-                })
-            }
-        }
-    };
-
-    //Add observer for editor
-    const observer = new MutationObserver(callback);
-    observer.observe(editorEl, { childList: true, subtree: true });
-
-} )( window, wp )
-
+})( window, wp )
