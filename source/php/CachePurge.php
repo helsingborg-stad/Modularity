d53122df2fa858c579729f8bedbdd349d865f7b5
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-10-10 09:48:42 +0200

    Add documentation to cache Purge.

--- source/php/CachePurge.php
+++ source/php/CachePurge.php
@@ -9,21 +9,31 @@ class CachePurge
         add_action('save_post', array($this, 'sendPurgeRequest'));
     }
 
-    public function sendPurgeRequest($post_id)
+    /**
+     * Send a purge request for a post if it's a modularity type.
+     *
+     * This function checks if a post with the given post ID is a modularity type. If it is, it sends
+     * a purge request to the master URL. Purge requests are not sent for post revisions.
+     *
+     * @param int $post_id The ID of the post to check and potentially send a purge request for.
+     * @return bool True if a purge request was sent, false otherwise (including revisions).
+     */
+    public function sendPurgeRequest($postId)
     {
 
         //Not for revisions
-        if (wp_is_post_revision($post_id)) {
+        if (wp_is_post_revision($postId)) {
             return;
         }
 
         //Check if modularity, then send purge!
-        if ($this->isModularityPost($post_id)) {
+        if ($this->isModularityPost($postId)) {
             wp_remote_request($this->getMasterUrl(),
                 array(
                     'method' => 'PURGE',
                     'timeout' => 2,
                     'redirection' => 0,
+                    'blocking' => false
                 )
             );
             return true;
@@ -32,14 +42,31 @@ class CachePurge
         return false;
     }
 
-    private function isModularityPost($post_id)
+    /**
+     * Check if a post is of a modularity type.
+     *
+     * This function determines if a post, identified by its post ID, belongs to a modularity
+     * type. Modularity types are identified by post types starting with "mod-".
+     *
+     * @param int $postId The ID of the post to check.
+     * @return bool True if the post is of a modularity type, false otherwise.
+     */
+    private function isModularityPost($postId)
     {
-        if (strpos(get_post_type($post_id), "mod-") === 0) {
+        if (strpos(get_post_type($postId), "mod-") === 0) {
             return true;
         }
         return false;
     }
 
+    /**
+     * Get the master URL for the website.
+     *
+     * If the website is not a multisite installation, it returns the home URL.
+     * If the website is part of a multisite network, it returns the site URL of the current blog.
+     *
+     * @return string The master URL of the website.
+     */
     private function getMasterUrl()
     {
         if (!is_multisite()) {
