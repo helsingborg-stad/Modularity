f8fded33865009488ff95fe05fb46aaf28fb570f
Niclas Norin <niclas.norin@helsingborg.se>
2023-12-12 13:50:30 +0100

    chore: renaming

---
+++ source/js/modularity-text-module.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", moduleModalWysiwygSelect);
+function moduleModalWysiwygSelect() {
+    setTimeout(function() {
+        const select = document.querySelector('#mceu_0-open');
+        if (select) {
+            select.addEventListener("click", () => {
+                setTimeout(function() {
+                    const parent = select.closest('.mce-toolbar-grp');
+                    const floatPanel = document.querySelector('.mce-floatpanel');
+                    if (floatPanel && parent && parent.style.position === 'fixed') {
+                        const pageOffset = window.scrollY;
+                        const selectRect = select.getBoundingClientRect();
+                        floatPanel.style.top = (pageOffset + selectRect.top - 5) + 'px';
+                        floatPanel.style.left = '0px';
+                    }
+                }, 100)
+            });
+        }
+    }, 1000); 
+}
--- source/php/App.php
+++ source/php/App.php
@@ -279,9 +279,9 @@ class App
         . \Modularity\Helper\CacheBust::name('js/dynamic-map-acf.js'));
         wp_enqueue_script('dynamic-map-acf');
 
-        wp_register_script('modularity-editor-wysiwyg', MODULARITY_URL . '/dist/'
-        . \Modularity\Helper\CacheBust::name('js/modularity-editor-wysiwyg.js'));
-        wp_enqueue_script('modularity-editor-wysiwyg');
+        wp_register_script('modularity-text-module', MODULARITY_URL . '/dist/'
+        . \Modularity\Helper\CacheBust::name('js/modularity-text-module.js'));
+        wp_enqueue_script('modularity-text-module');
 
         add_action('admin_head', function () {
             echo "
