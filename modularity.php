9ab915f26a31e5c3e9ff2fff2d7ff75e19f5662a
Niclas <niclasn@outlook.com>
2024-01-15 09:00:03 +0100

    Revert "build: update package file versions to 5.20.3"
    This reverts commit 6a58cb4f954bf8a94403373e6980a43371cbc621.

--- modularity.php
+++ modularity.php
@@ -4,7 +4,7 @@
  * Plugin Name: Modularity
  * Plugin URI: -
  * Description: Modular component system for WordPress
- * Version: 5.20.3
+ * Version: 5.20.2
  * Author: Kristoffer Svanmark, Sebastian Thulin
  * Author URI: -
  * Text domain: modularity
