51de63da6d1d6936ff684c1d3ec35e3129a0a69c
Niclas <niclasn@outlook.com>
2024-01-26 14:51:01 +0100

    fix: view as well

--- source/php/Module/Posts/views/expandable-list.blade.php
+++ source/php/Module/Posts/views/expandable-list.blade.php
@@ -64,6 +64,10 @@
                             @if (isset($accordionItem['column_values']) &&
                                     is_array($accordionItem['column_values']) &&
                                     !empty($accordionItem['column_values']))
+                                @if ($posts_hide_title_column)
+                                    @php $accordionItem['heading'] = []; @endphp
+                                @endif
+
                                 @accordion__item([
                                     'heading' => $accordionItem['column_values']
                                         ? array_merge((array) $accordionItem['heading'], (array) $accordionItem['column_values'])
