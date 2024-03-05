60c4233c0217cb8570a47ee53c82f650ac62b40f
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-03-05 14:39:37 +0100

    fix: cache bust module refresh for every 15 second.

--- source/js/helpers/ModulesRestAPI.js
+++ source/js/helpers/ModulesRestAPI.js
@@ -7,15 +7,17 @@ export class ModulesRestAPI {
     fetcher = {};
     endpoints = {};
     nonce = '';
+    cacheBustKey = 0;
 
     constructor(fetcher, endpoints, nonce) {
         this.fetcher = fetcher;
         this.endpoints = endpoints;
         this.nonce = nonce;
+        this.cacheBustKey = Math.floor(Date.now() / 15000);
     }
 
     async getModule(moduleID) {
-        const url = `${this.endpoints.getModule}/${moduleID}?_wpnonce=${this.nonce}`;
+        const url = `${this.endpoints.getModule}/${moduleID}?_wpnonce=${this.nonce}&cacheBust=${this.cacheBustKey}`;
         const response = await this.fetcher(url);
 
         if (!response.ok) {
