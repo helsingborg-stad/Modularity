564df65506976c9226503865bdef9c8478b02d1b
NiclasNorin <niclasn@outlook.com>
2024-03-13 09:38:05 +0100

    style: lint

--- source/php/Upgrade.php
+++ source/php/Upgrade.php
@@ -207,41 +207,69 @@ class Upgrade
     {        
         $this->migrateBlockFieldsValueToNewFields('acf/posts', [
                 'posts_display_as' => [
-                    'name' => 'display_as', 
-                    'key' => 'field_64ff23d0d91bf',
-                    'type' => 'replaceValue', 
-                    'values' => [
-                        'list' => 'list', 
-                        'expandable-list' => 'accordion', 
-                        'items' => 'card', 
-                        'news' => 'card', 
-                        'index' => 'card', 
-                        'segment' => 'segment', 
-                        'collection' => 'collection', 
-                        'features-grid' => 'box', 
-                        'grid' => 'block', 
-                        'default' => 'card'
+                    'name'      => 'display_as', 
+                    'key'       => 'field_64ff23d0d91bf',
+                    'type'      => 'replaceValue', 
+                    'values'    => [
+                        'list'              => 'list', 
+                        'expandable-list'   => 'accordion', 
+                        'items'             => 'card', 
+                        'news'              => 'card', 
+                        'index'             => 'card', 
+                        'segment'           => 'segment', 
+                        'collection'        => 'collection', 
+                        'features-grid'     => 'box', 
+                        'grid'              => 'block', 
+                        'default'           => 'card'
                     ],     
                 ],
                 'data' => [
-                    'name' => 'manual_inputs', 
-                    'key' => 'field_64ff22b2d91b7',
-                    'type' => 'repeater', 
-                    'fields' => [
-                        'post_title' => ['name' => 'title', 'key' => 'field_64ff22fdd91b8'], 
-                        'post_content' => ['name' => 'content', 'key' => 'field_64ff231ed91b9'],
-                        'permalink' => ['name' => 'link', 'key' => 'field_64ff232ad91ba'],
-                        'item_icon' => ['name' => 'box_icon', 'key' => 'field_65293de2a26c7'],
-                        'image' => ['name' => 'image', 'key' => 'field_64ff2355d91bb'],
-                        'column_values' => ['name' => 'accordion_column_values', 'key' => 'field_64ff2372d91bc', 'type' => 'repeater', 'fields' => ['value' => ['name' => 'value', 'key' => 'field_64ff23afd91bd']]]
+                    'name'      => 'manual_inputs', 
+                    'key'       => 'field_64ff22b2d91b7',
+                    'type'      => 'repeater', 
+                    'fields'    => [
+                        'post_title'    => [
+                            'name'  => 'title', 
+                            'key'   => 'field_64ff22fdd91b8'
+                        ], 
+                        'post_content' => [
+                            'name'  => 'content', 
+                            'key'   => 'field_64ff231ed91b9'
+                        ],
+                        'permalink' => [
+                            'name'  => 'link', 
+                            'key'   => 'field_64ff232ad91ba'
+                        ],
+                        'item_icon' => [
+                            'name'  => 'box_icon', 
+                            'key'   => 'field_65293de2a26c7'
+                        ],
+                        'image' => [
+                            'name'  => 'image', 
+                            'key'   => 'field_64ff2355d91bb'
+                        ],
+                        'column_values' => [
+                            'name'      => 'accordion_column_values', 
+                            'key'       => 'field_64ff2372d91bc', 
+                            'type'      => 'repeater', 
+                            'fields'    => [
+                                'value' => [
+                                    'name'  => 'value', 
+                                    'key'   => 'field_64ff23afd91bd'
+                                ]
+                            ]
+                        ]
                     ]
                 ],
                 'posts_list_column_titles' => [
-                    'name' => 'accordion_column_titles',
-                    'key' => 'field_65005968bbc75',
-                    'type' => 'repeater',
-                    'fields' => [
-                        'column_header' => ['name' => 'accordion_column_title', 'key' => 'field_65005a33bbc77'], 
+                    'name'      => 'accordion_column_titles',
+                    'key'       => 'field_65005968bbc75',
+                    'type'      => 'repeater',
+                    'fields'    => [
+                        'column_header' => [
+                            'name'  => 'accordion_column_title', 
+                            'key'   => 'field_65005a33bbc77'
+                        ], 
                     ]
                 ],
                 'title_column_label' => [
@@ -265,50 +293,50 @@ class Upgrade
 
         $this->migrateAcfFieldsValueToNewFields($postsModules, 
             [
-                'post_title' => 'title',
-                'post_content' => 'content',
-                'data' => [
-                    'name' => 'manual_inputs', 
-                    'type' => 'repeater', 
-                    'fields' => [
-                        'post_title' => 'title', 
-                        'post_content' => 'content',
+                'post_title'    => 'title',
+                'post_content'  => 'content',
+                'data'          => [
+                    'name'      => 'manual_inputs', 
+                    'type'      => 'repeater', 
+                    'fields'    => [
+                        'post_title'    => 'title', 
+                        'post_content'  => 'content',
                         'column_values' => 'accordion_column_values',
-                        'permalink' => 'link',
-                        'item_icon' => 'box_icon'
+                        'permalink'     => 'link',
+                        'item_icon'     => 'box_icon'
                     ]
                 ],
                 'posts_columns' => [
-                    'name' => 'columns',
-                    'type' => 'replaceValue',
-                    'values' => [
-                        'grid-md-12' => 'o-grid-12',
-                        'grid-md-6' => 'o-grid-6',
-                        'grid-md-4' => 'o-grid-4',
-                        'grid-md-3' => 'o-grid-3',
-                        'default' => 'o-grid-4'
+                    'name'      => 'columns',
+                    'type'      => 'replaceValue',
+                    'values'    => [
+                        'grid-md-12'    => 'o-grid-12',
+                        'grid-md-6'     => 'o-grid-6',
+                        'grid-md-4'     => 'o-grid-4',
+                        'grid-md-3'     => 'o-grid-3',
+                        'default'       => 'o-grid-4'
                     ]
                 ],
                 'posts_display_as' => [
-                    'name' => 'display_as', 
-                    'type' => 'replaceValue', 
-                    'values' => [
-                        'list' => 'list', 
-                        'expandable-list' => 'accordion', 
-                        'items' => 'card', 
-                        'news' => 'card', 
-                        'index' => 'card', 
-                        'segment' => 'segment', 
-                        'collection' => 'collection', 
-                        'features-grid' => 'box', 
-                        'grid' => 'block', 
-                        'default' => 'card'
+                    'name'      => 'display_as', 
+                    'type'      => 'replaceValue', 
+                    'values'    => [
+                        'list'              => 'list', 
+                        'expandable-list'   => 'accordion', 
+                        'items'             => 'card', 
+                        'news'              => 'card', 
+                        'index'             => 'card', 
+                        'segment'           => 'segment', 
+                        'collection'        => 'collection', 
+                        'features-grid'     => 'box', 
+                        'grid'              => 'block', 
+                        'default'           => 'card'
                     ]
                 ],
                 'posts_list_column_titles' => [
-                    'name' => 'accordion_column_titles',
-                    'type' => 'repeater',
-                    'fields' => [
+                    'name'      => 'accordion_column_titles',
+                    'type'      => 'repeater',
+                    'fields'    => [
                         'column_header' => 'accordion_column_title', 
                     ]
                 ],
