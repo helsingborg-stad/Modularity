45198216b89acbfd9410db479b29b1444632bc61
NiclasNorin <niclasn@outlook.com>
2024-03-18 17:01:47 +0100

    fix: changing migrators interface to handle both blocks and modules

--- source/php/Upgrade/Migrators/MigratorInterface.php
+++ source/php/Upgrade/Migrators/MigratorInterface.php
@@ -2,7 +2,19 @@
 
 namespace Modularity\Upgrade\Migrators;
 
+/**
+ * Interface MigratorInterface
+ * 
+ * This interface defines the contract for migrators.
+ */
 interface MigratorInterface
 {
-    public function migrate():mixed;
+    /**
+     * Perform migration.
+     * Blocks: returns the data attached to the block.
+     * Modules: Updates the fields using ACF functions.
+     * 
+     * @return mixed|void 
+     */
+    public function migrate();
 }
