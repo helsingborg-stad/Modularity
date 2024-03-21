35a15f94523254fb4406b124a1a195c5915a642a
Malin Kyttä <malinkytta@gmail.com>
2024-03-21 11:28:14 +0100

    refactor: edit modules block from js to ts (#606)
    Co-authored-by: Thor Brink <thor.brink@helsingborg.se>

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,7 @@ module.exports = {
 		'js/dynamic-map-acf': './source/js/admin/dynamic-map-acf.js',
 
 		'js/block-validation': './source/js/block-validation.ts',
-		'js/edit-modules-block-editor': './source/js/edit-modules-block-editor.js',
-
+		'js/edit-modules-block-editor': './source/js/edit-modules-block-editor.ts',
 		//Modules
 		'js/mod-curator-load-more': './source/php/Module/Curator/assets/mod-curator-load-more.js',
 		'js/table-init': './source/php/Module/Table/assets/table-init.js',
