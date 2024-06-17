56440d74341de7086f73c8ffba7290efbafc2298
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-06-17 11:51:06 +0200

    hotfix: cache module usage

--- source/php/Helper/ModuleUsageByName.php
+++ source/php/Helper/ModuleUsageByName.php
@@ -6,33 +6,72 @@ use Modularity\Helper\ModuleUsageById;
 
 class ModuleUsageByName
 {
+    private static $cache = [];
+    private static $cacheGroup = 'modularity-module-usage';
+    private static $cacheTtl = 3600;
+
+    private static function primeCache($postType, $cacheKey)
+    {
+        if (!isset(self::$cache[$cacheKey])) {
+            self::$cache[$cacheKey] = wp_cache_get($postType, self::$cacheGroup);
+        }
+    }
+
+    private static function setCache($postType, $data, $cacheKey)
+    {
+        self::$cache[$cacheKey] = $data;
+        wp_cache_set($postType, $data, self::$cacheGroup, self::$cacheTtl);
+        return self::$cache[$cacheKey];
+    }
+
     public static function getModuleUsageByName($postType)
     {
-        $modules    = self::getPagesByModuleName($postType);
-        $blocks     = self::getBlocksByModuleName($postType);
+        $cacheKey = __FUNCTION__ . ':' . $postType;
+        self::primeCache($postType, $cacheKey);
+
+        if (isset(self::$cache[$cacheKey])) {
+            return self::$cache[$cacheKey];
+        }
         
-        return array_unique(array_merge($modules, $blocks));
+        $modules = self::getPagesByModuleName($postType);
+        $blocks = self::getBlocksByModuleName($postType);
+
+        return self::setCache($postType, array_unique(array_merge($modules, $blocks)), $cacheKey);
     }
 
     public static function getBlocksByModuleName($postType)
     {
+        $cacheKey = __FUNCTION__ . ':' . $postType;
+        self::primeCache($postType, $cacheKey);
+
+        if (isset(self::$cache[$cacheKey])) {
+            return self::$cache[$cacheKey];
+        }
+
         global $wpdb;
 
         $postType = str_replace('mod-', 'acf/', $postType);
         $pages = $wpdb->get_results(
-            "SELECT *
+            "SELECT ID
             FROM $wpdb->posts
             WHERE post_content LIKE '%{$postType}%'
             AND post_status = 'publish'"
         );
-        
-        return is_array($pages) ? array_column($pages, 'ID') : [];
+
+        $result = is_array($pages) ? array_column($pages, 'ID') : [];
+        return self::setCache($postType, $result, $cacheKey);
     }
 
     public static function getPagesByModuleName($postType)
     {
-        $posts = self::getPostsByType($postType);
+        $cacheKey = __FUNCTION__ . ':' . $postType;
+        self::primeCache($postType, $cacheKey);
 
+        if (isset(self::$cache[$cacheKey])) {
+            return self::$cache[$cacheKey];
+        }
+
+        $posts = self::getPostsByType($postType);
         $pages = [];
         foreach ($posts as $post) {
             if (empty($post->ID)) {
@@ -45,19 +84,27 @@ class ModuleUsageByName
             }
         }
 
-
-        return is_array($pages) ? $pages : [];
+        $result = is_array($pages) ? $pages : [];
+        return self::setCache($postType, $result, $cacheKey);
     }
 
     public static function getPostsByType($postType)
     {
+        $cacheKey = __FUNCTION__ . ':' . $postType;
+        self::primeCache($postType, $cacheKey);
+
+        if (isset(self::$cache[$cacheKey])) {
+            return self::$cache[$cacheKey];
+        }
+
         $args = array(
             'post_type'      => $postType,
             'posts_per_page' => -1,
             'post_status'    => 'publish',
         );
 
-        return get_posts($args);
+        $result = get_posts($args);
+        return self::setCache($postType, $result, $cacheKey);
     }
 
     private static function getModuleUsageById($postId)
