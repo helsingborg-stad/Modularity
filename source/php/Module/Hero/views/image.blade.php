123aff63ff15dcc59441cb94196748e42bcf9c92
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2024-07-31 10:23:14 +0200

    fix: removes unrecongnized sidebar context in hero module. (#649)
    Gets passed in context param instead.
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Module/Hero/views/image.blade.php
+++ source/php/Module/Hero/views/image.blade.php
@@ -9,7 +9,6 @@
     "paragraph" => $paragraph,
     "stretch" => isset($blockData) ? ((bool) $blockData['align'] == 'full') : $stretch,
     "context" => ['hero', 'module.hero', 'module.hero.image', $sidebarContext . '.animation-item'],
-    "sidebar" => $sidebarContext ? $sidebarContext : false,
     "ariaLabel" => $ariaLabel,
     "heroView" => $heroView,
     "buttonArgs" => $buttonArgs
