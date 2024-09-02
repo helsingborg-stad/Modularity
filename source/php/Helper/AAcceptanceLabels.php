5ad463ac2df86e6e7094044aeb55d0a21b9326f5
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-02 16:33:02 +0200

    fix: make name of AcceptanceLabels class wrong

--- source/php/Helper/AAcceptanceLabels.php
+++ source/php/Helper/AAcceptanceLabels.php
@@ -10,7 +10,6 @@ class AcceptanceLabels {
                 'info' => sprintf(__('This part of the website shows content from %s. By continuing, <a href="%s"> you are accepting GDPR and privacy policy</a>.', 'modularity'), '{SUPPLIER_WEBSITE}', '{SUPPLIER_POLICY}'),
                 'button' => __('I understand, continue.', 'modularity'),
             ],
-
             'unknownLabels' => [
                 'title' => __('We need your consent to continue', 'modularity'),
                 'info' => __('This part of the website shows content from another website ({SUPPLIER_WEBSITE}). By continuing, you are accepting GDPR and privacy policy.', 'modularity'),
