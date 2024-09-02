f873b20a7fc60cebed386f778bcfe7e7ca14e084
Niclas Norin <103985736+NiclasNorin@users.noreply.github.com>
2024-09-02 08:59:41 +0200

    feat: audio module (#656)
    * feat: audio module
    
    * chore: icon
    
    * fix: corrected src
    
    * feat: audio size and alignment
    
    * fix: remove width

---
+++ source/php/Module/Audio/views/partials/content.blade.php
@@ -0,0 +1,3 @@
+<div class="u-display--flex u-justify-content--{{$alignment}}">
+    <audio controls src="{{$url}}" aria-labelledby="{{'mod-audio-' . $ID .'-label'}}"></audio>
+</div>
--- source/php/Module/Script/Script.php
+++ source/php/Module/Script/Script.php
@@ -122,21 +122,10 @@ class Script extends \Modularity\Module
         "u-padding__y--{$embededCardPadding} u-padding__x--$embededCardPadding" :
         '';
 
-        $data['lang'] = [
-            'knownLabels' => [
-                'title' => __('We need your consent to continue', 'modularity'),
-                'info' => sprintf(__('This part of the website shows content from %s. By continuing, <a href="%s"> you are accepting GDPR and privacy policy</a>.', 'modularity'), '{SUPPLIER_WEBSITE}', '{SUPPLIER_POLICY}'),
-                'button' => __('I understand, continue.', 'modularity'),
-            ],
-
-            'unknownLabels' => [
-                'title' => __('We need your consent to continue', 'modularity'),
-                'info' => __('This part of the website shows content from another website ({SUPPLIER_WEBSITE}). By continuing, you are accepting GDPR and privacy policy.', 'modularity'),
-                'button' => __('I understand, continue.', 'modularity'),
-            ],
-        ];
+        $data['lang'] = \Modularity\Helper\AcceptanceLabels::getLabels();
         $requiresAccept = false;
         $arrSrc = array();
+        
         foreach ($data['embed'] ?? [] as $embedSrc) {
             if ($embedSrc['requiresAccept'] == 1) {
                 $requiresAccept = true;
