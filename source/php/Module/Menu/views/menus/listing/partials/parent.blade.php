ad81b21513ebf3b00c8730932d53a06ebb1835fd
Niclas <niclasn@outlook.com>
2024-09-10 15:02:34 +0200

    feat: description menu module and hover animations

--- source/php/Module/Menu/views/menus/listing/partials/parent.blade.php
+++ source/php/Module/Menu/views/menus/listing/partials/parent.blade.php
@@ -14,15 +14,32 @@
         ])
         @endicon
     @endif
+    @group([
+        'direction' => 'vertical',
+        'classList' => [
+            'u-width--100',
+            'mod-menu__heading-label-wrapper',
+            empty($menuItem['icon']['icon']) ? 'u-margin__left--5' : '',
+        ]
+    ])
     @typography([
         'element' => 'h2',
         'variant' => 'h2',
         'classList' => [
-            empty($menuItem['icon']['icon']) ? 'u-margin__left--5' : '',
             'mod-menu__heading-label',
-            'u-color--primary'
         ]
     ])
         {{$menuItem['label'] ?? ""}}
     @endtypography
+    @if (!empty($menuItem['description']))
+        @typography([
+            'element' => 'span',
+            'classList' => [
+                'mod-menu__heading-description'
+            ]
+        ])
+        {{ $menuItem['description'] }}
+        @endtypography
+    @endif
+    @endgroup
 @endlink
