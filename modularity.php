d8893c7804b976a2319047dd56c5278d01304afd
GitHub Action <>
2024-09-11 12:14:02 +0000

    build: update package file versions to 6.25.0

--- modularity.php
+++ modularity.php
@@ -4,7 +4,7 @@
  * Plugin Name: Modularity
  * Plugin URI: -
  * Description: Modular component system for WordPress
- * Version: 6.24.2
+ * Version: 6.25.0
  * Author: Kristoffer Svanmark, Sebastian Thulin
  * Author URI: -
  * Text domain: modularity
