b7322d7c804c824fa307dd5d2648d0d7c0dfdc69
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-20 08:14:47 +0200

    feat: add a resolver for image alt text

--- source/php/Integrations/Component/ImageFocusResolver.php
+++ source/php/Integrations/Component/ImageFocusResolver.php
@@ -6,8 +6,18 @@ use \ComponentLibrary\Integrations\Image\ImageFocusResolverInterface;
 
 class ImageFocusResolver implements ImageFocusResolverInterface {
 
+  /**
+   * Constructor
+   * 
+   * @param array $data The data array to resolve from
+   */
   public function __construct(private $data){}
 
+  /**
+   * Get focus point
+   * 
+   * @return array
+   */
   public function getFocusPoint(): array {
     $data = $this->data;
     if($data && isset($data['left'], $data['top'])) {
--- source/php/Integrations/Component/ImageResolver.php
+++ source/php/Integrations/Component/ImageResolver.php
@@ -5,11 +5,33 @@ namespace Modularity\Integrations\Component;
 use \ComponentLibrary\Integrations\Image\ImageResolverInterface;
 
 class ImageResolver implements ImageResolverInterface {
+  
+  /**
+   * Get image url
+   * 
+   * @param int $id
+   * @param array $size
+   * @return string|null
+   */
   public function getImageUrl(int $id, array $size): ?string {
     $image = wp_get_attachment_image_src($id, $size); 
     if($image !== false && isset($image[0]) && filter_var($image[0], FILTER_VALIDATE_URL)) {
       return $image[0]; 
     }
     return null; 
+  }
+
+  /**
+   * Get image alt
+   * 
+   * @param int $id
+   * @return string|null
+   */
+  public function getImageAlt(int $id): ?string {
+    $alt = get_post_meta($id, '_wp_attachment_image_alt', true); 
+    if($alt) {
+      return $alt; 
+    }
+    return null;
   }
 };
