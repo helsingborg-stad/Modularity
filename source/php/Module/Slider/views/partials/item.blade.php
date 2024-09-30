2c11f8a706b9d81312e2c47f8a7e715f70e15f68
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2024-09-30 13:22:04 +0200

    feat: implement ImageContract in slider. (#672)
    * feat: implement ImageContract in slider.
    
    * fix: add link description
    
    * fix: show image poster on video slides.
    
    ---------
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Module/Slider/views/partials/item.blade.php
+++ source/php/Module/Slider/views/partials/item.blade.php
@@ -2,13 +2,13 @@
     'title'             => $slide['textblock_title'],
     'text'              => $slide['textblock_content'],
     'layout'            => $slide['textblock_position'],
-    'desktop_image'     => $slide['image']['src'],
-    'background_video'  => $slide['video_mp4']['url'] ?? false,
-    'link'              => $slide['link_url'],
-    'alt'               => $slide['image']['alt'],
+    'image'             => $slide['hasImageContract'] ? $slide['image'] : $slide['image']['src'],
+    'video'             => $slide['video_mp4']['url'] ?? false,
+    'link'              => $slide['link_url'] ?? null,
+    'linkDescription'   => $slide['link_url_description'] ?? null,
+    'alt'               => $slide['hasImageContract'] ? null : $slide['image']['alt'],
     'heroStyle'         => $sidebarContext === 'sidebar.slider-area',
     'cta'               => $slide['call_to_action'] ?? null,
-    'focusPoint'        => $slide['focusPoint'],
     'context'           => ['module.slider-item', $sidebarContext . '.module.slider-item']
 ])
 @endslider__item
