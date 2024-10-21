ebc6d53260edcc85347d4ca3797d5e6e95293f50
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-21 15:25:58 +0200

    fix: resolve broken tooltips

--- source/php/Module/Contacts/views/components/phone.blade.php
+++ source/php/Module/Contacts/views/components/phone.blade.php
@@ -1,14 +1,15 @@
-@button([
-    'text' => $lang->call,
-    'color' => 'default',
-    'style' => 'basic',
-    'href' => 'tel:' . $number,
-    'icon' => $type == 'smartphone' ? 'smartphone' : 'call',
-    'reversePositions' => 'true',
-    'attributeList' => [
-        'itemprop' => 'telephone',
-        'data-tooltip' => $number
-    ],
-    'classList' => ['c-button--phone', 'c-button--' . $type, 'u-margin--0']
-])
-@endbutton
+<span data-tooltip="{!! $number !!}">
+    @button([
+        'text' => $lang->call,
+        'color' => 'default',
+        'style' => 'basic',
+        'href' => 'tel:' . $number,
+        'icon' => $type == 'smartphone' ? 'smartphone' : 'call',
+        'reversePositions' => 'true',
+        'attributeList' => [
+            'itemprop' => 'telephone'
+        ],
+        'classList' => ['c-button--phone', 'c-button--' . $type, 'u-margin--0']
+    ])
+    @endbutton
+</span>
