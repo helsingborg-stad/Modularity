b175431db1a38d23e0b8adfef34fd4aa7bef4efb
Niclas <niclasn@outlook.com>
2024-10-21 14:15:18 +0200

    feat: private controller

---
+++ source/php/Module/ManualInput/Private/PrivateController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modularity\Module\ManualInput\Private;
+
+class PrivateController extends \Modularity\Module\ManualInput\ManualInput
+{
+    public function __construct(array $data, array $fields = [])
+    {
+        
+    }
+}
