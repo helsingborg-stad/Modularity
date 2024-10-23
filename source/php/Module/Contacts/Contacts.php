7db9d37173148a74fe0568ad12b88e2fb898e0e4
Thor Brink <thor.brink@helsingborg.se>
2024-10-23 08:37:33 +0000

    feat: allow additional fields for user in mod contacts

--- source/php/Module/Contacts/Contacts.php
+++ source/php/Module/Contacts/Contacts.php
@@ -83,29 +83,36 @@ class Contacts extends \Modularity\Module
                     break;
 
                 case 'user':
+                    $fields = function_exists('get_fields') ? get_fields('user_' . $contact['user']['ID']) : [];
                     $info = apply_filters('Modularity/mod-contacts/contact-info', array(
                          'id'                  => !empty($contact['user']['ID']) ? $contact['user']['ID'] : '',
-                         'image'               => null,
+                         'image'               => $fields['user_profile_picture_id'] ?? null,
                          'first_name'          => $contact['user']['user_firstname'] ?? '',
                          'last_name'           => $contact['user']['user_lastname'] ?? '',
                          'work_title'          => null,
                          'administration_unit' => null,
                          'email'               => $contact['user']['user_email'] ?? '',
-                         'phone'               => null,
-                         'social_media'        => null,
-                         'address'             => strip_tags($contact['address'] ?? '', '<br>'),
-                         'visiting_address'    => null,
+                         'phone'               => $fields['phone_numbers'] ?: null,
+                         'social_media'        => $this->getUserSocialMedia($fields),
+                         'address'             => strip_tags($fields['address'] ?? '', '<br>'),
+                         'visiting_address'    => strip_tags($fields['visiting_address'] ?? '', ['<br>', '<a>']),
                          'opening_hours'       => null,
                          'other'               => $contact['user']['user_description'] ?? '',
                      ), $contact, $contact['acf_fc_layout']);
                     break;
             }
 
-            //Parse thumbnail id's
-            $info['thumbnail'] = false;
+            $attachmentId = null;
+
             if (isset($info['image']) && !empty($info['image']) && isset($info['image']['id']) && is_numeric($info['image']['id'])) {
+                $attachmentId = $info['image']['id'];
+            } elseif (!empty($info['image']) && filter_var($info['image'], FILTER_VALIDATE_URL)) {
+                $attachmentId = attachment_url_to_postid($info['image']);
+            }
+
+            if (!empty($attachmentId)) {
                 $info['thumbnail'] = wp_get_attachment_image_src(
-                    $info['image']['id'],
+                    $attachmentId,
                     [400, 400]
                 );
             }
@@ -133,7 +140,7 @@ class Contacts extends \Modularity\Module
 
                     switch ($item['media']) {
                         case 'linkedin':
-                            $item['label'] = 'Linkedin';
+                            $item['label'] = 'LinkedIn';
                             break;
                         case 'twitter':
                             $item['label'] = 'X';
@@ -155,6 +162,29 @@ class Contacts extends \Modularity\Module
         }
 
         return $retContacts;
+    }
+
+    private function getUserSocialMedia(array $fields): ?array {
+        
+        $socialMedia = array();
+
+        if(!empty($fields['user_facebook_url'])) {
+            $socialMedia[] = ['media' => 'facebook', 'url' => $fields['user_facebook_url']];
+        }
+
+        if(!empty($fields['user_twitter_username'])) {
+            $socialMedia[] = ['media' => 'twitter', 'url' => $fields['user_twitter_username']];
+        }
+
+        if(!empty($fields['user_linkedin_url'])) {
+            $socialMedia[] = ['media' => 'linkedin', 'url' => $fields['user_linkedin_url']];
+        }
+
+        if(!empty($fields['user_instagram_username'])) {
+            $socialMedia[] = ['media' => 'instagram', 'url' => $fields['user_instagram_username']];
+        }
+
+        return $socialMedia ?: null;
     }
 
     public function template()
