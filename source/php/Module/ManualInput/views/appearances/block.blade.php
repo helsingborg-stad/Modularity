e14f87c8dd8de3a9cd36873fdca062ec14db6fd8
Niclas <niclasn@outlook.com>
2024-11-04 12:47:21 +0100

    fix: block not working in manual input

--- source/php/Module/ManualInput/views/appearances/block.blade.php
+++ source/php/Module/ManualInput/views/appearances/block.blade.php
@@ -3,7 +3,7 @@
     'content'   => $input['content'],
     'ratio'     => $input['isHighlighted'] ? '16:9' : $ratio,
     'image'     => $input['image'],
-    'classList' => array_merge($input['classList'] ?? [], [$input['columnSize'], 'u-height--100'])
+    'classList' => array_merge($input['classList'] ?? [], [$input['columnSize'], 'u-height--100']),
     'context'   => $context,
     'link'      => $input['link'],
     'attributeList' => $input['attributeList'] ?? []
