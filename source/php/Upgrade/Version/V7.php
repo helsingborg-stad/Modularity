b6b85e5b733219a494268f77f549ec4675fd5cb9
Niclas <niclasn@outlook.com>
2024-12-09 11:05:12 +0100

    feat: v7 upgrade

--- source/php/Upgrade/Version/V7.php
+++ source/php/Upgrade/Version/V7.php
@@ -3,97 +3,86 @@
 namespace Modularity\Upgrade\Version;
 
 use \Modularity\Upgrade\Migrators\Block\AcfBlockMigration;
-use \Modularity\Upgrade\Migrators\Module\AcfModuleMigration;
+use Modularity\Upgrade\Version\Helper\GetPagesByBlockName;
 use \Modularity\Upgrade\Version\Helper\GetPostsByPostType;
+use Modularity\Upgrade\Version\Helper\UpdatePageContent;
+use WP_CLI;
 
-class V6 implements versionInterface {
-    private $db;
-    private $name;
+class V7 implements versionInterface {
+    private string $name      = 'posts';
+    private string $fieldName = 'posts_display_as';
+    private string $newValue  = 'index';
+    private string $oldValue  = 'news';
 
-    public function __construct(\wpdb $db) {
-        $this->db = $db;
-        $this->name = 'manualinput';
+    public function __construct(private \wpdb $db) 
+    {
     }
 
     public function upgrade(): bool
     {
-        $this->upgradeBlocks();
         $this->upgradeModules();
+        $this->upgradeBlocks();
 
         return true;
     }
 
     private function upgradeModules() 
     {
-        $moduleMigrator = new AcfModuleMigration(
-            $this->db,
-            GetPostsByPostType::getPostsByPostType('mod-' . $this->name),
-            $this->getFields()
-        );
+        $postModules = GetPostsByPostType::getPostsByPostType('mod-' . $this->name);
+        
+        foreach ($postModules as $module) {
+            if (empty($module->ID)) {
+                continue;
+            }
 
-        $moduleMigrator->migrateModules();
+            $displayAs = get_field($this->fieldName, $module->ID);
+
+            if (empty($displayAs) || $displayAs !== $this->oldValue) {
+                continue;
+            }
+
+            $result = update_field($this->fieldName, $this->newValue, $module->ID);
+
+            if ($result) {
+                WP_CLI::line(sprintf('%s was updated', (string) $module->ID));
+            } else {
+                WP_CLI::warning(sprintf('Failed to update %s', (string) $module->ID));
+            }
+        }
     }
 
     private function upgradeBlocks() 
     {
-        $blockMigrator = new AcfBlockMigration(
-            $this->db,
-            'acf/' . $this->name,
-            $this->getFields()
-        );
+        $pages = GetPagesByBlockName::getPagesByBlockName($this->db, 'acf/' . $this->name);
 
-        $blockMigrator->migrateBlocks();
-    }
+        if (empty($pages)) {
+            WP_CLI::line('Success, no blocks to upgrade.');
+        }
 
-    private function getFields() 
-    {
-        return 
-        [
-            'posts_columns' => [
-                'type' => 'removeField'
-            ],
-            'posts_fields' => [
-                'type' => 'removeField'
-            ],
-            'posts_data_source' => [
-                'type' => 'removeField'
-            ],
-            'data' => [
-                'type' => 'removeField'
-            ],
-            'posts_sort_by' => [
-                'type' => 'removeField'
-            ],
-            'posts_sort_order' => [
-                'type' => 'removeField'
-            ],
-            'posts_taxonomy_filter' => [
-                'type' => 'removeField'
-            ],
-            'show_as_slider' => [
-                'type' => 'removeField'
-            ],
-            'posts_highlight_first' => [
-                'type' => 'removeField'
-            ],
-            'posts_display_as' => [
-                'type' => 'removeField'
-            ],
-            'posts_list_column_titles' => [
-                'type' => 'removeField'
-            ],
-            'taxonomy_display' => [
-                'type' => 'removeField'
-            ],
-            'post_single_show_featured_image' => [
-                'type' => 'removeField'
-            ],
-            'title_column_label' => [
-                'type' => 'removeField'
-            ],
-            'allow_freetext_filtering' => [
-                'type' => 'removeField'
-            ]
-        ];
+        foreach ($pages as $page) {
+            $blocks = parse_blocks($page->post_content);
+            $hasUpdatedBlocks = false;
+            foreach ($blocks as &$block) {
+                if ($block['blockName'] !== 'acf/' . $this->name) {
+                    continue;
+                }
+
+                if (
+                    !empty($block['attrs']['data'][$this->fieldName]) && 
+                    $block['attrs']['data'][$this->fieldName] !== $this->oldValue
+                ) {
+                    continue;
+                }
+
+                $block['attrs']['data'][$this->fieldName] = $this->newValue;
+                $hasUpdatedBlocks = true;
+            }
+
+            if ($hasUpdatedBlocks) {
+                $serializedBlocks = serialize_blocks($blocks);
+                UpdatePageContent::update($this->db, $page, $serializedBlocks);
+                WP_CLI::line('Updated blocks for page: ' . $page->ID);
+            }
+        }
     }
 }
