21f864b5850739134a19acf8aab727256358173f
Thor Brink <thor.brink@helsingborg.se>
2024-12-18 08:31:36 +0000

    test: run in separate process

--- source/php/Module/FilesList/FilesListTest.php
+++ source/php/Module/FilesList/FilesListTest.php
@@ -5,6 +5,7 @@ namespace Modularity\Module\FilesList;
 use AcfService\Implementations\FakeAcfService;
 use Modularity\Helper\AcfService;
 use Modularity\Helper\WpService;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use WpService\Implementations\FakeWpService;
@@ -12,6 +13,7 @@ use WpService\Implementations\FakeWpService;
 class FilesListTest extends TestCase
 {
     #[TestDox('prepareFileData() does not throw warnings when settings is not an array')]
+    #[RunInSeparateProcess]
     public function testPrepareFileDataSettingsIsNotArray()
     {
         WpService::set(new FakeWpService(['sanitizeTitle' => fn($title) => $title]));
