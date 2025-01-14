977ca9c8fbab74b6071c04e57837b3a1e9ca3d1b
Niclas <niclasn@outlook.com>
2025-01-14 15:39:08 +0100

    fix: posts corrected variable

--- source/php/Module/Posts/TemplateController/AbstractController.php
+++ source/php/Module/Posts/TemplateController/AbstractController.php
@@ -78,7 +78,7 @@ class AbstractController
                 return $post;
             }
 
-            if($post->originalPostId !== $wpService->getCurrentBlogId()) {
+            if($post->originalBlogId !== $wpService->getCurrentBlogId()) {
                 $wpService->switchToBlog($post->originalBlogId);
             }
 
