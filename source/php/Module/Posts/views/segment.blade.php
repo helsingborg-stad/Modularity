5cb03dc64d3c8f8db122fccf9016d0fb365218d0
Niclas <niclasn@outlook.com>
2025-01-16 17:12:19 +0100

    fix: corrected posts segment view post attributes

--- source/php/Module/Posts/views/segment.blade.php
+++ source/php/Module/Posts/views/segment.blade.php
@@ -4,10 +4,10 @@
 @includeWhen($preamble, 'partials.preamble')
 
 @if($posts)
-    <div class="o-grid{{ !empty($stretch) ? ' o-grid--stretch' : '' }} {{ !empty($noGutter) ? ' o-grid--no-gutter' : '' }} {{ (!empty($preamble)||(!$hideTitle && !empty($postTitle))) ? ' u-margin__top--4' : '' }} {{!empty($post->classList) ? implode(' ', $post->classList) : ''}}" data-js-item-id="{{$post->id}}" {{!empty($post->attributeList) ? implode(' ', $post->attributeList) : ''}}
+    <div class="o-grid{{ !empty($stretch) ? ' o-grid--stretch' : '' }} {{ !empty($noGutter) ? ' o-grid--no-gutter' : '' }} {{ (!empty($preamble)||(!$hideTitle && !empty($postTitle))) ? ' u-margin__top--4' : '' }} {{!empty($post->classList) ? implode(' ', $post->classList) : ''}}" 
         @if (!$hideTitle && !empty($postTitle)) aria-labelledby="{{ 'mod-posts-' . $ID . '-label' }}" @endif>
         @foreach ($posts as $post)
-            <div class="{{ $posts_columns }}">
+            <div class="{{ $posts_columns }}" data-js-item-id="{{$post->id}}" {{!empty($post->attributeList) ? implode(' ', $post->attributeList) : ''}}>
                 @include('partials.post.segment')
             </div>
         @endforeach
