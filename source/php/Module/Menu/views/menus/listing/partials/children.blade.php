c3d791f65be6f298db559fb2a71a526b125e6035
Niclas <niclasn@outlook.com>
2025-02-05 08:39:22 +0100

    fix: corrected animation

--- source/php/Module/Menu/views/menus/listing/partials/children.blade.php
+++ source/php/Module/Menu/views/menus/listing/partials/children.blade.php
@@ -3,6 +3,11 @@
     'compressed' => true,
     'classList' => [
         'mod-menu__children',
-    ]
+    ],
+    'attributeList' => [
+        'data-js-sizeobserver' => 'mod-menu-children',
+        'data-js-sizeobserver-axis' => 'y',
+        'data-js-sizeobserver-element-full-size' => ''
+    ],
 ])
 @endnav
