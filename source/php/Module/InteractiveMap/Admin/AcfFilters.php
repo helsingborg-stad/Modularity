b1998a3da102a5763f302bbcb7ce8d868f2b4d99
Niclas <niclasn@outlook.com>
2025-02-10 13:57:18 +0100

    feat: post type

--- source/php/Module/InteractiveMap/Admin/AcfFilters.php
+++ source/php/Module/InteractiveMap/Admin/AcfFilters.php
@@ -6,11 +6,21 @@ use WpService\WpService;
 
 class AcfFilters
 {
+    private string $placeSchemaName = 'Place';
+
     public function __construct(private WpService $wpService)
     {
-        // die;
-        if ($this->wpService) {
-            // $this->wpService->addFilter()
+        $this->wpService->addFilter('acf/load_field/key=field_67a9b074d4fc5', array($this, 'filterPostTypesBasedOnSchema'));
+    }
+
+    public function filterPostTypesBasedOnSchema($field)
+    {
+        $placePostTypes = \Municipio\SchemaData\Helper\GetSchemaType::getPostTypesFromSchemaType($this->placeSchemaName);
+
+        foreach ($placePostTypes as $postType) {
+            $field['choices'][$postType] = ucfirst($postType);
         }
+
+        return $field;
     }
 }
