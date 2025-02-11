d01835eaac7ef8df74d9d86991269dd77718367a
Michael Claesson <michael.claesson@consid.se>
2025-02-11 12:38:34 +0100

    Action support in notice module (#805)
    * Support for actions in module
    
    * Translation fixes
    
    * Dismissal support and language translation
    
    * Translation

--- source/php/Module/Notice/views/notice.blade.php
+++ source/php/Module/Notice/views/notice.blade.php
@@ -5,6 +5,14 @@
         'title' => !$hideTitle && !empty($postTitle) ? $postTitle : null,
         'text' => $notice_text,
     ],
+    'action' => isset($include_link) && $include_link === true && is_array($link) 
+        ? [
+            'text' => isset($link['title']) ? $link['title'] : $link['url'],
+            'url' => $link['url'],
+            'position' => $link_position,
+         ] 
+        : null,
+    'dismissable' => isset($dismissible) && $dismissible ? $dismissal_time : false,
     'icon' => $icon,
     'context' => ['notice', 'module.notice']
 ])
