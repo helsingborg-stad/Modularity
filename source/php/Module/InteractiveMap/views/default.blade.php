3245bbb6596e6867963fac375d4cfd7f53aee53a
NiclasNorin <niclasn@outlook.com>
2025-03-07 16:54:26 +0100

    refactor: testing filters

--- source/php/Module/InteractiveMap/views/default.blade.php
+++ source/php/Module/InteractiveMap/views/default.blade.php
@@ -1,6 +1,10 @@
-<div class="interactive-map openstreetmap" style="position: relative; height: 700px; width: 100%;" data-js-interactive-map="{{$mapID}}" data-js-interactive-map-data="{{$mapData}}">
-    <div 
-        style="position: unset; height: 700px; width: 100%; background: #f0f0f0;"
-        id="{{$mapID}}">
-    </div>
 <div>
+    <div class="interactive-map openstreetmap" style="position: relative; height: 700px; width: 100%;" data-js-interactive-map="{{$mapID}}" data-js-interactive-map-data="{{$mapData}}">
+        <div 
+            style="position: unset; height: 700px; width: 100%; background: #f0f0f0;"
+            id="{{$mapID}}">
+        </div>
+
+    </div>
+    @includeWhen(!empty($structuredLayerFilters), 'filters')
+</div>
--- source/php/Module/InteractiveMap/views/filters.blade.php
+++ source/php/Module/InteractiveMap/views/filters.blade.php
@@ -3,4 +3,14 @@
         'data-js-interactive-map-filters' => ''
     ]
 ])
+    @if (!empty($structuredLayerFilters[0]))
+        @element([
+
+        ])
+
+            @foreach($structuredLayerFilters[0] as $filter)
+                @dump($filter)
+            @endforeach
+        @endelement
+    @endif
 @endelement
