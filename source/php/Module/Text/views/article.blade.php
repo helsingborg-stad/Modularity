089d6d81f1977962e2475dfe6f37ea672d4c0bb0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-03-18 19:32:42 +0100

    fix: only render text modules as articles, if it contains headings

--- source/php/Module/Text/views/article.blade.php
+++ source/php/Module/Text/views/article.blade.php
@@ -1,5 +1,5 @@
 @element([
-    'componentElement' => 'article',
+    'componentElement' => ((!$hideTitle && !empty($postTitle)) || $hasHeadingsInContent) ? 'article' : 'div',
     'attributeList' => [
         ...($font_size ? ['class' => $font_size] : []),
         ...(!$hideTitle && !empty($postTitle) ? ['aria-labelledby' => 'mod-text-' . $ID . '-label'] : []),
