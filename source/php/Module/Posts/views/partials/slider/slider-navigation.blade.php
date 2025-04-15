f3b5028bd2983737442a065733963f14ca4cd1fb
Michael Claesson <michael.claesson@consid.se>
2025-04-15 09:35:38 +0200

    Remove populated choices

--- source/php/Module/Posts/views/partials/slider/slider-navigation.blade.php
+++ source/php/Module/Posts/views/partials/slider/slider-navigation.blade.php
@@ -1,17 +1,17 @@
     <div class="o-grid-12@sm o-grid-4@md o-grid-4@lg u-display--flex u-align-items--end u-justify-content--end">
-        @if ($posts_data_source !== 'input' && $archiveLinkUrl)
-            <div class="t-read-more-section">
-                @button([
-                    'text' => __('Show more', 'modularity'),
-                    'color' => 'default',
-                    'style' => 'basic',
-                    'href' => $archiveLinkUrl,
-                    'classList' => ['u-flex-grow--1@xs', 'u-margin__right--2'],
-                ])
-                @endbutton
-            </div>
+        @if (($posts_data_source !== 'input' && $archiveLinkUrl))
+        <div class="t-read-more-section">
+            @button([
+                'text' => __('Show more', 'modularity'),
+                'color' => 'default',
+                'style' => 'basic',
+                'href' => $archiveLinkUrl,
+                'classList' => ['u-flex-grow--1@xs', 'u-margin__right--2']
+            ])
+            @endbutton
+        </div>
         @endif
-        <div class="splide__arrows c-slider__arrows" id="js-custom-buttons-{{ $sliderId }}">
+        <div class="splide__arrows c-slider__arrows" id="js-custom-buttons-{{$sliderId}}">
             @button([
                 'classList' => ['splide__arrow', 'splide__arrow--prev'],
                 'icon' => 'keyboard_arrow_left',
