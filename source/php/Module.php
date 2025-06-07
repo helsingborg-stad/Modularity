e6bfc6e46ccace304ed2db31c6dd6a2f53fa336a
Niclas Norin <103985736+NiclasNorin@users.noreply.github.com>
2025-06-07 13:20:01 +0200

    fix: undefined var notice (#863)
    Co-authored-by: Sebastian Thulin <sebastianthulin@users.noreply.github.com>

--- source/php/Module.php
+++ source/php/Module.php
@@ -292,7 +292,7 @@ class Module
      */
     public function isInline(): bool
     {
-        return !empty($this->data['inline']);
+        return (bool) ($this->args['inline'] ?? false);
     }
 
     /**
