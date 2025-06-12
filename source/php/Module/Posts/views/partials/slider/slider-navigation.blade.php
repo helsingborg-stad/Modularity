5e8e69f27e1f054ccdbaaa598b1625fb1923efbf
NiclasNorin <niclasn@outlook.com>
2025-06-12 16:58:50 +0200

    refactor: splide slider custom buttons

--- source/php/Module/Posts/views/partials/slider/slider-navigation.blade.php
+++ source/php/Module/Posts/views/partials/slider/slider-navigation.blade.php
@@ -13,19 +13,23 @@
         @endif
         <div class="splide__arrows c-slider__arrows" id="js-custom-buttons-{{$sliderId}}">
             @button([
-                'classList' => ['splide__arrow', 'splide__arrow--prev'],
                 'icon' => 'keyboard_arrow_left',
                 'style' => 'filled',
                 'color' => 'primary',
                 'ariaLabel' => $ariaLabels->prev,
+                'attributeList' => [
+                    'data-js-slider-prev' => true
+                ]
             ])
             @endbutton
             @button([
-                'classList' => ['splide__arrow', 'splide__arrow--next'],
                 'icon' => 'keyboard_arrow_right',
                 'style' => 'filled',
                 'color' => 'primary',
                 'ariaLabel' => $ariaLabels->next,
+                'attributeList' => [
+                    'data-js-slider-next' => true
+                ]
             ])
             @endbutton
         </div>
